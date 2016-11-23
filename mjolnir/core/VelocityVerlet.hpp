af7d38fab03c979d901ead68122d93007c4ce2b2
ToruNiina <niina.toru.68u@gmail.com>
2016-11-23 17:55:52 +0900

    add VelocityVerlet class

---
+++ mjolnir/core/VelocityVerlet.hpp
@@ -0,0 +1,67 @@
+#ifndef MJOLNIR_VELOCITY_VERLET_INTEGRATOR
+#define MJOLNIR_VELOCITY_VERLET_INTEGRATOR
+#include "ParticleContainer.hpp"
+#include "ForceField.hpp"
+
+namespace mjolnir
+{
+
+template<typename traitsT>
+class VelocityVerlet
+{
+  public:
+    typedef traitsT traits_type;
+    typedef typename traits_type::time_type time_type;
+    typedef typename traits_type::coordinate_type coordinate_type;
+
+  public:
+
+    VelocityVerlet(const time_type dt, const std::size_t number_of_particles)
+        : dt_(dt), halfdt_(dt * 0.5), halfdt2_(dt * dt * 0.5),
+          acceleration_(number_of_particles)
+    {}
+    ~VelocityVerlet() = default;
+
+    time_type step(ParticleContainer<traitsT>& pcon, ForceField<traitsT>& ff);
+
+  private:
+    time_type dt_;      //!< dt
+    time_type halfdt_;  //!< dt/2
+    time_type halfdt2_; //!< dt^2/2
+    std::vector<coordinate_type> acceleration_;
+};
+
+// at the initial step, acceleration_ must be initialized
+template<typename traitsT>
+typename VelocityVerlet<traitsT>::time_type
+VelocityVerlet<traitsT>::step(const time_type time,
+        ParticleContainer<traitsT>& pcon, ForceField<traitsT>& ff)
+{
+    // calc r(t+dt)
+    for(auto iter = make_zip(pcon.begin(), acceleration_.begin());
+            iter != make_zip(pcon.end(), acceleration_.end()); ++iter)
+    {
+        get<0>(iter)->position += dt_ * (get<0>(iter)->velocity) +
+                                  halfdt2_ * (*get<1>(iter));
+        get<0>(iter)->velocity += halfdt_ * (*get<1>(iter));
+    }
+
+    // calc force(t+dt)
+    ff(pcon);
+
+    // calc a(t+dt) and v(t+dt)
+    for(auto iter = make_zip(pcon.begin(), acceleration_.begin());
+            iter != make_zip(pcon.end(), acceleration_.end()); ++iter)
+    {
+        const coordinate_type acc = *get<0>(iter)->force / *get<0>(iter)->mass;
+        *get<1>(iter) = acc;
+        get<0>(iter)->velocity += halfdt_ * acc;
+    }
+
+    return time + dt;
+}
+
+
+} // mjolnir
+
+#endif /* MJOLNIR_VELOCITY_VERLET_INTEGRATOR */
