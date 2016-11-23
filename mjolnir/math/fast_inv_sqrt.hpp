b3506ba49900af478a8f6d4c276850b43f3acd08
ToruNiina <niina.toru.68u@gmail.com>
2016-11-23 17:52:45 +0900

    add fast inverse square root

---
+++ mjolnir/math/fast_inv_sqrt.hpp
@@ -0,0 +1,32 @@
+#ifndef MJOLNIR_FAST_INV_SQRT
+#define MJOLNIR_FAST_INV_SQRT
+#include <cstdint>
+
+namespace mjolnir
+{
+ 
+inline double fast_inv_sqrt(double x)
+{
+    const double xhalf = 0.5 * x;
+    std::int64_t i = *(reinterpret_cast<std::int64_t*>(&x));
+    i = 0x5fe6ec85e7de30daLL - (i >> 1);
+    x = *(reinterpret_cast<double*>(&i));
+    x *= (1.5 - xhalf * x * x);
+    x *= (1.5 - xhalf * x * x);
+    x *= (1.5 - xhalf * x * x);
+    return x;
+}
+
+inline float fast_inv_sqrt(float x)
+{
+    const float xhalf = 0.5f * x;
+    int i = *(reinterpret_cast<int*>(&x));
+    i = 0x5f3759df - (i >> 1);
+    x = *(reinterpret_cast<float*>(&i));
+    return x * (1.5f - xhalf * x * x);
+}
+ 
+ 
+}// mjolnir
+
+#endif /* MJOLNIR_FAST_INV_SQRT */
