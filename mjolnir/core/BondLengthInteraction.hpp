8d7e06c1f5490f723d0173b28391f90933e365c6
ToruNiina <niina.toru.68u@gmail.com>
2016-11-24 17:29:04 +0900

    use LocalPotentialBase instead of PotentialBase

--- mjolnir/core/BondLengthInteraction.hpp
+++ mjolnir/core/BondLengthInteraction.hpp
@@ -1,14 +1,15 @@
 #ifndef MJOLNIR_BOND_LENGTH_INTERACTION
 #define MJOLNIR_BOND_LENGTH_INTERACTION
 #include "Particle.hpp"
-#include "PotentialBase.hpp"
+#include "LocalPotentialBase.hpp"
 #include <mjolnir/math/fast_inv_sqrt.hpp>
 
 namespace mjolnir
 {
 
+/*! @brief calculate energy and force of Bond length type local interaction */
 template<typename traitsT>
-class LengthInteraction
+class BondLengthInteraction
 {
   public:
 
@@ -17,28 +18,27 @@ class LengthInteraction
     typedef typename traits_type::real_type real_type;
     typedef typename traits_type::coordinate_type coordinate_type;
     typedef Particle<coordinate_type> particle_type;
-    typedef PotentialBase<traitsT, 2> potential_type;
+    typedef LocalPotentialBase<traits_type> potential_type;
 
   public:
 
-    LengthInteraction() = default;
-    ~LengthInteraction() = default;
+    BondLengthInteraction() = default;
+    ~BondLengthInteraction() = default;
 
     void
     calc_force(particle_type& p1, particle_type& p2, const potential_type& pot);
 
-    energy_type
+    real_type
     calc_energy(const particle_type& p1, const particle_type& p2,
                 const potential_type& pot);
-
 };
 
 template<typename traitsT>
 inline void
-LengthInteraction<traitsT>::calc_force(particle_type& p1, particle_type& p2, 
-        const PotentialBase<traitsT, 2>& pot)
+BondLengthInteraction<traitsT>::calc_force(particle_type& p1, particle_type& p2,
+        const potential_type& pot)
 {
-    const real_type f = -1 * pot.derivative({{p1.position, p2.position}});
+    const real_type f = -1 * pot.derivative(length(p1.position - p2.position));
     const coordinate_type dpos = p2.position - p1.position;
     const coordinate_type force = dpos * (fast_inv_sqrt(length_sq(dpos)) * f);
     p1.force -= force;
@@ -47,14 +47,13 @@ LengthInteraction<traitsT>::calc_force(particle_type& p1, particle_type& p2,
 }
 
 template<typename traitsT>
-inline typename LengthInteraction<traitsT>::energy_type
-LengthInteraction<traitsT>::calc_energy(
+inline typename BondLengthInteraction<traitsT>::real_type
+BondLengthInteraction<traitsT>::calc_energy(
         const particle_type& p1, const particle_type& p2,
-        const PotentialBase<traitsT, 2>& pot)
+        const potential_type& pot)
 {
-    return pot.potential({{p1.position, p2.position}});
+    return pot.potential(length(p1.position - p2.position));
 }
-
 
 } // mjolnir
 #endif /* MJOLNIR_BOND_LENGTH_INTERACTION */
