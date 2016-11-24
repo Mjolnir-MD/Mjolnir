a324baed90f4d38d28f03d3fe9f352c4bea36a07
ToruNiina <niina.toru.68u@gmail.com>
2016-11-24 17:29:30 +0900

    remove baseclass of LennardJones; it is a global potential

--- mjolnir/core/LennardJones.hpp
+++ mjolnir/core/LennardJones.hpp
@@ -2,14 +2,17 @@
 #define MJOLNIR_LENNARD_JONES_POTENTIAL
 #include <mjolnir/math/Vector.hpp>
 #include <mjolnir/math/fast_inv_sqrt.hpp>
-#include "PotentialBase.hpp"
 #include <cmath>
 
 namespace mjolnir
 {
 
+/*! @brief Lennard-Jones type potential & derivative                       *
+ * designed for global force field. so it doesn't have its own parameters. *
+ * V(r)  =  4. * epsilon * ((r/sigma)^12 - (r/sigma)^6))                   *
+ * dV/dr = 24. * epsilon / r * ((r/sigma)^6 - 2 * (r/sigma)^12)            */
 template<typename traitsT>
-class LennardJones : public PotentialBase<traitsT, 2>
+class LennardJones
 {
   public:
     typedef traitsT traits_type;
@@ -17,42 +20,40 @@ class LennardJones : public PotentialBase<traitsT, 2>
     typedef typename traits_type::coordinate_type coordinate_type;
 
   public:
-    LennardJones(const real_type sigma, const real_type epsilon)
-        : sigma6_inv_(1. / std::pow(sigma, 6)), epsilon_(epsilon)
-    {}
+    LennardJones() = default;
+    ~LennardJones() = default;
 
     real_type
-    potential(std::array<coordinate_type const&, 2> pos) const override;
+    potential(const real_type length_square,
+              const real_type sigma, const real_type epsilon) const;
+
     real_type
-    derivative(std::array<coordinate_type const&, 2> pos) const override;
-
-  private:
-
-    real_type sigma6_inv_;
-    real_type epsilon_;
+    derivative(const real_type length_square,
+               const real_type sigma, const real_type epsilon) const;
 };
 
 template<typename traitsT>
 inline typename LennardJones<traitsT>::real_type
-LennardJones<traitsT>::potential(
-        std::array<coordinate_type const&, 2> pos) const
+LennardJones<traitsT>::potential(const real_type length_square,
+              const real_type sigma, const real_type epsilon) const
 {
-    const real_type r_sq   = length_sq(pos[0] - pos[1]);
-    const real_type r6s6   = r_sq * r_sq * r_sq * sigma6_inv_;
+    const real_type r_sq   = length_square;
+    const real_type sigma6_inv = 1. / std::pow(sigma, 6);
+    const real_type r6s6   = r_sq * r_sq * r_sq * sigma6_inv;
     const real_type r12s12 = r6s6 * r6s6;
-    return 4. * epsilon_ * (r12s12 - r6s6);
+    return 4. * epsilon * (r12s12 - r6s6);
 }
 
 template<typename traitsT>
 inline typename LennardJones<traitsT>::real_type
-LennardJones<traitsT>::derivative(
-        std::array<coordinate_type const&, 2> pos) const
+LennardJones<traitsT>::derivative(const real_type length_square,
+              const real_type sigma, const real_type epsilon) const
 {
-    const real_type r_inv  = fast_inv_sqrt(length_sq(pos[0] - pos[1]));
-    const real_type r_sq   = length_sq(pos[0] - pos[1]);
-    const real_type r6s6   = r_sq * r_sq * r_sq * sigma6_inv_;
+    const real_type r_inv  = fast_inv_sqrt(length_square);
+    const real_type r_sq   = length_square;
+    const real_type r6s6   = r_sq * r_sq * r_sq / std::pow(sigma, 6);
     const real_type r12s12 = r6s6 * r6s6;
-    return 24. * epsilon_ * r_inv * (r6s6 - 2 * r12s12);
+    return 24. * epsilon * r_inv * (r6s6 - 2 * r12s12);
 }
 
 } // mjolnir
