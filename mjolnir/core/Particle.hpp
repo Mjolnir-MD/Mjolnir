8f2d3052141fb33408c1e1f38f35f59616f07893
ToruNiina <niina.toru.68u@gmail.com>
2016-12-22 18:07:51 +0900

    add helper function make_particle

--- mjolnir/core/Particle.hpp
+++ mjolnir/core/Particle.hpp
@@ -14,5 +14,14 @@ struct Particle
     coordT force;
 };
 
+template<typename coordT>
+constexpr inline Particle<coordT>
+make_particle(const scalar_type<coordT> mass, const coordT& pos,
+              const coordT& vel, const coordT& f)
+{
+    return Particle<coordT>{mass, pos, vel, f};
+}
+
+
 } // mjolnir
 #endif /* MJOLNIR_PARTICLE */
