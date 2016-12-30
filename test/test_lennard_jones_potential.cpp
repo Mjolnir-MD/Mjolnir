51768326e1b3f9abcf5f5ab00a5b680fc9c0f2a0
ToruNiina <niina.toru.68u@gmail.com>
2016-12-30 12:20:15 +0900

    add test code for lennard jones potential

---
+++ test/test_lennard_jones_potential.cpp
@@ -0,0 +1,70 @@
+#define BOOST_TEST_MODULE "test_lennard_jones_potential"
+
+#ifdef UNITTEST_FRAMEWORK_LIBRARY_EXIST
+#include <boost/test/unit_test.hpp>
+#else
+#define BOOST_TEST_NO_LIB
+#include <boost/test/included/unit_test.hpp>
+#endif
+
+#include <mjolnir/core/LennardJonesPotential.hpp>
+#include <mjolnir/core/DefaultTraits.hpp>
+#include <mjolnir/util/make_unique.hpp>
+
+typedef mjolnir::DefaultTraits traits;
+
+constexpr static unsigned int      seed = 32479327;
+constexpr static std::size_t       N    = 10000;
+constexpr static traits::real_type h    = 1e-6;
+
+BOOST_AUTO_TEST_CASE(LJ_constructable)
+{
+    std::unique_ptr<mjolnir::GlobalPotentialBase<traits>> lj = 
+        mjolnir::make_unique<mjolnir::LennardJonesPotential<traits>>();
+
+    BOOST_CHECK(lj);
+}
+
+BOOST_AUTO_TEST_CASE(LJ_derivative)
+{
+    std::unique_ptr<mjolnir::LennardJonesPotential<traits>> lj = 
+        mjolnir::make_unique<mjolnir::LennardJonesPotential<traits>>();
+
+    const traits::real_type sigma   = 3.0;
+    const traits::real_type epsilon = 1.0;
+    lj->emplace(std::make_pair(sigma, epsilon));
+    lj->emplace(std::make_pair(sigma, epsilon));
+
+    const traits::real_type x_min = 0.8 * sigma;
+    const traits::real_type x_max =
+        mjolnir::LennardJonesPotential<traits>::cutoff_ratio * sigma;
+    const traits::real_type dx = (x_max - x_min) / N;
+
+    traits::real_type x = x_min;
+    for(std::size_t i=0; i<N; ++i)
+    {
+        const traits::real_type pot1 = lj->potential(0, 1, x + h);
+        const traits::real_type pot2 = lj->potential(0, 1, x - h);
+        const traits::real_type dpot = (pot1 - pot2) / (2 * h);
+
+        const traits::real_type deri = lj->derivative(0, 1, x);
+
+        BOOST_CHECK_CLOSE_FRACTION(dpot, deri, h);
+        x += dx;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
