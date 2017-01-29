6e107d68a05e095c710df4c7942902afefddc438
ToruNiina <niina.toru.68u@gmail.com>
2017-01-29 16:03:35 +0900

    create log-manager, typedefs, and macros

--- mjolnir/util/logger.hpp
+++ mjolnir/util/logger.hpp
@@ -1,16 +1,16 @@
 #ifndef MJOLNIR_UTIL_LOGGER
 #define MJOLNIR_UTIL_LOGGER
+#include "make_unique.hpp"
 #include <string>
 #include <fstream>
 #include <iostream>
 #include <map>
-#include "make_unique.hpp"
 
 namespace mjolnir
 {
 
 template<typename charT, typename char_traits = std::char_traits<charT>>
-class Logger
+class basic_logger
 {
   public:
     typedef std::basic_string<charT, char_traits> string_type;
@@ -27,9 +27,9 @@ class Logger
 
   public:
 
-    explicit Logger(const string_type& name): name_(name){}
-    Logger(Logger&& l) = default;
-    ~Logger() = default;
+    explicit basic_logger(const string_type& name): name_(name){}
+    basic_logger(basic_logger&& l) = default;
+    ~basic_logger() = default;
 
     template<typename ... T_args>
     void log(Level level, T_args&& ... args) const
@@ -47,7 +47,7 @@ class Logger
             return;
         }
         else
-        {
+        {// TODO wchar, u16char, u32char
             return log_(std::cerr, to_string(level), name_,
                         std::forward<T_args>(args)...);
         }
@@ -57,14 +57,6 @@ class Logger
     {
         output_[lev] = std::forward<string_type>(fname);
         return;
-    }
-
-    static
-    Logger& get_logger(const string_type& name)
-    {
-        if(loggers_.count(name) == 0)
-            loggers_.emplace(name, make_unique<Logger>(name));
-        return *(loggers_[name]);
     }
 
   private:
@@ -88,11 +80,11 @@ class Logger
     {
         switch(l)
         {
-            case Level::Debug: return "Log-Debug: ";
-            case Level::Info:  return "Log-Info:  ";
-            case Level::Warn:  return "Log-Warn:  ";
-            case Level::Error: return "Log-Error: ";
-            default:           return "Log-Unknown ";
+            case Level::Debug: return "Log-Debug:";
+            case Level::Info:  return "Log-Info:";
+            case Level::Warn:  return "Log-Warn:";
+            case Level::Error: return "Log-Error:";
+            default:           return "Log-Unknown";
         }
     }
 
@@ -101,19 +93,66 @@ class Logger
     string_type name_;
     std::map<Level, string_type> output_;
 
+};
+
+template<typename charT, typename char_traits = std::char_traits<charT>>
+class LoggerManager
+{
+  public:
+    typedef std::basic_string<charT, char_traits> string_type;
+
+  public:
     static
-    std::map<string_type, std::unique_ptr<Logger>> loggers_;
+    basic_logger<charT, char_traits>& get_logger(const string_type& name)
+    {
+        if(loggers_.count(name) == 0)
+            loggers_.emplace(
+                    name, make_unique<basic_logger<charT, char_traits>>(name));
+        return *(loggers_[name]);
+    }
+
+  private:
+    static
+    std::map<string_type,
+             std::unique_ptr<basic_logger<charT, char_traits>>> loggers_;
 };
 
 template<typename charT, typename traits>
 std::map<std::basic_string<charT, traits>,
-         std::unique_ptr<Logger<charT, traits>>>
-Logger<charT, traits>::loggers_;
+         std::unique_ptr<basic_logger<charT, traits>>>
+LoggerManager<charT, traits>::loggers_;
+
+typedef basic_logger<char>     Logger;
+typedef basic_logger<wchar_t>  wLogger;
+typedef basic_logger<char16_t> u16Logger;
+typedef basic_logger<char32_t> u32Logger;
+
+typedef basic_logger<char>::Level     LogLevel;
+typedef basic_logger<wchar_t>::Level  wLogLevel;
+typedef basic_logger<char16_t>::Level u16LogLevel;
+typedef basic_logger<char32_t>::Level u32LogLevel;
 
 #ifdef MJOLNIR_DEBUG
-#define MJOLNIR_LOG(args...) logger_.log(args);
-#else  // NO-DEBUG
-#define MJOLNIR_LOG(args...) /* args */
+#define MJOLNIR_SET_LOGGER(name)   auto& logger_ = LoggerManager<char>::get_logger(name);
+#define MJOLNIR_LOG(args...)       logger_.log(args);
+#define MJOLNIR_LOG_DEBUG(args...) logger_.log(LogLevel::Debug, args);
+#define MJOLNIR_LOG_INFO(args...)  logger_.log(LogLevel::Info,  args);
+#define MJOLNIR_LOG_WARN(args...)  logger_.log(LogLevel::Warn,  args);
+#define MJOLNIR_LOG_ERROR(args...) logger_.log(LogLevel::Error, args);
+#elif defined(MJOLNIR_NO_DEBUG)
+#define MJOLNIR_SET_LOGGER(name)   /*name*/
+#define MJOLNIR_LOG(args...)       /*args*/
+#define MJOLNIR_LOG_DEBUG(args...) /*args*/
+#define MJOLNIR_LOG_INFO(args...)  /*args*/
+#define MJOLNIR_LOG_WARN(args...)  /*args*/
+#define MJOLNIR_LOG_ERROR(args...) /*args*/
+#else  // normal one
+#define MJOLNIR_SET_LOGGER(name)   auto& logger_ = LoggerManager<char>::get_logger(name);
+#define MJOLNIR_LOG(args...)       /*args*/
+#define MJOLNIR_LOG_DEBUG(args...) /*args*/
+#define MJOLNIR_LOG_INFO(args...)  logger_.log(LogLevel::Info,  args);
+#define MJOLNIR_LOG_WARN(args...)  logger_.log(LogLevel::Warn,  args);
+#define MJOLNIR_LOG_ERROR(args...) logger_.log(LogLevel::Error, args);
 #endif // MJOLNIR_DEBUG
 
 } // mjolnir
