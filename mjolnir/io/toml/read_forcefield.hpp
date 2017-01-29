334a41cff1aa499b8d9baa2e436514d8efead03d
ToruNiina <niina.toru.68u@gmail.com>
2017-01-29 16:11:16 +0900

    add LOG to io/toml

--- mjolnir/io/toml/read_forcefield.hpp
+++ mjolnir/io/toml/read_forcefield.hpp
@@ -12,10 +12,13 @@ template<typename traitsT>
 ForceField<traitsT>
 read_force_field(const toml::Table& tab)
 {
+    MJOLNIR_SET_LOGGER("read_toml_file");
+    MJOLNIR_LOG_DEBUG("read_force_field CALLED");
+
     LocalForceField<traitsT> local;
     try
     {
-        const toml::Array<toml::Table> lff = 
+        const toml::Array<toml::Table> lff =
             toml::get<toml::Array<toml::Table>>(tab.at("localforcefield"));
         LocalForceField<traitsT> tmp = read_local_force_field<traitsT>(lff);
         local = std::move(tmp);
@@ -25,7 +28,7 @@ read_force_field(const toml::Table& tab)
     GlobalForceField<traitsT> global;
     try
     {
-        const toml::Array<toml::Table> gff = 
+        const toml::Array<toml::Table> gff =
             toml::get<toml::Array<toml::Table>>(tab.at("globalforcefield"));
         GlobalForceField<traitsT> tmp = read_global_force_field<traitsT>(gff);
         global = std::move(tmp);
@@ -33,6 +36,7 @@ read_force_field(const toml::Table& tab)
     catch(std::exception& except){}
 
     ForceField<traitsT> ff(std::move(local), std::move(global));
+    MJOLNIR_LOG_DEBUG("read_force_field RETURNED");
     return ff;
 }
 
--- mjolnir/io/toml/read_particles.hpp
+++ mjolnir/io/toml/read_particles.hpp
@@ -4,6 +4,7 @@
 #include <mjolnir/util/zip_iterator.hpp>
 #include <mjolnir/util/make_zip.hpp>
 #include <mjolnir/util/make_unique.hpp>
+#include <mjolnir/util/logger.hpp>
 #include <toml/toml.hpp>
 
 namespace mjolnir
@@ -13,10 +14,16 @@ template<typename traitsT>
 ParticleContainer<traitsT>
 read_particles(const toml::Table& tab)
 {
+    MJOLNIR_SET_LOGGER("read_toml_file");
+    MJOLNIR_LOG_DEBUG("read_particles CALLED");
+
     const std::size_t num_particles = static_cast<std::size_t>(
         toml::get<toml::Integer>(tab.at("number_of_particle")));
+    MJOLNIR_LOG_INFO("num_particles", num_particles);
+
     const std::vector<toml::Table> particles =
         toml::get<toml::Array<toml::Table>>(tab.at("particles"));
+    MJOLNIR_LOG_INFO("particles.size()", particles.size());
     if(num_particles != particles.size())
         throw std::runtime_error(
                 "number_of_particles is not equal to particles.size()");
@@ -33,6 +40,8 @@ read_particles(const toml::Table& tab)
                 get<0>(iter)->at("velocity"));
         const auto acc = toml::get<toml::Array<toml::Float>>(
                 get<0>(iter)->at("acceleration"));
+        MJOLNIR_LOG_DEBUG("mass", mass, "position", pos, "velocity", vel,
+                          "acceleration", acc, "from TOML file");
 
         const typename traitsT::coordinate_type
             position(pos.at(0), pos.at(1), pos.at(2));
@@ -40,9 +49,12 @@ read_particles(const toml::Table& tab)
             velocity(vel.at(0), vel.at(1), vel.at(2));
         const typename traitsT::coordinate_type
             acceleration(acc.at(0), acc.at(1), acc.at(2));
+        MJOLNIR_LOG_INFO("mass", mass, "position", position,
+                         "velocity", velocity, "acceleration", acceleration);
 
         *get<1>(iter) = make_particle(mass, position, velocity, acceleration);
     }
+    MJOLNIR_LOG_DEBUG("read_particles RETURNED");
     return pcon;
 }
 
