56033c9d44cc5ddb99a33427d8a6964d0ad566b0
ToruNiina <niina.toru.68u@gmail.com>
2017-02-06 17:09:38 +0900

    change potential container and add initialize method

--- mjolnir/core/LocalForceField.hpp
+++ mjolnir/core/LocalForceField.hpp
@@ -18,17 +18,14 @@ class LocalForceField
 {
   public:
     typedef traitsT traits_type;
-    typedef typename traits_type::time_type time_type;
-    typedef typename traits_type::real_type real_type;
+    typedef typename traits_type::time_type       time_type;
+    typedef typename traits_type::real_type       real_type;
     typedef typename traits_type::coordinate_type coordinate_type;
-    typedef Particle<coordinate_type> particle_type;
-    typedef ParticleContainer<traits_type> particle_container_type;
-    typedef LocalPotentialBase<traits_type> potential_base;
-    typedef std::unique_ptr<potential_base> potential_ptr;
-    typedef std::array<std::size_t, 2> pid2_type;
-    typedef std::array<std::size_t, 3> pid3_type;
-    typedef std::array<std::size_t, 4> pid4_type;
-  
+    typedef Particle<coordinate_type>             particle_type;
+    typedef ParticleContainer<traits_type>        particle_container_type;
+    typedef LocalPotentialBase<traits_type>       potential_base;
+    typedef std::unique_ptr<potential_base>       potential_ptr;
+
   public:
 
     LocalForceField() = default;
@@ -38,11 +35,13 @@ class LocalForceField
     LocalForceField& operator=(LocalForceField const&) = delete;
     LocalForceField& operator=(LocalForceField&&)      = default;
 
+    void initialize(const particle_container_type& pcon, const time_type dt);
+
     void emplace_bond(std::size_t i, std::size_t j, potential_ptr&& pot);
     void emplace_angle(std::size_t i, std::size_t j, std::size_t k,
-            potential_ptr&& pot);
-    void emplace_dihedral(std::size_t i, std::size_t j, std::size_t k, std::size_t l,
-            potential_ptr&& pot);
+                       potential_ptr&& pot);
+    void emplace_dihedral(std::size_t i, std::size_t j, std::size_t k,
+                          std::size_t l, potential_ptr&& pot);
 
     void      calc_force(particle_container_type& pcon);
     real_type calc_energy(const particle_container_type& pcon) const;
@@ -55,9 +54,11 @@ class LocalForceField
 
     struct bond_potential_type
     {
+        typedef LocalInteractionBase<traitsT, 2> interaction_type;
+        typedef typename interaction_type::particle_ptrs particle_ptrs;
         bond_potential_type() = default;
         bond_potential_type(std::size_t i_, std::size_t j_, potential_ptr&& pot_)
-           : i(i_), j(j_), pot(std::forward<potential_ptr>(pot_))
+           : indices{{i_, j_}}, pot(std::forward<potential_ptr>(pot_))
         {}
         ~bond_potential_type() = default;
         bond_potential_type(const bond_potential_type&) = delete;
@@ -65,16 +66,19 @@ class LocalForceField
         bond_potential_type& operator=(const bond_potential_type&) = delete;
         bond_potential_type& operator=(bond_potential_type&&)      = default;
 
-        std::size_t i, j;
-        potential_ptr pot;
+        std::array<std::size_t, 2> indices;
+        particle_ptrs              particles;
+        potential_ptr              pot;
     };
 
     struct angle_potential_type
     {
+        typedef LocalInteractionBase<traitsT, 3> interaction_type;
+        typedef typename interaction_type::particle_ptrs particle_ptrs;
         angle_potential_type() = default;
         angle_potential_type(std::size_t i_, std::size_t j_, std::size_t k_,
                              potential_ptr&& pot_)
-           : i(i_), j(j_), k(k_), pot(std::forward<potential_ptr>(pot_))
+           : indices{{i_, j_, k_}}, pot(std::forward<potential_ptr>(pot_))
         {}
         ~angle_potential_type() = default;
         angle_potential_type(const angle_potential_type&) = delete;
@@ -82,16 +86,19 @@ class LocalForceField
         angle_potential_type& operator=(const angle_potential_type&) = delete;
         angle_potential_type& operator=(angle_potential_type&&)      = default;
 
-        std::size_t i, j, k;
-        potential_ptr pot;
+        std::array<std::size_t, 3> indices;
+        particle_ptrs              particles;
+        potential_ptr              pot;
     };
 
     struct dihd_potential_type
     {
+        typedef LocalInteractionBase<traitsT, 4> interaction_type;
+        typedef typename interaction_type::particle_ptrs particle_ptrs;
         dihd_potential_type() = default;
         dihd_potential_type(std::size_t i_, std::size_t j_, std::size_t k_,
-                            std::size_t l_, potential_ptr&& pot_)
-           : i(i_), j(j_), k(k_), l(l_), pot(std::forward<potential_ptr>(pot_))
+                std::size_t l_, potential_ptr&& pot_)
+           : indices{{i_, j_, k_, l_}}, pot(std::forward<potential_ptr>(pot_))
         {}
         ~dihd_potential_type() = default;
         dihd_potential_type(const dihd_potential_type&) = delete;
@@ -99,8 +106,9 @@ class LocalForceField
         dihd_potential_type& operator=(const dihd_potential_type&) = delete;
         dihd_potential_type& operator=(dihd_potential_type&&)      = default;
 
-        std::size_t i, j, k, l;
-        potential_ptr pot;
+        std::array<std::size_t, 4> indices;
+        particle_ptrs              particles;
+        potential_ptr              pot;
     };
 
     std::vector<bond_potential_type>  bond_potentials;
@@ -112,7 +120,8 @@ template<typename traitsT>
 void LocalForceField<traitsT>::emplace_bond(
         std::size_t i, std::size_t j, potential_ptr&& pot)
 {
-    bond_potentials.emplace_back(i, j, std::forward<potential_ptr>(pot));
+    bond_potentials.emplace_back(
+            i, j, std::forward<potential_ptr>(pot));
     return ;
 }
 
@@ -120,16 +129,44 @@ template<typename traitsT>
 void LocalForceField<traitsT>::emplace_angle(
         std::size_t i, std::size_t j, std::size_t k, potential_ptr&& pot)
 {
-    angle_potentials.emplace_back(i, j, k, std::forward<potential_ptr>(pot));
+    angle_potentials.emplace_back(i, j, k,
+            std::forward<potential_ptr>(pot));
     return ;
 }
 
 template<typename traitsT>
 void LocalForceField<traitsT>::emplace_dihedral(
-        std::size_t i, std::size_t j, std::size_t k, std::size_t l, potential_ptr&& pot)
+        std::size_t i, std::size_t j, std::size_t k, std::size_t l,
+        potential_ptr&& pot)
 {
-    dihd_potentials.emplace_back(i, j, k, l, std::forward<potential_ptr>(pot));
+    dihd_potentials.emplace_back(i, j, k, l,
+            std::forward<potential_ptr>(pot));
     return ;
+}
+
+template<typename traitsT>
+void LocalForceField<traitsT>::initialize(
+        const particle_container_type& pcon, const time_type dt)
+{
+    for(auto iter = bond_potentials.begin(); iter != bond_potentials.end(); ++iter)
+    {
+        iter->particles[0] = const_cast<particle_type*>(&(pcon.at(iter->indices[0])));
+        iter->particles[1] = const_cast<particle_type*>(&(pcon.at(iter->indices[1])));
+    }
+    for(auto iter = angle_potentials.begin(); iter != angle_potentials.end(); ++iter)
+    {
+        iter->particles[0] = const_cast<particle_type*>(&(pcon.at(iter->indices[0])));
+        iter->particles[1] = const_cast<particle_type*>(&(pcon.at(iter->indices[1])));
+        iter->particles[2] = const_cast<particle_type*>(&(pcon.at(iter->indices[2])));
+    }
+    for(auto iter = dihd_potentials.begin(); iter != dihd_potentials.end(); ++iter)
+    {
+        iter->particles[0] = const_cast<particle_type*>(&(pcon.at(iter->indices[0])));
+        iter->particles[1] = const_cast<particle_type*>(&(pcon.at(iter->indices[1])));
+        iter->particles[2] = const_cast<particle_type*>(&(pcon.at(iter->indices[2])));
+        iter->particles[3] = const_cast<particle_type*>(&(pcon.at(iter->indices[3])));
+    }
+    return;
 }
 
 template<typename traitsT>
@@ -137,17 +174,15 @@ void LocalForceField<traitsT>::calc_force(particle_container_type& pcon)
 {
     for(auto iter = bond_potentials.cbegin();
             iter != bond_potentials.cend(); ++iter)
-        bond.calc_force(pcon[iter->i], pcon[iter->j], *(iter->pot));
+        bond.calc_force(iter->particles, *(iter->pot));
 
     for(auto iter = angle_potentials.cbegin();
             iter != angle_potentials.cend(); ++iter)
-        angle.calc_force(pcon[iter->i], pcon[iter->j], pcon[iter->k],
-                         *(iter->pot));
+        angle.calc_force(iter->particles, *(iter->pot));
 
     for(auto iter = dihd_potentials.cbegin();
             iter != dihd_potentials.cend(); ++iter)
-        dihd.calc_force(pcon[iter->i], pcon[iter->j], pcon[iter->k],
-                        pcon[iter->l], *(iter->pot));
+        dihd.calc_force(iter->particles, *(iter->pot));
 
     return;
 }
@@ -160,19 +195,15 @@ LocalForceField<traitsT>::calc_energy(const particle_container_type& pcon) const
 
     for(auto iter = bond_potentials.cbegin();
             iter != bond_potentials.cend(); ++iter)
-        energy += bond.calc_energy(
-                pcon[iter->i], pcon[iter->j], *(iter->pot));
+        energy += bond.calc_energy(iter->particles, *(iter->pot));
 
     for(auto iter = angle_potentials.cbegin();
             iter != angle_potentials.cend(); ++iter)
-        energy += angle.calc_energy(
-                pcon[iter->i], pcon[iter->j], pcon[iter->k], *(iter->pot));
+        energy += angle.calc_energy(iter->particles, *(iter->pot));
 
     for(auto iter = dihd_potentials.cbegin();
             iter != dihd_potentials.cend(); ++iter)
-        energy += dihd.calc_energy(
-                pcon[iter->i], pcon[iter->j], pcon[iter->k], pcon[iter->l],
-                *(iter->pot));
+        energy += dihd.calc_energy(iter->particles, *(iter->pot));
     return energy;
 }
 
