3d432dde1da8d472892497cf50fb73c9e6e6eece
ToruNiina <niina.toru.68u@gmail.com>
2017-02-08 12:59:25 +0900

    read boundary condition in time integrator

--- mjolnir/io/toml/read_time_integrator.hpp
+++ mjolnir/io/toml/read_time_integrator.hpp
@@ -11,6 +11,76 @@ namespace mjolnir
 
 template<typename traitsT>
 std::unique_ptr<Integrator<traitsT>>
+read_underdamped_langevin(const toml::Table& sim, const std::string boundary,
+        const std::shared_ptr<RandomNumberGenerator<traitsT>>& rng)
+{
+    MJOLNIR_SET_LOGGER("read_toml_file");
+    MJOLNIR_LOG_DEBUG("read_time_integrator CALLED");
+
+    const typename traitsT::real_type delta_t =
+        toml::get<toml::Float>(sim.at("delta_t"));
+    MJOLNIR_LOG_INFO("delta_t", delta_t);
+
+    const typename traitsT::real_type temperature =
+        toml::get<toml::Float>(sim.at("temperature"));
+    MJOLNIR_LOG_INFO("temperatrue", temperature);
+
+    const typename traitsT::real_type kB =
+        toml::get<toml::Float>(sim.at("kB"));
+    MJOLNIR_LOG_INFO("kB", kB);
+
+    const std::size_t num_particles =
+        toml::get<toml::Integer>(sim.at("number_of_particle"));
+    MJOLNIR_LOG_INFO("num_particles", num_particles);
+
+    std::vector<typename traitsT::real_type> fric =
+        toml::get<toml::Array<toml::Float>>(sim.at("friction_constant"));
+    MJOLNIR_LOG_INFO("fric.size()", fric.size());
+
+    if(num_particles != fric.size())
+        throw std::runtime_error("friction constant not enough");
+
+    if(boundary == "Unlimited")
+        return make_unique<UnderdampedLangevin<traitsT>>(
+            delta_t, num_particles, temperature, kB, std::move(fric), rng);
+    else if(boundary == "Periodic")
+        return make_unique<
+            UnderdampedLangevin<traitsT, PeriodicBoundaryXYZ<traitsT>>>(
+                delta_t, num_particles, temperature, kB, std::move(fric), rng);
+    else
+        throw std::runtime_error("unknown boundary condition: " + boundary);
+}
+
+template<typename traitsT>
+std::unique_ptr<Integrator<traitsT>>
+read_nve_newtonian(const toml::Table& sim, const std::string boundary)
+{
+    MJOLNIR_SET_LOGGER("read_toml_file");
+    MJOLNIR_LOG_DEBUG("read_time_integrator CALLED");
+
+    const typename traitsT::real_type delta_t =
+        toml::get<toml::Float>(sim.at("delta_t"));
+    MJOLNIR_LOG_INFO("delta_t", delta_t);
+
+    const std::size_t num_particles =
+        toml::get<toml::Integer>(sim.at("number_of_particle"));
+    MJOLNIR_LOG_INFO("num_particles", num_particles);
+
+    MJOLNIR_LOG_DEBUG("read_time_integrator RETURNED");
+
+    if(boundary == "Unlimited")
+        return make_unique<VelocityVerlet<traitsT>>(delta_t, num_particles);
+    else if(boundary == "Periodic")
+        return make_unique<
+            VelocityVerlet<traitsT, PeriodicBoundaryXYZ<traitsT>>>(
+                delta_t, num_particles);
+    else
+        throw std::runtime_error("unknown boundary condition: " + boundary);
+}
+
+
+template<typename traitsT>
+std::unique_ptr<Integrator<traitsT>>
 read_time_integrator(const toml::Table& sim,
         const std::shared_ptr<RandomNumberGenerator<traitsT>>& rng)
 {
@@ -21,47 +91,17 @@ read_time_integrator(const toml::Table& sim,
         toml::get<toml::String>(sim.at("time_integration"));
     MJOLNIR_LOG_INFO("time integration", integral);
 
+    const std::string boundary =
+        toml::get<toml::String>(sim.at("boundary"));
+    MJOLNIR_LOG_INFO("boundary condition", boundary);
+
     if(integral == "Underdamped Langevin")
     {
-        const typename traitsT::real_type delta_t =
-            toml::get<toml::Float>(sim.at("delta_t"));
-        MJOLNIR_LOG_INFO("delta_t", delta_t);
-
-        const typename traitsT::real_type temperature =
-            toml::get<toml::Float>(sim.at("temperature"));
-        MJOLNIR_LOG_INFO("temperatrue", temperature);
-
-        const typename traitsT::real_type kB =
-            toml::get<toml::Float>(sim.at("kB"));
-        MJOLNIR_LOG_INFO("kB", kB);
-
-        const std::size_t num_particles =
-            toml::get<toml::Integer>(sim.at("number_of_particle"));
-        MJOLNIR_LOG_INFO("num_particles", num_particles);
-
-        std::vector<typename traitsT::real_type> fric =
-            toml::get<toml::Array<toml::Float>>(sim.at("friction_constant"));
-        MJOLNIR_LOG_INFO("fric.size()", fric.size());
-
-        if(num_particles != fric.size())
-            throw std::runtime_error("friction constant not enough");
-
-        MJOLNIR_LOG_DEBUG("read_time_integrator RETURNED");
-        return make_unique<UnderdampedLangevin<traitsT>>(
-            delta_t, num_particles, temperature, kB, std::move(fric), rng);
+        return read_underdamped_langevin<traitsT>(sim, boundary, rng);
     }
     else if(integral == "NVE Newtonian")
     {
-        const typename traitsT::real_type delta_t =
-            toml::get<toml::Float>(sim.at("delta_t"));
-        MJOLNIR_LOG_INFO("delta_t", delta_t);
-
-        const std::size_t num_particles =
-            toml::get<toml::Integer>(sim.at("number_of_particle"));
-        MJOLNIR_LOG_INFO("num_particles", num_particles);
-
-        MJOLNIR_LOG_DEBUG("read_time_integrator RETURNED");
-        return make_unique<VelocityVerlet<traitsT>>(delta_t, num_particles);
+        return read_nve_newtonian<traitsT>(sim, boundary);
     }
     else
         throw std::invalid_argument("unknown time integral method: " + integral);
