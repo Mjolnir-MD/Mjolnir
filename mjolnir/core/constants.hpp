621a7e70693cff53b59a64abd366b50fad12a39d
ToruNiina <niina.toru.68u@gmail.com>
2017-02-15 16:12:15 +0900

    add physical constants to constatns.hpp

--- mjolnir/core/constants.hpp
+++ mjolnir/core/constants.hpp
@@ -8,8 +8,30 @@ template<typename traitsT>
 struct constants
 {
     typedef typename traitsT::real_type real_type;
-    constexpr static real_type tolerance = 1e-10; // XXX
+    constexpr static real_type tolerance = 1e-8;
+    constexpr static real_type pi        = 3.14159265358979323846264338;
 };
+
+template<typename traitsT>
+class physics
+{
+    typedef typename traitsT::real_type real_type;
+
+    // constexpr...?
+    static real_type kB;
+    static real_type NA;
+    static real_type e;
+    static real_type vacuum_permittivity;
+};
+
+template<typename traitsT>
+physics<traitsT>::real_type physics<traitsT>::kB = 1.986231313e-3;
+template<typename traitsT>
+physics<traitsT>::real_type physics<traitsT>::NA = 6.0221417930e23;
+template<typename traitsT>
+physics<traitsT>::real_type physics<traitsT>::e = 1.60217648740e-19;
+template<typename traitsT>
+physics<traitsT>::real_type physics<traitsT>::vacumm_permittivity = 8.854187817e-12;
 
 }//mjolnir
 #endif /* MJOLNIR_CONSTANTS */
