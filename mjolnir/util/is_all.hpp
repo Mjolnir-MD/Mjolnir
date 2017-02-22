c5e4ce2513bf46d6305750d510e13e5bed9da9f7
ToruNiina <niina.toru.68u@gmail.com>
2017-02-23 00:26:45 +0900

    refine is_all meta function

--- mjolnir/util/is_all.hpp
+++ mjolnir/util/is_all.hpp
@@ -1,23 +1,33 @@
 #ifndef MJOLNIR_IS_ALL
 #define MJOLNIR_IS_ALL
-#include <type_traits>
+#include "meta.hpp"
 
 namespace mjolnir
 {
 
-template<template<typename T1, typename T2> class T_condition,
-         typename T_ref, typename T_front, typename ... T_args>
-struct is_all
-    : std::integral_constant<bool, T_condition<T_ref, T_front>::value &&
-                                   is_all<T_condition, T_ref, T_args...>::value>
+template<template<typename...> class, typename ...>
+struct is_all;
+
+template<template<typename> class condT, typename frontT, typename ... Ts>
+struct is_all<condT, frontT, Ts...>
+    : public _and_<condT<frontT>, is_all<condT, Ts...>>::type
 {};
 
-template<template<typename T1, typename T2> class T_condition,
-         typename T_ref, typename T_last>
-struct is_all<T_condition, T_ref, T_last>
-    : std::integral_constant<bool, T_condition<T_ref, T_last>::value>
+template<template<typename> class condT, typename lastT>
+struct is_all<condT, lastT>
+    : public condT<lastT>::type
+{};
+
+template<template<typename, typename> class condT, typename refT,
+         typename frontT, typename ... Ts>
+struct is_all<condT, refT, frontT, Ts...>
+    : public _and_<condT<refT, frontT>, is_all<condT, refT, Ts...>>::type
+{};
+
+template<template<typename, typename> class condT, typename refT, typename lastT>
+struct is_all<condT, refT, lastT>
+    : public condT<refT, lastT>::type
 {};
 
 } // mjolnir
-
 #endif/* MJOLNIR_IS_ALL */
