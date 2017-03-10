16de65d2b561f084123ce4192f19f7b170376c1e
ToruNiina <niina.toru.68u@gmail.com>
2017-03-10 22:47:06 +0900

    add specialization for avx

---
+++ megingjord/simd/avx/pack_avx.hpp
@@ -0,0 +1,40 @@
+#ifndef MEGINGJORD_SIMD_PACK_AVX
+#define MEGINGJORD_SIMD_PACK_AVX
+
+#ifndef MEGINGJORD_SIMD_PACK
+#error "DO NOT USE megingjord/simd/pack_avx.hpp alone"
+#endif
+
+#ifndef MJOLNIR_HAVE_AVX
+#error "mjolnir detects no AVX on the architecture"
+#endif
+
+namespace megingjord
+{
+namespace simd
+{
+
+template<>
+struct pack<float, 8>
+{
+    typedef float  value_type;
+    typedef __m256 type;
+    typedef aligned_array<value_type, 8> array_type;
+    constexpr static std::size_t size       = 8;
+    constexpr static std::size_t align_byte = 32;
+};
+
+template<>
+struct pack<double, 4>
+{
+    typedef double  value_type;
+    typedef __m256d type;
+    typedef aligned_array<value_type, 4> array_type;
+    constexpr static std::size_t size       = 4;
+    constexpr static std::size_t align_byte = 32;
+};
+
+
+} // simd
+} // megingjord
+#endif /* MEGINGJORD_SIMD_PACK_AVX */
