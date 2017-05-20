b301553243a3b72312669c3da2f43f54f07657cd
ToruNiina <niina.toru.68u@gmail.com>
2017-05-20 15:30:42 +0900

    make BoundaryCondition non-static

--- mjolnir/core/BoundaryCondition.hpp
+++ mjolnir/core/BoundaryCondition.hpp
@@ -15,14 +15,12 @@ struct UnlimitedBoundary
     UnlimitedBoundary() = default;
     ~UnlimitedBoundary() = default;
 
-    static coordinate_type
-    adjust_direction(coordinate_type dr) {return dr;}
-    static coordinate_type
-    adjust_absolute(coordinate_type r) {return r;}
+    coordinate_type adjust_direction(coordinate_type dr) const {return dr;}
+    coordinate_type adjust_position (coordinate_type r ) const {return r;}
 };
 
 template<typename traitsT>
-struct PeriodicBoundaryXYZ
+struct CubicPeriodicBoundary
 {
   public:
     typedef traitsT traits_type;
@@ -30,76 +28,47 @@ struct PeriodicBoundaryXYZ
     typedef typename traits_type::coordinate_type coordinate_type;
 
   public:
-    PeriodicBoundaryXYZ() = default;
-    ~PeriodicBoundaryXYZ() = default;
+    CubicPeriodicBoundary() = default;
+    ~CubicPeriodicBoundary() = default;
+    CubicPeriodicBoundary(const coordinate_type& lw, const coordinate_type& up)
+    : lower(lw), upper(up), system_size(up-lw), system_size_half(0.5*(up-lw))
+    {}
 
-    static void
-    set_system(const coordinate_type& lower, const coordinate_type upper)
+    coordinate_type adjust_direction(coordinate_type dr) const
     {
-        lower_         = lower;
-        upper_         = upper;
-        sys_size_      = upper_ - lower_;
-        sys_size_half_ = 0.5 * sys_size_;
-
-        assert(sys_size_[0] > 0.0);
-        assert(sys_size_[1] > 0.0);
-        assert(sys_size_[2] > 0.0);
-
-        assert(sys_size_half_[0] > 0.0);
-        assert(sys_size_half_[1] > 0.0);
-        assert(sys_size_half_[2] > 0.0);
-        return;
-    }
-
-    static coordinate_type
-    adjust_direction(coordinate_type dr)
-    {
-             if(dr[0] < -sys_size_half_[0]) dr[0] += sys_size_[0];
-        else if(dr[0] >  sys_size_half_[0]) dr[0] -= sys_size_[0];
-             if(dr[1] < -sys_size_half_[1]) dr[1] += sys_size_[1];
-        else if(dr[1] >  sys_size_half_[1]) dr[1] -= sys_size_[1];
-             if(dr[2] < -sys_size_half_[2]) dr[2] += sys_size_[2];
-        else if(dr[2] >  sys_size_half_[2]) dr[2] -= sys_size_[2];
+             if(dr[0] < -system_size_half[0]) dr[0] += system_size[0];
+        else if(dr[0] >  system_size_half[0]) dr[0] -= system_size[0];
+             if(dr[1] < -system_size_half[1]) dr[1] += system_size[1];
+        else if(dr[1] >  system_size_half[1]) dr[1] -= system_size[1];
+             if(dr[2] < -system_size_half[2]) dr[2] += system_size[2];
+        else if(dr[2] >  system_size_half[2]) dr[2] -= system_size[2];
         return dr;
     }
 
-    static coordinate_type
-    adjust_absolute(coordinate_type pos)
+    coordinate_type adjust_position(coordinate_type pos) const
     {
-             if(pos[0] < lower_[0]) pos[0] += sys_size_[0];
-        else if(pos[0] > upper_[0]) pos[0] -= sys_size_[0];
-             if(pos[1] < lower_[1]) pos[1] += sys_size_[1];
-        else if(pos[1] > upper_[1]) pos[1] -= sys_size_[1];
-             if(pos[2] < lower_[2]) pos[2] += sys_size_[2];
-        else if(pos[2] > upper_[2]) pos[2] -= sys_size_[2];
+             if(pos[0] < lower[0]) pos[0] += system_size_[0];
+        else if(pos[0] > upper[0]) pos[0] -= system_size_[0];
+             if(pos[1] < lower[1]) pos[1] += system_size_[1];
+        else if(pos[1] > upper[1]) pos[1] -= system_size_[1];
+             if(pos[2] < lower[2]) pos[2] += system_size_[2];
+        else if(pos[2] > upper[2]) pos[2] -= system_size_[2];
         return pos;
     }
 
-    static coordinate_type const& lower_bound() {return lower_;}
-    static coordinate_type const& upper_bound() {return upper_;}
-    static coordinate_type const& system_size() {return sys_size_;}
+    coordinate_type&       lower_bound()       {return lower;}
+    coordinate_type const& lower_bound() const {return lower;}
+    coordinate_type&       upper_bound()       {return upper;}
+    coordinate_type const& upper_bound() const {return upper;}
+    coordinate_type const& range() const {return system_size;}
 
   private:
 
-    static coordinate_type lower_;
-    static coordinate_type upper_;
-    static coordinate_type sys_size_;
-    static coordinate_type sys_size_half_;
+    coordinate_type lower;
+    coordinate_type upper;
+    coordinate_type system_size;
+    coordinate_type system_size_half;
 };
-
-template<typename traitsT>
-typename PeriodicBoundaryXYZ<traitsT>::coordinate_type
-PeriodicBoundaryXYZ<traitsT>::lower_;
-template<typename traitsT>
-typename PeriodicBoundaryXYZ<traitsT>::coordinate_type
-PeriodicBoundaryXYZ<traitsT>::upper_;
-template<typename traitsT>
-typename PeriodicBoundaryXYZ<traitsT>::coordinate_type
-PeriodicBoundaryXYZ<traitsT>::sys_size_;
-template<typename traitsT>
-typename PeriodicBoundaryXYZ<traitsT>::coordinate_type
-PeriodicBoundaryXYZ<traitsT>::sys_size_half_;
-
 
 }//mjolnir
 #endif /* MJOLNIR_BOUNDARY_CONDITION */
