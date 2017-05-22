b2c01d12b6cf7443d1525cb173db09d86da15500
ToruNiina <niina.toru.68u@gmail.com>
2017-05-22 19:22:09 +0900

    use system & observer_ptr

--- mjolnir/core/UnderdampedLangevin.hpp
+++ mjolnir/core/UnderdampedLangevin.hpp
@@ -1,97 +1,104 @@
 #ifndef MJOLNIR_UNDERDAMPED_LANGEVIN_INTEGRATOR
 #define MJOLNIR_UNDERDAMPED_LANGEVIN_INTEGRATOR
-#include "Integrator.hpp"
-#include "BoundaryCondition.hpp"
+#include "constants.hpp"
 #include "RandomNumberGenerator.hpp"
+#include <mjolnir/util/observer_ptr.hpp>
 #include <mjolnir/util/zip_iterator.hpp>
 #include <mjolnir/util/make_zip.hpp>
-#include <memory>
 
 namespace mjolnir
 {
 
-template<typename traitsT, typename boundaryT = UnlimitedBoundary<traitsT>>
-class UnderdampedLangevin : public Integrator<traitsT>
+template<typename traitsT>
+class UnderdampedLangevinStepper
 {
   public:
-    typedef traitsT   traits_type;
-    typedef boundaryT boundary_type;
-    typedef typename traits_type::time_type time_type;
-    typedef typename traits_type::real_type real_type;
+    typedef traitsT traits_type;
+    typedef System<traits_type>     system_type;
+    typedef ForceField<traits_type> forcefield_type;
+    typedef observer_ptr<RandomNumberGenerator<traits_type>> rng_type;
+    typedef typename traits_type::boundary_type   boundary_type;
+    typedef typename traits_type::real_type       real_type;
     typedef typename traits_type::coordinate_type coordinate_type;
 
   public:
 
-    UnderdampedLangevin(const time_type dt, const std::size_t number_of_particles,
-            const real_type temperature, const real_type kB,
-            std::vector<real_type>&& friction_constant,
-            const std::shared_ptr<RandomNumberGenerator<traits_type>>& rng)
-        : dt_(dt), halfdt_(dt * 0.5), halfdt2_(dt * dt * 0.5), rng_(rng),
-          temperature_(temperature), kB_(kB),
-          gamma_(std::forward<std::vector<real_type>>(friction_constant)),
-          noise_(number_of_particles), acceleration_(number_of_particles)
+    UnderdampedLangevinStepper(const time_type dt,
+        std::vector<real_type>&& gamma, RandomNumberGenerator<traitsT>& rng)
+        : dt_(dt), halfdt_(dt * 0.5), halfdt2_(dt * dt * 0.5), rng_(&rng),
+          gamma_(std::move(gamma)), noise_(gamma_.size()), accel_(gamma_.size())
     {}
-    ~UnderdampedLangevin() override = default;
+    ~UnderdampedLangevinStepper() = default;
 
-    void initialize(const ParticleContainer<traitsT>& pcon) override;
+    void initialize(const system_type& pcon);
+    real_type step(const real_type time, system_type& sys, forcefield_type& ff);
 
-    time_type step(const time_type time, ParticleContainer<traitsT>& pcon,
-                   ForceField<traitsT>& ff) override;
-
-    real_type  T() const {return temperature_;}
-    real_type& T()       {return temperature_;}
-
-    time_type  delta_t() const override {return dt_;}
-    time_type& delta_t()       override {return dt_;}
+    time_type delta_t() const noexcept {return dt_;}
+    void  set_delta_t(const real_type dt) noexcept
+    {
+        dt_ = dt; halfdt_ = dt * 0.5; halfdt2_ = dt*dt*0.5;
+    }
 
   private:
-    time_type dt_;      //!< dt
-    time_type halfdt_;  //!< dt/2
-    time_type halfdt2_; //!< dt^2/2
-    real_type kB_;
-    real_type temperature_;
-    std::shared_ptr<RandomNumberGenerator<traits_type>> rng_;
-    std::vector<real_type> gamma_;
+
+    coordinate_type gen_random_accel(const real_type m, const real_type g)
+    {
+        const real_type coef = this->noise_coef_ * std::sqrt(g / m);
+        return coordinate_type(this->rng_->gaussian(0., coef),
+                               this->rng_->gaussian(0., coef),
+                               this->rng_->gaussian(0., coef));
+    }
+
+  private:
+    real_type dt_;
+    real_type halfdt_;
+    real_type halfdt2_;
+    real_type noise_coef_;
+    rng_type  rng_;
+    std::vector<real_type>       gamma_;
     std::vector<coordinate_type> noise_;
-    std::vector<coordinate_type> acceleration_;
+    std::vector<coordinate_type> accel_;
 };
 
-template<typename traitsT, typename boundaryT>
-void UnderdampedLangevin<traitsT, boundaryT>::initialize(
-        const ParticleContainer<traitsT>& pcon)
+template<typename traitsT, typename randomT>
+void UnderdampedLangevinStepper<traitsT, randomT>::initialize(
+        const system_type& system)
 {
-    for(auto iter = make_zip(pcon.cbegin(),  gamma_.cbegin(),
-                             noise_.begin(), acceleration_.begin());
-            iter != make_zip(pcon.cend(),    gamma_.cend(),
-                             noise_.end(),   acceleration_.end()); ++iter)
-    {
-        // set a = f/m
-        *get<3>(iter) = get<0>(iter)->force / get<0>(iter)->mass;
+    this->noise_coef =
+        std::sqrt(2 * physics<real_type>::kB * sys.temperature() / dt_);
 
-        // set random force
-        *get<2>(iter) = rng_->underdamped_langevin(
-                get<0>(iter)->mass, *get<1>(iter), dt_, temperature_, kB_);
+    for(auto iter = make_zip(system.cbegin(), gamma_.cbegin(),
+                             noise_.begin(),  accel_.begin());
+            iter != make_zip(system.cend(),   gamma_.cend(),
+                             noise_.end(),    accel_.end()); ++iter)
+    {
+        *get<3>(iter) = get<0>(iter)->force / get<0>(iter)->mass;
+        *get<2>(iter) = gen_random_accel(get<0>(iter)->mass,
+                            /*gamma = */*get<1>(iter));
     }
 
     return;
+
 }
 
-// at the initial step, acceleration_ and noise_ must be initialized
-template<typename traitsT, typename boundaryT>
-typename UnderdampedLangevin<traitsT, boundaryT>::time_type
-UnderdampedLangevin<traitsT, boundaryT>::step(const time_type time,
-        ParticleContainer<traitsT>& pcon, ForceField<traitsT>& ff)
+template<typename traitsT, typename randomT>
+typename UnderdampedLangevinStepper<traitsT, randomT>::real_type
+UnderdampedLangevinStepper<traitsT, randomT>::step(
+        const real_type time, system_type& sys, forcefield_type& ff)
 {
-    // calc r(t+dt)
-    for(auto iter = make_zip(pcon.begin(),    gamma_.cbegin(),
-                             noise_.cbegin(), acceleration_.cbegin());
-            iter != make_zip(pcon.end(),      gamma_.cend(),
-                             noise_.cend(),   acceleration_.cend()); ++iter)
+    this->noise_coef =
+        std::sqrt(2 * physics<real_type>::kB * sys.temperature() / dt_);
+
+    for(auto iter(make_zip(sys.begin(),     gamma_.cbegin(),
+                           noise_.cbegin(), accel_.cbegin())),
+             end_(make_zip(sys.end(),       gamma_.cend(),
+                           noise_.cend(),   accel_.cend())),
+            iter != end_; ++iter)
     {
         const real_type hgdt   = (*get<1>(iter) * halfdt_);
         const real_type o_hgdt = 1. - hgdt;
 
-        const coordinate_type noisy_force = ((*get<2>(iter)) + (*get<3>(iter)));
+        const coordinate_type noisy_force = ((*get<2>(iter)) + (*get<3>(iter)) / m);
 
         get<0>(iter)->position = boundary_type::adjust_absolute(
                 get<0>(iter)->position +
@@ -106,17 +113,18 @@ UnderdampedLangevin<traitsT, boundaryT>::step(const time_type time,
     ff.calc_force(pcon);
 
     // calc a(t+dt) and v(t+dt), generate noise
-    for(auto iter = make_zip(pcon.begin(),   gamma_.cbegin(),
-                             noise_.begin(), acceleration_.begin());
-            iter != make_zip(pcon.end(),     gamma_.cend(),
-                             noise_.end(),   acceleration_.end()); ++iter)
+    for(auto iter(make_zip(sys.begin(),    gamma_.cbegin(),
+                           noise_.begin(), accel_.begin()));
+             end_(make_zip(sys.end(),      gamma_.cend(),
+                           noise_.end(),   accel_.end()));
+            iter != end_; ++iter)
     {
         // consider cash 1/m
         const coordinate_type acc = get<0>(iter)->force / (get<0>(iter)->mass);
         *get<3>(iter) = acc;
 
-        const coordinate_type noise = rng_->underdamped_langevin(
-                get<0>(iter)->mass, *get<1>(iter), dt_, temperature_, kB_);
+        const coordinate_type noise =
+            this->gen_random_accel(get<0>(iter)->mass, *get<1>(iter));
         *get<2>(iter) = noise;
 
         const real_type gm = (*get<1>(iter));
@@ -127,6 +135,7 @@ UnderdampedLangevin<traitsT, boundaryT>::step(const time_type time,
 
     return time + dt_;
 }
+
 
 } // mjolnir
 #endif /* MJOLNIR_UNDERDAMPED_LANGEVIN_INTEGRATOR */
