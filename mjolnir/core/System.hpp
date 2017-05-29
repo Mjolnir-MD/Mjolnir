3df21d487671ae83e596d494141847144ba7d90e
ToruNiina <niina.toru.68u@gmail.com>
2017-05-29 17:45:43 +0900

    fix typename and add const begin/end to System

--- mjolnir/core/System.hpp
+++ mjolnir/core/System.hpp
@@ -11,18 +11,19 @@ class System
 {
   public:
     typedef traitsT  traits_type;
+    typedef typename traits_type::real_type         real_type;
     typedef typename traits_type::coordinate_type   coordinate_type;
+    typedef typename traits_type::boundary_type     boundary_type;
     typedef Particle<coordinate_type>               particle_type;
     typedef std::vector<particle_type>              container_type;
     typedef typename container_type::iterator       iterator;
     typedef typename container_type::const_iterator const_iterator;
-    typedef typename traits_type::boundary_type     boundary_type;
 
     System()  = default;
     ~System() = default;
 
     System(container_type&& pcon, boundary_type&& bound)
-        : boundary_(bound), particles(pcon), temperature_(0.), pressure_(0.),
+        : boundary_(bound), particles_(pcon), temperature_(0.), pressure_(0.),
           ionic_strength_(0.)
     {}
 
@@ -35,9 +36,9 @@ class System
     real_type& ionic_strength()       noexcept {return ionic_strength_;}
     real_type  ionic_strength() const noexcept {return ionic_strength_;}
 
-    coordinate_type adjust_direction(coordiate_type dr) const noexcept
+    coordinate_type adjust_direction(coordinate_type dr) const noexcept
     {return boundary_.adjust_direction(dr);}
-    coordinate_type  adjust_position(coordiate_type dr) const noexcept
+    coordinate_type  adjust_position(coordinate_type dr) const noexcept
     {return boundary_.adjust_position(dr);}
 
     particle_type &      operator[](std::size_t i)       noexcept
@@ -51,6 +52,8 @@ class System
 
     iterator       begin()        noexcept {return particles_.begin();}
     iterator       end()          noexcept {return particles_.end();}
+    const_iterator begin()  const noexcept {return particles_.cbegin();}
+    const_iterator end()    const noexcept {return particles_.cend();}
     const_iterator cbegin() const noexcept {return particles_.cbegin();}
     const_iterator cend()   const noexcept {return particles_.cend();}
 
