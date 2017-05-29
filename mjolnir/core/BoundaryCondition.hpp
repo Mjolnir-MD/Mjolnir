b0803c7e5a18a102fc7ddf2b4526fdf599267e92
ToruNiina <niina.toru.68u@gmail.com>
2017-05-29 16:47:28 +0900

    fix typo

--- mjolnir/core/BoundaryCondition.hpp
+++ mjolnir/core/BoundaryCondition.hpp
@@ -23,23 +23,24 @@ struct CubicPeriodicBoundary
 {
   public:
     typedef realT  real_type;
-    typedef coordT coordiante_type;
+    typedef coordT coordinate_type;
 
   public:
     CubicPeriodicBoundary() = default;
     ~CubicPeriodicBoundary() = default;
-    CubicPeriodicBoundary(const coordinate_type& lw, const coordinate_type& up) noexcept
-    : lower(lw), upper(up), system_size(up-lw), system_size_half(0.5*(up-lw))
+    CubicPeriodicBoundary(
+            const coordinate_type& lw, const coordinate_type& up) noexcept
+    : lower(lw), upper(up), system_size_(up-lw), system_size_half(0.5*(up-lw))
     {}
 
     coordinate_type adjust_direction(coordinate_type dr) const noexcept
     {
-             if(dr[0] < -system_size_half[0]) dr[0] += system_size[0];
-        else if(dr[0] >  system_size_half[0]) dr[0] -= system_size[0];
-             if(dr[1] < -system_size_half[1]) dr[1] += system_size[1];
-        else if(dr[1] >  system_size_half[1]) dr[1] -= system_size[1];
-             if(dr[2] < -system_size_half[2]) dr[2] += system_size[2];
-        else if(dr[2] >  system_size_half[2]) dr[2] -= system_size[2];
+             if(dr[0] < -system_size_half[0]) dr[0] += system_size_[0];
+        else if(dr[0] >  system_size_half[0]) dr[0] -= system_size_[0];
+             if(dr[1] < -system_size_half[1]) dr[1] += system_size_[1];
+        else if(dr[1] >  system_size_half[1]) dr[1] -= system_size_[1];
+             if(dr[2] < -system_size_half[2]) dr[2] += system_size_[2];
+        else if(dr[2] >  system_size_half[2]) dr[2] -= system_size_[2];
         return dr;
     }
 
@@ -58,13 +59,13 @@ struct CubicPeriodicBoundary
     coordinate_type const& lower_bound() const noexcept {return lower;}
     coordinate_type&       upper_bound()       noexcept {return upper;}
     coordinate_type const& upper_bound() const noexcept {return upper;}
-    coordinate_type const& range() const noexcept {return system_size;}
+    coordinate_type const& range() const noexcept {return system_size_;}
 
   private:
 
     coordinate_type lower;
     coordinate_type upper;
-    coordinate_type system_size;
+    coordinate_type system_size_;
     coordinate_type system_size_half;
 };
 
