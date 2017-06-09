9209a91e9146bf9a10b6b54d28bd6c36a07789d6
yutaka <p7dea87c7e4n7j@gmail.com>
2017-06-09 20:40:13 +0900

    add Spatial partition for mock up.

---
+++ mjolnir/core/SpatialPartitionForMU.hpp
@@ -0,0 +1,32 @@
+#ifndef MJOLNIR_CORE_SPATIAL_PARTITION_FOR_MU
+#define MJOLNIR_CORE_SPATIAL_PARTITION_FOR_MU
+#include "System.hpp"
+#include <algorithm>
+#include <limits>
+
+namespace mjolnir
+{
+namespace mockup
+{
+template<typename traitsT>
+class SpatialPartitionForMU
+{
+  public:
+    typedef traitsT traits_type;
+    typedef System<traits_type> system_type;
+    typedef typename traits_type::real_type       real_type;
+    typedef typename traits_type::coordinate_type coordinate_type;
+    typedef std::vector<std::size_t> index_array;
+    
+  public:
+
+    SpatialPartitionForMU();
+    ~SpatialPartitionForMU();
+
+    
+	
+}    
+}//end namspace mockup.
+    
+}//end namespace mjolnir
+#endif// MJOLNIR_CORE_SPATIAL_PARTITION_FOR_MU
