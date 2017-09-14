e9984ec9cdbccccd8c9f1cba21321afb12692692
yutaka <p7dea87c7e4n7j@gmail.com>
2017-09-14 21:50:19 +0900

    make spartial partition for Implicit membrane potential.

---
+++ mjolnir/core/ImplicitMembraneList.hpp
@@ -0,0 +1,54 @@
+#ifndef MJOLNIR_IMPLICIT_MEMBRANE_LIST
+#define MJOLNIR_IMPLICIT_MEMBRANE_LIST
+
+#include <cmath>
+
+namespace mjolnir
+{
+template<typename traitT>
+class ImplicitMembraneList
+{
+  public:
+    typedef traitT traits_type;
+    typedef System<traits_type> system_type;
+    typedef typename traits_type::real_type real_type;
+    typedef typename traits_type::coordinate_type coordinate_type;
+    typedef typename std::vector<std::size_t> obj_index_array;
+    
+    ImplicitMembraneList() = default;
+    ~ImplicitMembraneList() = default;
+
+    ImplicitMembraneList(const real_type cutoff):cutoff_(cutoff){}
+    
+    void initialize(const system_type& sys);
+    void update(const system_type& sys);
+    obj_index_array const& obj_indices()const noexcept {return obj_indices_;}
+    
+  private:
+
+    real_type cutoff_;
+    obj_index_array obj_indices_;
+};
+
+template<typename traitT>
+void ImplicitMembraneList<traitT>::initialize(const system_type& sys)
+{
+    for(std::size_t particle_idx = 0;  particle_idx < sys.size(); ++particle_idx)
+    {
+	if(abs(sys.at(particle_idx).position.at(2)) < cutoff_)
+	    obj_indices_.push_back(particle_idx);
+    }
+    return;
+}
+
+template<typename traitT>
+void ImplicitMembraneList<traitT>::update(const system_type& sys)
+{
+    obj_indices_.clear();
+    initialize(sys);
+    return;
+}
+
+}
+
+#endif /* MJOLNIR_IMPLICIT_MEMBRANE_LIST */
