1855dddc6e5589dcb09188182dff626b7cd447e3
ToruNiina <niina.toru.68u@gmail.com>
2017-10-26 15:15:08 +0900

    add operator""_str to jarngreipr/util/string

--- jarngreipr/util/string.hpp
+++ jarngreipr/util/string.hpp
@@ -1,23 +1,28 @@
 #ifndef JARNGREIPR_STRING
 #define JARNGREIPR_STRING
+#include <iterator>
 #include <string>
+#include <algorithm>
 #include <cctype>
 
-namespace jarngreipr
+namespace mjolnir
 {
 
 template<typename charT, typename traitsT, typename allocT>
 std::basic_string<charT, traitsT, allocT>
 remove_whitespace(const std::basic_string<charT, traitsT, allocT>& str)
 {
-    std::basic_string<charT, traitsT, allocT> retval;
-    for(auto iter = str.cbegin(); iter != str.cend(); ++iter)
-    {
-        if(!std::isspace(*iter))
-            retval.push_back(*iter);
-    }
+    std::basic_string<charT, traitsT, allocT> retval; retval.reserve(str.size());
+    std::copy_if(str.cbegin(), str.cend(), std::back_inserter(retval),
+                 [](const charT c){return !(std::isspace(c));});
     return retval;
 }
 
-} // jarngreipr
+// this is re-definition of C++14 std::literals::string_literals::operator""s.
+inline std::string operator"" _str(const char* str, std::size_t len)
+{
+    return std::string{str, len};
+}
+
+} // mjolnir
 #endif /*JARNGREIPR_STRING*/
