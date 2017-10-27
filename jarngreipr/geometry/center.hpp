25e2719454e2a91d7842a4041aeae7d82daa99db
ToruNiina <niina.toru.68u@gmail.com>
2017-10-27 11:20:52 +0900

    add center_if(container, predicate)

--- jarngreipr/geometry/center.hpp
+++ jarngreipr/geometry/center.hpp
@@ -16,7 +16,7 @@ inline coordT center(const Bead<coordT>& bead)
 }
 
 template<typename coordT>
-inline coordT center(const std::vector<Bead<coordT>>& beads)
+coordT center(const std::vector<Bead<coordT>>& beads)
 {
     typedef typename scalar_type_of<coordT>::type real_type;
     coordT pos(0,0,0);
@@ -24,8 +24,27 @@ inline coordT center(const std::vector<Bead<coordT>>& beads)
     {
         pos += bead.position();
     }
-    return pos / static_cast<real_type>(atoms.size());
+    return pos / static_cast<real_type>(beads.size());
 }
+
+template<typename coordT, typename UnaryPredicate>
+coordT center_if(const std::vector<Bead<coordT>>& beads,
+                 UnaryPredicate&& satisfy)
+{
+    typedef typename scalar_type_of<coordT>::type real_type;
+    std::size_t N = 0;
+    coordT pos(0,0,0);
+    for(const auto& bead : beads)
+    {
+        if(satisfy(bead))
+        {
+            pos += bead.position();
+            ++N;
+        }
+    }
+    return pos / static_cast<real_type>(N);
+}
+
 
 template<typename coordT>
 inline coordT center(const PDBAtom<coordT>& atom) noexcept
@@ -34,7 +53,7 @@ inline coordT center(const PDBAtom<coordT>& atom) noexcept
 }
 
 template<typename coordT>
-inline coordT center(const std::vector<PDBAtom<coordT>>& atoms) noexcept
+coordT center(const std::vector<PDBAtom<coordT>>& atoms) noexcept
 {
     typedef typename scalar_type_of<coordT>::type real_type;
     coordT pos(0,0,0);
@@ -45,8 +64,26 @@ inline coordT center(const std::vector<PDBAtom<coordT>>& atoms) noexcept
     return pos / static_cast<real_type>(atoms.size());
 }
 
+template<typename coordT, typename UnaryPredicate>
+coordT center_if(const std::vector<PDBAtom<coordT>>& atoms,
+                 UnaryPredicate&& satisfy)
+{
+    typedef typename scalar_type_of<coordT>::type real_type;
+    std::size_t N = 0;
+    coordT pos(0,0,0);
+    for(const auto& atom : atoms)
+    {
+        if(satisfy(atom))
+        {
+            pos += atom.position;
+            ++N;
+        }
+    }
+    return pos / static_cast<real_type>(N);
+}
+
 template<typename coordT>
-inline coordT center(const PDBResidue<coordT>& residue) noexcept
+coordT center(const PDBResidue<coordT>& residue) noexcept
 {
     typedef typename scalar_type_of<coordT>::type real_type;
     coordT pos(0,0,0);
@@ -57,8 +94,26 @@ inline coordT center(const PDBResidue<coordT>& residue) noexcept
     return pos / static_cast<real_type>(residue.size());
 }
 
+template<typename coordT, typename UnaryPredicate>
+coordT center_if(const PDBResidue<coordT>& residue, UnaryPredicate&& satisfy)
+{
+    typedef typename scalar_type_of<coordT>::type real_type;
+    std::size_t N = 0;
+    coordT pos(0,0,0);
+    for(const auto& atom : residue)
+    {
+        if(satisfy(atom))
+        {
+            pos += atom.position;
+            ++N;
+        }
+    }
+    return pos / static_cast<real_type>(N);
+}
+
+
 template<typename coordT>
-inline coordT center(const PDBChain<coordT>& chain) noexcept
+coordT center(const PDBChain<coordT>& chain) noexcept
 {
     typedef typename scalar_type_of<coordT>::type real_type;
     std::size_t num_p = 0;
@@ -69,6 +124,26 @@ inline coordT center(const PDBChain<coordT>& chain) noexcept
         {
             pos += atom.position;
             ++num_p;
+        }
+    }
+    return pos / static_cast<real_type>(num_p);
+}
+
+template<typename coordT, typename UnaryPredicate>
+coordT center_if(const PDBChain<coordT>& chain, UnaryPredicate&& satisfy)
+{
+    typedef typename scalar_type_of<coordT>::type real_type;
+    std::size_t num_p = 0;
+    coordT pos(0,0,0);
+    for(const auto& residue : chain)
+    {
+        for(const auto& atom : residue)
+        {
+            if(satisfy(atom))
+            {
+                pos += atom.position;
+                ++num_p;
+            }
         }
     }
     return pos / static_cast<real_type>(num_p);
