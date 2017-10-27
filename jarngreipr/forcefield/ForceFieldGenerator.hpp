b5108df3455a99c3c8aab9a8f0eaa10d25cc4544
ToruNiina <niina.toru.68u@gmail.com>
2017-10-27 20:14:34 +0900

    add (Intra|Inter)ChainForceFieldGenerator

--- jarngreipr/forcefield/ForceFieldGenerator.hpp
+++ jarngreipr/forcefield/ForceFieldGenerator.hpp
@@ -13,7 +13,7 @@ namespace mjolnir
 {
 
 template<typename coordT>
-class ForceFieldGenerator
+class IntraChainForceFieldGenerator
 {
   public:
     typedef coordT                      coordinate_type;
@@ -25,12 +25,41 @@ class ForceFieldGenerator
     typedef std::map<std::size_t, ConnectingIndices> connection_info;
 
   public:
-    virtual ~ForceFieldGenerator() = default;
+    virtual ~IntraChainForceFieldGenerator() = default;
 
     //!@brief generate parameter values and write out to ostream.
-    //@return return connection info. for scaling or ignoring Lennard-Jones.
+    //@return connection_information. for scaling or ignoring Global interaction
     virtual connection_info
-    generate(std::ostream& ostrm, const cg_chain_type& chain) const = 0;
+    generate(std::ostream& ostrm,
+             const cg_chain_type& chain, const std::size_t offset) const = 0;
+
+    //!@brief if chain contains invalid bead, return false.
+    virtual bool
+    check_bead_kind(const cg_chain_type& chain) const = 0;
+};
+
+// mainly for GoContact.
+template<typename coordT>
+class InterChainForceFieldGenerator
+{
+  public:
+    typedef coordT                      coordinate_type;
+    typedef PDBAtom<coordinate_type>    atom_type;
+    typedef PDBResidue<coordinate_type> residue_type;
+    typedef PDBChain<coordinate_type>   chain_type;
+    typedef Bead<coordinate_type>       bead_type;
+    typedef CGChain<coordinate_type>    cg_chain_type;
+    typedef std::map<std::size_t, ConnectingIndices> connection_info;
+
+  public:
+    virtual ~IntraChainForceFieldGenerator() = default;
+
+    //!@brief generate parameter values and write out to ostream.
+    //@return connection_information. for scaling or ignoring Global interaction
+    virtual connection_info
+    generate(std::ostream& ostrm,
+        const std::vector<std::pair<cg_chain_type, std::size_t>>& chain_offset
+        ) const = 0;
 
     //!@brief if chain contains invalid bead, return false.
     virtual bool
