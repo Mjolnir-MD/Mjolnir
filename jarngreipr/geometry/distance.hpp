ab5b87cbde0b4f4b5c0151d62d022d5ec100f026
ToruNiina <niina.toru.68u@gmail.com>
2017-10-27 11:56:39 +0900

    refactor geometry/distance.hpp

--- jarngreipr/geometry/distance.hpp
+++ jarngreipr/geometry/distance.hpp
@@ -1,6 +1,7 @@
 #ifndef JARNGREIPR_GEOMETRY_DISTANCE
 #define JARNGREIPR_GEOMETRY_DISTANCE
 #include <jarngreipr/model/Bead.hpp>
+#include <jarngreipr/model/CGChain.hpp>
 #include <jarngreipr/io/PDBAtom.hpp>
 #include <jarngreipr/io/PDBResidue.hpp>
 #include <jarngreipr/io/PDBChain.hpp>
@@ -11,14 +12,16 @@ namespace mjolnir
 
 template<typename coordT>
 inline typename scalar_type_of<coordT>::type
-distance_sq(const Bead<coordT>& lhs, const Bead<coordT>& rhs) noexcept
+distance_sq(const std::unique_ptr<Bead<coordT>>& lhs,
+            const std::unique_ptr<Bead<coordT>>& rhs) noexcept
 {
-    return distance_sq(lhs.position(), rhs.position());
+    return distance_sq(lhs->position(), rhs->position());
 }
 
 template<typename coordT>
 inline typename scalar_type_of<coordT>::type
-distance(const Bead<coordT>& lhs, const Bead<coordT>& rhs) noexcept
+distance(const std::unique_ptr<Bead<coordT>>& lhs,
+         const std::unique_ptr<Bead<coordT>>& rhs) noexcept
 {
     return std::sqrt(distance_sq(lhs, rhs));
 }
@@ -39,21 +42,54 @@ distance(const PDBAtom<coordT>& lhs, const PDBAtom<coordT>& rhs) noexcept
 
 /* ------------------------------ min_distance ------------------------------ */
 
-// TODO: refactoring (according to DRY)
+// preparation for generics...
 
 template<typename coordT>
-typename scalar_type_of<coordT>::type
-min_distance_sq(const std::vector<PDBAtom<coordT>>& lhs,
-                const std::vector<PDBAtom<coordT>>& rhs)
+inline typename scalar_type_of<coordT>::type
+min_distance_sq(const std::unique_ptr<Bead<coordT>>& lhs,
+                const std::unique_ptr<Bead<coordT>>& rhs) noexcept
 {
-    typedef typename scalar_type_of<coordT>::type real_type;
+    return distance_sq(lhs->position(), rhs->position());
+}
+
+template<typename coordT>
+inline typename scalar_type_of<coordT>::type
+min_distance(const std::unique_ptr<Bead<coordT>>& lhs,
+             const std::unique_ptr<Bead<coordT>>& rhs) noexcept
+{
+    return std::sqrt(distance_sq(lhs, rhs));
+}
+
+template<typename coordT>
+inline typename scalar_type_of<coordT>::type
+min_distance_sq(const PDBAtom<coordT>& lhs, const PDBAtom<coordT>& rhs) noexcept
+{
+    return distance_sq(lhs->position(), rhs->position());
+}
+
+template<typename coordT>
+inline typename scalar_type_of<coordT>::type
+min_distance(const PDBAtom<coordT>& lhs, const PDBAtom<coordT>& rhs) noexcept
+{
+    return std::sqrt(distance_sq(lhs, rhs));
+}
+
+// containerT :=
+//   {vector<PDBAtom>, vector<std::unique_ptr<Bead>>, PDBResidue, PDBChain}
+// To add another class to this list, define ::value_type::coordinate_type.
+template<typename containerT>
+typename scalar_type_of<typename containerT::value_type::coordinate_type>::type
+min_distance_sq(const containerT& lhs, const containerT& rhs)
+{
+    typedef typename scalar_type_of<
+        typename containerT::value_type::coordinate_type>::type real_type;
     auto min_dist = std::numeric_limits<real_type>::infinity();
 
     for(auto l : lhs)
     {
         for(auto r : rhs)
         {
-            const auto dist = distance_sq(l, r);
+            const auto dist = min_distance_sq(l, r);
             if(min_dist > dist) {min_dist = dist;}
         }
     }
@@ -68,106 +104,21 @@ min_distance(const std::vector<PDBAtom<coordT>>& lhs,
     return std::sqrt(min_distance_sq(lhs, rhs));
 }
 
-template<typename coordT>
-typename scalar_type_of<coordT>::type
-min_distance_sq(const PDBResidue<coordT>& lhs, const PDBResidue<coordT>& rhs)
-{
-    typedef typename scalar_type_of<coordT>::type real_type;
-    auto min_dist = std::numeric_limits<real_type>::infinity();
-
-    for(auto l : lhs)
-    {
-        for(auto r : rhs)
-        {
-            const auto dist = distance_sq(l, r);
-            if(min_dist > dist) {min_dist = dist;}
-        }
-    }
-    return min_dist;
-}
-
-template<typename coordT>
-inline typename scalar_type_of<coordT>::type
-min_distance(const PDBResidue<coordT>& lhs, const PDBResidue<coordT>& rhs)
-{
-    return std::sqrt(min_distance_sq(lhs, rhs));
-}
-
-template<typename coordT>
-typename scalar_type_of<coordT>::type
-min_distance_sq(const PDBChain<coordT>& lhs, const PDBChain<coordT>& rhs)
-{
-    typedef typename scalar_type_of<coordT>::type real_type;
-    auto min_dist = std::numeric_limits<real_type>::infinity();
-
-    for(auto l : lhs)
-    {
-        for(auto r : rhs)
-        {
-            const auto dist = distance_sq(l, r);
-            if(min_dist > dist) {min_dist = dist;}
-        }
-    }
-    return min_dist;
-}
-
-template<typename coordT>
-inline typename scalar_type_of<coordT>::type
-min_distance(const PDBChain<coordT>& lhs, const PDBChain<coordT>& rhs)
-{
-    return std::sqrt(min_distance_sq(lhs, rhs));
-}
-
 /* ---------------------------- distance_center ----------------------------- */
-// TODO: refactoring (according to DRY)
 
-template<typename coordT>
-typename scalar_type_of<coordT>::type
-distance_center_sq(const std::vector<PDBAtom<coordT>>& lhs,
-                   const std::vector<PDBAtom<coordT>>& rhs) noexcept
+template<typename containerT>
+typename scalar_type_of<typename containerT::value_type::coordinate_type>::type
+distance_center_sq(const containerT& lhs, const containerT& rhs) noexcept
 {
     return distance_sq(center(lhs), center(rhs));
 }
 
-template<typename coordT>
-inline typename scalar_type_of<coordT>::type
-distance_center(const std::vector<PDBAtom<coordT>>& lhs,
-                const std::vector<PDBAtom<coordT>>& rhs) noexcept
+template<typename containerT>
+typename scalar_type_of<typename containerT::value_type::coordinate_type>::type
+distance_center(const containerT& lhs, const containerT& rhs) noexcept
 {
     return std::sqrt(distance_center_sq(lhs, rhs));
 }
 
-template<typename coordT>
-typename scalar_type_of<coordT>::type
-distance_center_sq(const PDBResidue<coordT>& lhs,
-                   const PDBResidue<coordT>& rhs) noexcept
-{
-    return distance_sq(center(lhs), center(rhs));
-}
-
-template<typename coordT>
-inline typename scalar_type_of<coordT>::type
-distance_center(const PDBResidue<coordT>& lhs,
-                const PDBResidue<coordT>& rhs) noexcept
-{
-    return std::sqrt(distance_center_sq(lhs, rhs));
-}
-
-template<typename coordT>
-typename scalar_type_of<coordT>::type
-distance_center_sq(const PDBChain<coordT>& lhs,
-                   const PDBChain<coordT>& rhs) noexcept
-{
-    return distance_sq(center(lhs), center(rhs));
-}
-
-template<typename coordT>
-inline typename scalar_type_of<coordT>::type
-distance_center(const PDBChain<coordT>& lhs,
-                const PDBChain<coordT>& rhs) noexcept
-{
-    return std::sqrt(distance_center_sq(lhs, rhs));
-}
-
-} // jarngreipr
+} // mjolnir
 #endif /* JARNGREIPR_GEOMETRY_DISTANCE */
