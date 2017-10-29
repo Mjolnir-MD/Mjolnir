a3d96d2e0526958071b5daaa71aa84a44b919821
ToruNiina <niina.toru.68u@gmail.com>
2017-10-29 18:26:56 +0900

    fix? loop in write_as_xyz

--- jarngreipr/io/write_as_xyz.hpp
+++ jarngreipr/io/write_as_xyz.hpp
@@ -2,7 +2,7 @@
 #define JARNGREIPR_WRITE_AS_XYZ
 #include <jarngreipr/model/CGChain.hpp>
 #include <jarngreipr/io/XYZData.hpp>
-#include <jarngreipr/io/XYZWrite.hpp>
+#include <jarngreipr/io/XYZWriter.hpp>
 
 namespace mjolnir
 {
@@ -11,10 +11,12 @@ template<typename T>
 void write_as_xyz(std::ostream& os, const CGChain<T>& chain)
 {
     os << chain.size() << '\n';
-    os << chain.front().kind() << '\n';
-    for(const auto& bead : chain)
+    os << chain.front()->kind() << '\n';
+    for(std::size_t i=0; i<chain.size(); ++i)
     {
-        os << XYZLine<T>{bead->name, bead->position()} << '\n';
+        const auto& bead = chain.at(i);
+        const XYZLine<T> xyz(bead->name(), bead->position());
+        os << xyz << '\n';
     }
     return;
 }
