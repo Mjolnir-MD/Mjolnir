f0b663a2d7c79fa454eaace91940328244fe8399
ToruNiina <niina.toru.68u@gmail.com>
2017-11-23 16:48:38 +0900

    update jarngreipr; not so well now

--- src/jarngreipr.cpp
+++ src/jarngreipr.cpp
@@ -1,115 +1,206 @@
+#include <extlib/toml/toml.hpp>
 #include <jarngreipr/io/PDBReader.hpp>
-#include <jarngreipr/model/ClementiGo.hpp>
-#include <mjolnir/core/DefaultTraits.hpp>
+#include <jarngreipr/io/PDBWriter.hpp>
+#include <jarngreipr/io/write_as_xyz.hpp>
+#include <jarngreipr/io/write_as_xyz.hpp>
+#include <jarngreipr/model/CGChain.hpp>
+#include <jarngreipr/model/make_coarse_grained.hpp>
+// #include <jarngreipr/io/read_parameter_table.hpp>
+// #include <jarngreipr/model/ClementiGo.hpp>
+#include <mjolnir/math/Vector.hpp>
 #include <mjolnir/core/RandomNumberGenerator.hpp>
+#include <algorithm>
 #include <map>
 
-typedef mjolnir::DefaultTraits traits;
+using mjolnir::operator"" _str;
 
-constexpr static double T = 300.0;
-constexpr static double kB = 1.986231313e-3;
+std::vector<char> split_ids(const std::string& key)
+{
+    std::vector<char> ids;
+    if(key.find('-') == std::string::npos)
+    {
+        ids.push_back(key.front());
+    }
+    else
+    {
+        const char first = key.front();
+        const char last  = key.back();
+        for(char c = first; c <= last; ++c)
+        {
+            ids.push_back(c);
+        }
+    }
+    return ids;
+}
 
 int main(int argc, char **argv)
 {
+    typedef mjolnir::Vector<double, 3>    coord_type;
+    typedef mjolnir::PDBChain<coord_type> pdb_chain_type;
+    typedef mjolnir::CGChain<coord_type>  cg_chain_type;
+
     if(argc != 2)
     {
-        std::cerr << "./jarngreipr [file.pdb]" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " [file.toml]\n";
+        std::cerr << "    see input/example.toml for the format" << std::endl;
         return 1;
     }
-    mjolnir::RandomNumberGenerator<traits> rng(1234567);
-    std::map<std::string, double> mass;
-    mass["ALA"] = 71.09;
-    mass["ARG"] = 156.19;
-    mass["ASN"] = 114.11;
-    mass["ASP"] = 115.09;
-    mass["CYS"] = 103.15;
-    mass["GLN"] = 128.14;
-    mass["GLU"] = 129.12;
-    mass["GLY"] = 57.05;
-    mass["HIS"] = 137.14;
-    mass["ILE"] = 113.16;
-    mass["LEU"] = 113.16;
-    mass["LYS"] = 128.17;
-    mass["MET"] = 131.19;
-    mass["PHE"] = 147.18;
-    mass["PRO"] = 97.12;
-    mass["SER"] = 87.08;
-    mass["THR"] = 101.11;
-    mass["TRP"] = 186.21;
-    mass["TYR"] = 163.18;
-    mass["VAL"] = 99.14;
-    std::cerr << "parameter setup end" << std::endl;
 
-    // hardly-coding c-alpha model
-    std::string fname(argv[1]);
-    jarngreipr::PDBReader<traits> pdb;
-    auto atoms  = pdb.read(fname);
-    std::cerr << atoms.size() << " particles found" << std::endl;
-    auto chains = pdb.parse(atoms);
-    std::cerr << chains.size() << " chains found" << std::endl;
+    const auto input_data = toml::parse(std::string(argv[1]));
 
-    jarngreipr::ClementiGo<traits> clementigo;
-    clementigo.make(chains);
+    /* prepairing parameters */
+    const auto& general  = input_data.at("general").cast<toml::value_t::Table>();
+    const auto file_name = toml::get<std::string>(general.at("file_name"));
+    const auto seed      = toml::get<std::int64_t>(general.at("seed"));
+//     const auto parameter_table = mjolnir::read_parameters(
+//             toml::get<std::string>(general.at("parameter_path")));
 
-    // output
+    /* generating coarse-grained structures */
+    const auto& structure_tables = toml::get<std::vector<toml::Table>>(
+            input_data.at("structures"));
 
-    // particles
+    // structure = listof{chain}; chain = pairof{reference, initial}
+    std::vector<std::vector<std::tuple<char, cg_chain_type, cg_chain_type>>>
+        structure_sets;
 
-    std::cout << "number_of_particle = " << clementigo.beads().size() << std::endl;
-    std::vector<double> fric_consts;
-    std::cout << "# particles {{{" << std::endl;
-    std::cout << "particles = [" << std::endl;
-    for(auto iter = clementigo.beads().cbegin();
-            iter != clementigo.beads().cend(); ++iter) // bead
+    std::size_t structure_index=0;
+    for(const auto& structure_table : structure_tables)
     {
-        const std::string name = (*iter)->name();
-        const typename traits::real_type m = mass[name];
+        std::vector<std::tuple<char, cg_chain_type, cg_chain_type>>
+            structure_set;
 
-        const auto pos = (*iter)->position(0);
-        const auto vel = rng.maxwell_boltzmann(m, T, kB);
-        const auto acc = traits::coordinate_type(0., 0., 0.);
+        if(structure_table.count("index") == 0)
+        {
+            ++structure_index; // XXX more safer way?
+        }
+        else
+        {
+            structure_index =
+                toml::get<std::size_t>(structure_table.at("index"));
+        }
 
-        std::cout << std::scientific;
-        std::cout << "{mass = " << m << ", position = ["
-                  << pos[0] << ", " << pos[1] << ", " << pos[2]
-                  << "], velocity = ["
-                  << vel[0] << ", " << vel[1] << ", " << vel[2]
-                  << "]},"
-                  << std::endl;
+        for(const auto& kv : structure_table)
+        {
+            // value = {file = "*.pdb", initial = "*.pdb", model = "CarbonAlpha"}
+            const auto& value = kv.second.cast<toml::value_t::Table>();
+            for(char cid : split_ids(kv.first))
+            {
+                structure_set.emplace_back(
+                        cid, cg_chain_type{}, cg_chain_type{});
+            }
+            const auto model = toml::get<std::string>(value.at("model"));
 
-        const typename traits::real_type f = 0.005 * 168.7 / m;
-        fric_consts.push_back(f);
+            // read pdb file
+            const std::string pdb_filename =
+                toml::get<std::string>(value.at("file"));
+
+            std::string ini_filename = pdb_filename;
+            try
+            {
+                ini_filename = toml::get<std::string>(value.at("initial"));
+            }
+            catch(...)
+            {
+                // use pdb_filename as initial configuration file.
+            }
+            /* read reference file */{
+                mjolnir::PDBReader<coord_type> pdb_reader(pdb_filename);
+                if(not pdb_reader.is_good())
+                {
+                    std::cerr << "jarngreipr: at main(): file open error \n"
+                        << "    while reading pdb file in [[structures]] table.: "
+                        << pdb_filename << std::endl;
+                    return 1;
+                }
+
+                // find all chains
+                while(not pdb_reader.is_eof())
+                {
+                    const auto chain = pdb_reader.read_next_chain();
+                    const char chain_id = chain.chain_id();
+                    const auto found = std::find_if(
+                        structure_set.begin(), structure_set.end(),
+                        [=](const std::tuple<char, cg_chain_type, cg_chain_type>& v){
+                            return std::get<0>(v) == chain_id;
+                        });
+                    if(found != structure_set.end())
+                    {
+                        std::get<1>(*found) =
+                            mjolnir::make_coarse_grained(chain, model);
+                    }
+                }
+                for(const auto& item : structure_set)
+                {
+                    if(std::get<1>(item).empty())
+                    {
+                        throw std::runtime_error(
+                            "jarngreipr: at main(): missing chain "_str +
+                            std::string(1, std::get<0>(item)) + " in file "_str +
+                            ini_filename);
+                    }
+                }
+            }
+            /* read reference file */{
+                mjolnir::PDBReader<coord_type> ini_reader(ini_filename);
+                if(not ini_reader.is_good())
+                {
+                    std::cerr << "jarngreipr: at main(): file open error \n"
+                        << "    while reading pdb file in [[structures]] table.: "
+                        << ini_filename << std::endl;
+                    return 1;
+                }
+
+                // find all chains
+                while(not ini_reader.is_eof())
+                {
+                    const auto chain = ini_reader.read_next_chain();
+                    const char chain_id = chain.chain_id();
+                    const auto found = std::find_if(
+                        structure_set.begin(), structure_set.end(),
+                        [=](const std::tuple<char, cg_chain_type, cg_chain_type>& v){
+                            return std::get<0>(v) == chain_id;
+                        });
+                    if(found != structure_set.end())
+                    {
+                        std::get<2>(*found) =
+                            mjolnir::make_coarse_grained(chain, model);
+                    }
+                }
+                for(const auto& item : structure_set)
+                {
+                    if(std::get<2>(item).empty())
+                    {
+                        throw std::runtime_error(
+                            "jarngreipr: at main(): missing chain "_str +
+                            std::string(1, std::get<0>(item)) + " in file "_str +
+                            ini_filename);
+                    }
+                }
+            }
+        }
+        structure_sets.push_back(std::move(structure_set));
     }
-    std::cout << "]" << std::endl;
-    std::cout << "# }}}" << std::endl;
-
-    std::cout << "# friction {{{" << std::endl;
-    std::cout << "friction_constant = [" << std::endl;
-    for(auto iter = fric_consts.cbegin(); iter != fric_consts.cend(); ++iter)
-        std::cout << *iter << ", " << std::endl;
-    std::cout << "]" << std::endl;
-    std::cout << "# }}}" << std::endl;
-
-    clementigo.output_bond(std::cout);
-    clementigo.output_go(std::cout);
-    clementigo.output_angle(std::cout);
-    clementigo.output_dihedral(std::cout);
-
-    // exvs
-    std::cout << "[[globalforcefield]]" << std::endl;
-    std::cout << "interaction = \"Global\"" << std::endl;
-    std::cout << "potential = \"ExcludedVolume\"" << std::endl;
-    std::cout << "epsilon = 0.6" << std::endl;
-    std::cout << "# params{{{" << std::endl;
-    std::cout << "parameters = [" << std::endl;
-    for(std::size_t i=0; i<clementigo.beads().size(); ++i)
-    {
-        std::cout << "{sigma = 2.0}," << std::endl;
+    /* output coarse-grained structure */{
+        for(const auto& structure_set : structure_sets)
+        {
+            std::ofstream refstr(file_name + "_cg_"_str +
+                                 std::to_string(structure_index) + ".xyz"_str);
+            std::ofstream inicon(file_name + "_init_"_str +
+                                 std::to_string(structure_index) + ".xyz"_str);
+            std::size_t offset = 0;
+            for(const auto& structure : structure_set)
+            {
+                write_as_pdb(refstr, std::get<1>(structure), offset);
+                write_as_pdb(inicon, std::get<2>(structure), offset);
+                offset += structure.size();
+            }
+        }
     }
-    std::cout << "]" << std::endl;
-    std::cout << "# }}}" << std::endl;
 
-    clementigo.output_exception(std::cout);
+
+
+
+
 
     return 0;
 }
