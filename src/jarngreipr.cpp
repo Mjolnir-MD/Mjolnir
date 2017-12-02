0abfe080e368747eb0d885f1675022aeb0c9af7f
ToruNiina <niina.toru.68u@gmail.com>
2017-12-02 18:51:33 +0900

    add system setup experimentally

--- src/jarngreipr.cpp
+++ src/jarngreipr.cpp
@@ -272,11 +272,20 @@ int main(int argc, char **argv)
     const auto input_data = toml::parse(std::string(argv[1]));
 
     /* prepairing parameters */
-    const auto& general  = input_data.at("general").cast<toml::value_t::Table>();
+    const auto general   = toml::get<toml::Table>(input_data.at("general"));
     const auto file_name = toml::get<std::string>(general.at("file_name"));
     const auto seed      = toml::get<std::int64_t>(general.at("seed"));
-//     const auto parameter_table = mjolnir::read_parameters(
-//             toml::get<std::string>(general.at("parameter_path")));
+    std::mt19937 mt(seed);
+
+    const auto parameters =
+        toml::parse(toml::get<std::string>(general.at("parameters")));
+    const auto& mass =
+        toml::get<toml::Table>(parameters.at("mass"));
+    const auto& phys =
+        toml::get<toml::Table>(parameters.at("physical_constants"));
+
+    const auto system_config =
+        toml::get<std::vector<toml::Table>>(input_data.at("systems"));
 
     /* generating coarse-grained structures */
     const auto structure_config =
@@ -284,12 +293,66 @@ int main(int argc, char **argv)
 
     // vector<map<char, PDBChain<coord>>>
     // in most cases, the size of vector is one.
-    const auto ref_pdbs = mjolnir::read_reference_structures<coord_type>(structure_config);
-    const auto ini_pdbs = mjolnir::read_initial_structures  <coord_type>(structure_config);
-    const auto models   = mjolnir::read_coarse_grained_models(structure_config);
-    // Coarse Grained Structure -> cgs
+    const auto ref_pdbs =
+        mjolnir::read_reference_structures<coord_type>(structure_config);
+    const auto ini_pdbs =
+        mjolnir::read_initial_structures  <coord_type>(structure_config);
+    const auto models   =
+        mjolnir::read_coarse_grained_models(structure_config);
+
+    // apply coarse-grained model to input pdbs
     const auto ref_cgss = mjolnir::apply_coarse_grained_models(ref_pdbs, models);
     const auto ini_cgss = mjolnir::apply_coarse_grained_models(ini_pdbs, models);
+
+    // output system setting and cg structure
+    assert(system_config.size() == ini_cgss.size());
+    for(std::size_t i=0; i<system_config.size(); ++i)
+    {
+        std::cout << "[[systems]]\n";
+        // TODO {{{
+        const auto& sysconf = system_config.at(i);
+        const auto T = toml::get<double>(sysconf.at("temperature"));
+        std::cout << "temperature = " << std::fixed << T << '\n';
+        std::cout << "ionic_strength = " << std::fixed
+                  << toml::get<double>(sysconf.at("ionic_strength")) << '\n';
+        std::cout << "boundary = {";
+        for(const auto& kv : toml::get<toml::Table>(sysconf.at("boundary")))
+        {
+            if(kv.first == "type")
+            {
+                std::cout << kv.first << "=\"" << toml::get<std::string>(kv.second) << '\"';
+            }
+            else
+            {
+                const auto& crd = toml::get<std::array<double, 3>>(kv.second);
+                std::cout << kv.first << "=[" << crd[0] << ',' << crd[1] << ','
+                          << crd[2] << ']';
+            }
+        }
+        std::cout << "}\n";
+        // }}}
+
+        std::cout << "particles = [\n";
+        const auto& chain_table = ini_cgss.at(i);
+        for(const auto& id_chain_pair : chain_table)
+        {
+            std::cerr << "chain ID = " << id_chain_pair.first;
+            std::cerr << " has " << id_chain_pair.second.size() << " beads." << std::endl;
+            for(std::size_t j=0; j<id_chain_pair.second.size(); ++j)
+            {
+                const auto& bead = id_chain_pair.second.at(j);
+                const auto kB = toml::get<double>(phys.at("kB"));
+                const auto m  = toml::get<double>(mass.at(bead->name()));
+                const auto p  = bead->position();
+                std::normal_distribution<double> mxw_blz(0.0, std::sqrt(kB*T/m));
+                std::cout << "{mass = " << m
+                          <<  ", position = [" << p[0] << ',' << p[1] << ',' << p[2]
+                          << "], velocity = [" << mxw_blz(mt) << ','
+                          << mxw_blz(mt) << ',' << mxw_blz(mt) << "]},\n";
+            }
+        }
+        std::cout << "]\n";
+    }
 
     const std::unique_ptr<mjolnir::IntraChainForceFieldGenerator<coord_type>>
         ffgen = mjolnir::make_unique<mjolnir::ClementiGo<coord_type>>();
@@ -297,6 +360,7 @@ int main(int argc, char **argv)
     std::size_t offset(0);
     for(const auto& chains : ref_cgss)
     {
+        std::cout << "[[forcefields]]\n";
         for(const auto& idch : chains)
         {
             const auto& ch = idch.second;
