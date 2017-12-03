46d8e3c9d908d6c54a322f0a88902368ae149797
ToruNiina <niina.toru.68u@gmail.com>
2017-12-03 17:16:31 +0900

    use detail::value_at in read_integrator

--- mjolnir/input/read_integrator.hpp
+++ mjolnir/input/read_integrator.hpp
@@ -3,6 +3,7 @@
 #include <extlib/toml/toml.hpp>
 #include <mjolnir/core/VelocityVerletStepper.hpp>
 #include <mjolnir/core/UnderdampedLangevinStepper.hpp>
+#include <mjolnir/input/get_toml_value.hpp>
 
 namespace mjolnir
 {
@@ -11,9 +12,12 @@ template<typename traitsT>
 VelocityVerletStepper<traitsT>
 read_velocity_verlet_stepper(const toml::Table& data)
 {
-    const auto& simulator = data.at("simulator").cast<toml::value_t::Table>();
-    return VelocityVerletStepper<traitsT>(
-            toml::get<typename traitsT::real_type>(simulator.at("delta_t")));
+    typedef typename traitsT::real_type real_type;
+
+    const auto& simulator = detail::value_at(data, "simulator", "<root>"
+            ).cast<toml::value_t::Table>();
+    return VelocityVerletStepper<traitsT>(toml::get<real_type>(
+            detail::value_at(simulator, "delta_t", "[[simulator]]")));
 }
 
 
@@ -22,25 +26,30 @@ UnderdampedLangevinStepper<traitsT>
 read_underdamped_langevin_stepper(const toml::Table& data)
 {
     typedef typename traitsT::real_type real_type;
-    const auto& simulator = data.at("simulator").cast<toml::value_t::Table>();
-    const auto& registrator =
-        simulator.at("register").cast<toml::value_t::Array>();
 
-    const std::uint32_t seed = toml::get<std::uint32_t>(simulator.at("seed"));
+    const auto& simulator   = detail::value_at(data, "simulator", "<root>"
+            ).cast<toml::value_t::Table>();
+    const auto& registrator = detail::value_at(
+            simulator, "register", "[[simulator]]").cast<toml::value_t::Array>();
+
+    const std::uint32_t seed = toml::get<std::uint32_t>(
+            detail::value_at(simulator, "seed", "[[simulator]]"));
 
     std::vector<real_type> gamma(registrator.size());
     for(const auto& tab : registrator)
     {
         const auto& params = tab.cast<toml::value_t::Table>();
-        const std::size_t idx = toml::get<std::size_t>(params.at("index"));
-        const real_type    gm = toml::get<real_type>(params.at("gamma"));
+        const std::size_t idx = toml::get<std::size_t>(detail::value_at(
+                params, "index", "<anonymous> in register"));
+        const real_type    gm = toml::get<real_type>(detail::value_at(
+                params, "gamma", "<anonymous> in register"));
 
-        if(gamma.size() <= idx) gamma.resize(idx+1);
+        if(gamma.size() <= idx){gamma.resize(idx+1);}
         gamma.at(idx) = gm;
     }
 
-    return UnderdampedLangevinStepper<traitsT>(
-            toml::get<typename traitsT::real_type>(simulator.at("delta_t")),
+    return UnderdampedLangevinStepper<traitsT>(toml::get<real_type>(
+            detail::value_at(simulator, "delta_t", "[[simulator]]")),
             std::move(gamma), RandomNumberGenerator<traitsT>(seed));
 }
 
