52532c1658df3ebdb96e41a7607c3e955ed06d1c
ToruNiina <niina.toru.68u@gmail.com>
2017-12-03 19:37:25 +0900

    allow empty forcefield

--- mjolnir/input/read_forcefield.hpp
+++ mjolnir/input/read_forcefield.hpp
@@ -2,7 +2,8 @@
 #define MJOLNIR_READ_FORCEFIELD
 #include <extlib/toml/toml.hpp>
 #include <mjolnir/core/ForceField.hpp>
-#include "read_interaction.hpp"
+#include <mjolnir/input/get_toml_value.hpp>
+#include <mjolnir/input/read_interaction.hpp>
 
 namespace mjolnir
 {
@@ -40,16 +41,29 @@ template<typename traitsT>
 ForceField<traitsT>
 read_forcefield(const toml::Table& data, std::size_t N)
 {
-    const auto& ffs = data.at("forcefields").cast<toml::value_t::Array>();
+    const auto& ffs = detail::value_at(data, "forcefields", "<root>"
+            ).cast<toml::value_t::Array>();
+
     if(ffs.size() <= N)
+    {
         throw std::out_of_range("no enough forcefields: " + std::to_string(N));
+    }
     const auto& ff = ffs.at(N).cast<toml::value_t::Table>();
 
+    std::vector<toml::Table> fflocal;
+    std::vector<toml::Table> ffglobal;
+
+    if(ffs.count("local"))
+    {
+        fflocal  = toml::get<std::vector<toml::Table>>(ff.at("local"));
+    }
+    if(ffs.count("global"))
+    {
+        ffglobal = toml::get<std::vector<toml::Table>>(ff.at("global"));
+    }
     return ForceField<traitsT>(
-        read_local_forcefield<traitsT>(
-            toml::get<std::vector<toml::Table>>(ff.at("local"))),
-        read_global_forcefield<traitsT>(
-            toml::get<std::vector<toml::Table>>(ff.at("global"))));
+            read_local_forcefield<traitsT>(std::move(fflocal)),
+            read_global_forcefield<traitsT>(std::move(ffglobal)));
 }
 
 
