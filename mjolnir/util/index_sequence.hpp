97eba9b2e7779a4e766e10ba1a856b5945d98593
ToruNiina <niina.toru.68u@gmail.com>
2018-02-27 22:08:07 +0900

    fix index_sequence template specialization

--- mjolnir/util/index_sequence.hpp
+++ mjolnir/util/index_sequence.hpp
@@ -20,33 +20,25 @@ struct index_sequence_concatenator<
     typedef index_sequence<v1 ..., v2 ...> type;
 };
 
-// [0, 4) -> [0, 2) ++ [2, 4) -> [0, 1) ++ [1, 2) ++ [2, 3) ++ [3, 4)
-// [0, 3) -> [0, 1) ++ [1, 3) -> [0, 1) ++ [1, 2) ++ [2, 3)
-template<std::size_t first, std::size_t last>
+template<std::size_t N>
 struct index_sequence_generator
 {
-    constexpr static inline std::size_t
-    middle(const std::size_t low, const std::size_t high) noexcept
-    {
-        return low + (high - low) / 2;
-    }
-
     typedef typename index_sequence_concatenator<
-            typename index_sequence_generator<first, middle(first, last)>::type,
-            typename index_sequence_generator<middle(first, last), last>::type
+            typename index_sequence_generator<N-1>::type,
+            index_sequence<N>
         >::type type;
 };
 
-template<std::size_t N>
-struct index_sequence_generator<N, N+1>
+template<>
+struct index_sequence_generator<0>
 {
-    typedef index_sequence<N> type;
+    typedef index_sequence<0> type;
 };
 } // detail
 
 template<std::size_t N>
 using make_index_sequence =
-    typename detail::index_sequence_generator<0, N>::type;
+    typename detail::index_sequence_generator<N-1>::type;
 
 } // mjolnir
 #endif// MJOLNIR_INDEX_SEQUENCE_H
