94bd215969ca4d3200cd40ae59cbbd51a4ed7a76
ToruNiina <niina.toru.68u@gmail.com>
2018-03-14 20:57:13 +0900

    fix timing and simplify velocity verlet

--- mjolnir/core/VelocityVerletStepper.hpp
+++ mjolnir/core/VelocityVerletStepper.hpp
@@ -30,35 +30,24 @@ class VelocityVerletStepper
   public:
 
     VelocityVerletStepper(const real_type dt) noexcept
-        : dt_(dt), halfdt_(dt * 0.5), halfdt2_(dt * dt * 0.5)
+        : dt_(dt), halfdt_(dt * 0.5)
     {}
     ~VelocityVerletStepper() = default;
 
-    void initialize(const system_type& sys);
+    void initialize(const system_type& sys){return;}
 
     real_type step(const real_type time, system_type& sys, forcefield_type& ff);
 
     real_type delta_t() const noexcept {return dt_;}
     void  set_delta_t(const real_type dt) noexcept
     {
-        dt_ = dt; halfdt_ = dt * 0.5; halfdt2_ = halfdt_ * dt_;
+        dt_ = dt; halfdt_ = dt * 0.5;
     }
 
   private:
     real_type dt_;      //!< dt
     real_type halfdt_;  //!< dt/2
-    real_type halfdt2_; //!< dt^2/2
-    std::vector<coordinate_type> acceleration_;
 };
-
-template<typename traitsT, typename rescalingT>
-void VelocityVerletStepper<traitsT, rescalingT>::initialize(const system_type& sys)
-{
-    acceleration_.resize(sys.size());
-    std::transform(sys.cbegin(), sys.cend(), acceleration_.begin(),
-            [](const particle_type& p){return p.force / p.mass;});
-    return;
-}
 
 template<typename traitsT, typename rescalingT>
 typename VelocityVerletStepper<traitsT, rescalingT>::real_type
@@ -66,35 +55,24 @@ VelocityVerletStepper<traitsT, rescalingT>::step(
         const real_type time, system_type& system, forcefield_type& ff)
 {
     real_type max_speed2(0.);
-    // calc r(t+dt)
-    for(auto iter = make_zip(system.begin(), acceleration_.cbegin());
-            iter != make_zip(system.end(),   acceleration_.cend()); ++iter)
+    for(std::size_t i=0; i<system.size(); ++i)
     {
-        const auto& particle = *(get<0>(iter));
-        const auto& acc      = *(get<1>(iter));
-
+        auto& particle = system[i];
         max_speed2 = std::max(max_speed2, length_sq(particle.velocity));
 
-        get<0>(iter)->position = system.adjust_position(
-            particle.position + dt_ * (particle.velocity) + halfdt2_ * acc);
-
-        get<0>(iter)->velocity += halfdt_ * acc;
+        particle.velocity += (halfdt_ / particle.mass) * particle.force;
+        particle.position = system.adjust_position(particle.position +
+                                             dt_ * particle.velocity);
+        particle.force    = coordinate_type(0.0, 0.0, 0.0);
     }
     system.max_speed() = std::sqrt(max_speed2);
 
-    // calc f(t+dt)
     ff.calc_force(system);
 
-    // calc a(t+dt) and v(t+dt)
-    for(auto iter = make_zip(system.begin(), acceleration_.begin());
-            iter != make_zip(system.end(),   acceleration_.end()); ++iter)
+    for(std::size_t i=0; i<system.size(); ++i)
     {
-        const auto& particle = *(get<0>(iter));
-
-        const coordinate_type acc = particle.force / (particle.mass);
-       *get<1>(iter) = acc;
-        get<0>(iter)->velocity += halfdt_ * acc;
-        get<0>(iter)->force     = coordinate_type(0., 0., 0.);
+        auto& particle = system[i];
+        particle.velocity += (halfdt_ / particle.mass) * particle.force;
     }
 
     remove_translation(system);
@@ -102,8 +80,6 @@ VelocityVerletStepper<traitsT, rescalingT>::step(
 
     return time + dt_;
 }
-
-
 
 } // mjolnir
 #endif /* MJOLNIR_VELOCITY_VERLET_INTEGRATOR */
