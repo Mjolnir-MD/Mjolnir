e7ef6406d6a45f9b4cc8f4b7853c54438f0139c0
ToruNiina <niina.toru.68u@gmail.com>
2018-03-22 14:41:55 +0900

    add group ignoration method class

---
+++ mjolnir/potential/global/GroupIgnoration.hpp
@@ -0,0 +1,56 @@
+#ifndef MJOLNIR_GROUP_IGNORATION
+#define MJOLNIR_GROUP_IGNORATION
+
+namespace mjolnir
+{
+
+template<typename group_idT>
+struct GroupIgnorationBase
+{
+    typedef group_idT group_id_type;
+    virtual ~GroupIgnorationBase() = default;
+    virtual bool is_ignored(const group_id_type& i, const group_id_type& j
+                            ) const noexcept = 0;
+};
+
+template<typename group_idT>
+struct IgnoreNothing final : public GroupIgnorationBase<group_idT>
+{
+    typedef GroupIgnorationBase<group_idT> base_type;
+    typedef typename base_type::group_id_type group_id_type;
+    ~IgnoreNothing() override = default;
+    bool is_ignored(const group_id_type& i, const group_id_type& j
+                    ) const noexcept override
+    {
+        return false;
+    }
+};
+
+template<typename group_idT>
+struct IgnoreSelf final : public GroupIgnorationBase<group_idT>
+{
+    typedef GroupIgnorationBase<group_idT> base_type;
+    typedef typename base_type::group_id_type group_id_type;
+    ~IgnoreSelf() override = default;
+    bool is_ignored(const group_id_type& i, const group_id_type& j
+                    ) const noexcept override
+    {
+        return i == j;
+    }
+};
+
+template<typename group_idT>
+struct IgnoreOthers final : public GroupIgnorationBase<group_idT>
+{
+    typedef GroupIgnorationBase<group_idT> base_type;
+    typedef typename base_type::group_id_type group_id_type;
+    ~IgnoreOthers() override = default;
+    bool is_ignored(const group_id_type& i, const group_id_type& j
+                    ) const noexcept override
+    {
+        return i != j;
+    }
+};
+
+} // mjolnir
+#endif// MJOLNIR_GROUP_IGNORATION
