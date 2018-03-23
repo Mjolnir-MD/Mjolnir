fd19a665712a6d78739154e8d1a2577ebe62bfb9
ToruNiina <niina.toru.68u@gmail.com>
2018-03-23 13:18:49 +0900

    make inline function inline

--- megingjord/util/aligned_array.hpp
+++ megingjord/util/aligned_array.hpp
@@ -23,7 +23,7 @@ struct aligned_array
     typedef std::ptrdiff_t    difference_type;
     typedef std::reverse_iterator<iterator>       reverse_iterator;
     typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
-    constexpr static std::size_t alignment = N_align;
+    constexpr static std::size_t alignment = Alignment;
 
     alignas(alignment) T elems[N];
 
@@ -31,190 +31,42 @@ struct aligned_array
     constexpr size_type max_size() const noexcept {return N;}
     constexpr bool      empty()    const noexcept {return false;}
 
-    void fill(const value_type& u){std::fill_n(begin(),size(),u);}
+    void fill(const value_type& u){std::fill_n(begin(), size(), u);}
 
-    reference       operator[](const size_type n)       noexcept;
-    const_reference operator[](const size_type n) const noexcept;
-    reference       at(const size_type n);
-    const_reference at(const size_type n) const;
+    reference       operator[](const size_type n)       noexcept {return elems[n];}
+    const_reference operator[](const size_type n) const noexcept {return elems[n];}
+    reference       at(const size_type n)
+    {if(n > N){throw std::out_of_range("aligned_array::at");} return elems[n];}
+    const_reference at(const size_type n) const
+    {if(n > N){throw std::out_of_range("aligned_array::at");} return elems[n];}
 
-    reference       front()       noexcept;
-    const_reference front() const noexcept;
-    reference       back()        noexcept;
-    const_reference back()  const noexcept;
-    pointer         data()        noexcept;
-    const_pointer   data()  const noexcept;
+    reference       front()       noexcept {return elems[i];}
+    const_reference front() const noexcept {return elems[i];}
+    reference       back()        noexcept {return elems[N-1];}
+    const_reference back()  const noexcept {return elems[N-1];}
+    pointer         data()        noexcept {return std::addressof(elems[0]);}
+    const_pointer   data()  const noexcept {return std::addressof(elems[0]);}
 
-    iterator       begin()        noexcept;
-    iterator       end()          noexcept;
-    const_iterator begin()  const noexcept;
-    const_iterator end()    const noexcept;
-    const_iterator cbegin() const noexcept;
-    const_iterator cend()   const noexcept;
+    iterator       begin()        noexcept {return this->data();}
+    iterator       end()          noexcept {return this->data() + N;}
+    const_iterator begin()  const noexcept {return this->data();}
+    const_iterator end()    const noexcept {return this->data() + N;}
+    const_iterator cbegin() const noexcept {return this->data();}
+    const_iterator cend()   const noexcept {return this->data() + N;}
 
-    reverse_iterator       rbegin()        noexcept;
-    reverse_iterator       rend()          noexcept;
-    const_reverse_iterator rbegin()  const noexcept;
-    const_reverse_iterator rend()    const noexcept;
-    const_reverse_iterator crbegin() const noexcept;
-    const_reverse_iterator crend()   const noexcept;
+    reverse_iterator       rbegin()        noexcept
+    {return reverse_iterator(this->begin());}
+    reverse_iterator       rend()          noexcept
+    {return reverse_iterator(this->end());}
+    const_reverse_iterator rbegin()  const noexcept
+    {return const_reverse_iterator(this->begin());}
+    const_reverse_iterator rend()    const noexcept
+    {return const_reverse_iterator(this->end());}
+    const_reverse_iterator crbegin() const noexcept
+    {return const_reverse_iterator(this->begin());}
+    const_reverse_iterator crend()   const noexcept
+    {return const_reverse_iterator(this->end());}
 };
-
-template<typename T, std::size_t N, std::size_t Na>
-inline typename aligned_array<T, N, Na>::reference
-aligned_array<T, N, Na>::operator[](const size_type n) noexcept
-{
-    return elems[n];
-}
-
-template<typename T, std::size_t N, std::size_t Na>
-inline typename aligned_array<T, N, Na>::const_reference
-aligned_array<T, N, Na>::operator[](const size_type n) const noexcept
-{
-    return elems[n];
-}
-
-template<typename T, std::size_t N, std::size_t Na>
-inline typename aligned_array<T, N, Na>::reference
-aligned_array<T, N, Na>::at(const size_type n)
-{
-    if(n >= N) throw std::out_of_range("aligned_array::at: n >= N");
-    return elems[n];
-}
-
-template<typename T, std::size_t N, std::size_t Na>
-inline typename aligned_array<T, N, Na>::const_reference
-aligned_array<T, N, Na>::at(const size_type n) const
-{
-    if(n >= N) throw std::out_of_range("aligned_array::at: n >= N");
-    return elems[n];
-}
-
-template<typename T, std::size_t N, std::size_t Na>
-inline typename aligned_array<T, N, Na>::reference
-aligned_array<T, N, Na>::front() noexcept
-{
-    return elems[0];
-}
-
-template<typename T, std::size_t N, std::size_t Na>
-inline typename aligned_array<T, N, Na>::const_reference
-aligned_array<T, N, Na>::front() const noexcept
-{
-    return elems[0];
-}
-
-template<typename T, std::size_t N, std::size_t Na>
-inline typename aligned_array<T, N, Na>::reference
-aligned_array<T, N, Na>::back() noexcept
-{
-    return elems[N-1];
-}
-
-template<typename T, std::size_t N, std::size_t Na>
-inline typename aligned_array<T, N, Na>::const_reference
-aligned_array<T, N, Na>::back() const noexcept
-{
-    return elems[N-1];
-}
-
-template<typename T, std::size_t N, std::size_t Na>
-inline typename aligned_array<T, N, Na>::pointer
-aligned_array<T, N, Na>::data() noexcept
-{
-    return elems;
-}
-
-template<typename T, std::size_t N, std::size_t Na>
-inline typename aligned_array<T, N, Na>::const_pointer
-aligned_array<T, N, Na>::data() const noexcept
-{
-    return elems;
-}
-
-template<typename T, std::size_t N, std::size_t Na>
-inline typename aligned_array<T, N, Na>::iterator
-aligned_array<T, N, Na>::begin() noexcept
-{
-    return iterator(data());
-}
-
-template<typename T, std::size_t N, std::size_t Na>
-inline typename aligned_array<T, N, Na>::iterator
-aligned_array<T, N, Na>::end() noexcept
-{
-    return iterator(data() + N);
-}
-
-template<typename T, std::size_t N, std::size_t Na>
-inline typename aligned_array<T, N, Na>::const_iterator
-aligned_array<T, N, Na>::begin() const noexcept
-{
-    return const_iterator(data());
-}
-
-template<typename T, std::size_t N, std::size_t Na>
-inline typename aligned_array<T, N, Na>::const_iterator
-aligned_array<T, N, Na>::end() const noexcept
-{
-    return const_iterator(data() + N);
-}
-
-template<typename T, std::size_t N, std::size_t Na>
-inline typename aligned_array<T, N, Na>::const_iterator
-aligned_array<T, N, Na>::cbegin() const noexcept
-{
-    return const_iterator(data());
-}
-
-template<typename T, std::size_t N, std::size_t Na>
-inline typename aligned_array<T, N, Na>::const_iterator
-aligned_array<T, N, Na>::cend() const noexcept
-{
-    return const_iterator(data() + N);
-}
-
-template<typename T, std::size_t N, std::size_t Na>
-inline typename aligned_array<T, N, Na>::reverse_iterator
-aligned_array<T, N, Na>::rbegin() noexcept
-{
-    return reverse_iterator(end());
-}
-
-template<typename T, std::size_t N, std::size_t Na>
-inline typename aligned_array<T, N, Na>::reverse_iterator
-aligned_array<T, N, Na>::rend() noexcept
-{
-    return reverse_iterator(begin());
-}
-
-template<typename T, std::size_t N, std::size_t Na>
-inline typename aligned_array<T, N, Na>::const_reverse_iterator
-aligned_array<T, N, Na>::rbegin() const noexcept
-{
-    return const_reverse_iterator(end());
-}
-
-template<typename T, std::size_t N, std::size_t Na>
-inline typename aligned_array<T, N, Na>::const_reverse_iterator
-aligned_array<T, N, Na>::rend() const noexcept
-{
-    return const_reverse_iterator(begin());
-}
-
-template<typename T, std::size_t N, std::size_t Na>
-inline typename aligned_array<T, N, Na>::const_reverse_iterator
-aligned_array<T, N, Na>::crbegin() const noexcept
-{
-    return const_reverse_iterator(end());
-}
-
-template<typename T, std::size_t N, std::size_t Na>
-inline typename aligned_array<T, N, Na>::const_reverse_iterator
-aligned_array<T, N, Na>::crend() const noexcept
-{
-    return const_reverse_iterator(begin());
-}
 
 } // megingjord
 #endif /* MEGINGJORD_ALIGNED_ARRAY */
