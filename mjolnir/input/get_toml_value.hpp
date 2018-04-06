b4566c7b92dd053d08ccec43e1e76b5a6b43e616
ToruNiina <niina.toru.68u@gmail.com>
2018-04-06 15:42:24 +0900

    remove default value of get_toml_value
    the argument that used to receieve default value is for error message
    generation. it should be provided by caller for user-experience.

--- mjolnir/input/get_toml_value.hpp
+++ mjolnir/input/get_toml_value.hpp
@@ -1,6 +1,7 @@
 #ifndef MJOLNIR_INPUT_GET_TOML_VALUE
 #define MJOLNIR_INPUT_GET_TOML_VALUE
 #include <extlib/toml/toml.hpp>
+#include <mjolnir/util/throw_exception.hpp>
 #include <string>
 #include <stdexcept>
 
@@ -9,7 +10,7 @@ namespace mjolnir
 
 inline toml::value const&
 toml_value_at(const toml::Table& tab, const std::string& key,
-         const std::string& tablename = "<couldn't provided>")
+              const std::string& tablename)
 {
     try
     {
@@ -17,8 +18,9 @@ toml_value_at(const toml::Table& tab, const std::string& key,
     }
     catch(const std::out_of_range& oor)
     {
-        throw std::out_of_range("mjolnir: while reading toml file: key " + key +
-                std::string(" in table ") + tablename + std::string(" missing"));
+        throw_exception<std::runtime_error>(
+            "mjolnir: while reading toml file: key(", key,
+            ") in table(", tablename, ") missing");
     }
 }
 
