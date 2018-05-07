2a348882fa6ba0e596af55e7f04c89eab0f754da
ToruNiina <niina.toru.68u@gmail.com>
2018-05-07 17:14:21 +0900

    make format of [[systems]] clean

--- jarngreipr/io/write_system.hpp
+++ jarngreipr/io/write_system.hpp
@@ -2,6 +2,7 @@
 #define JARNGREIPR_WRITE_SYSTEM_HPP
 #include <extlib/toml/toml.hpp>
 #include <mjolnir/util/get_toml_value.hpp>
+#include <iomanip>
 
 namespace jarngreipr
 {
@@ -24,13 +25,35 @@ void write_system(std::basic_ostream<charT, traits>& os, const toml::Table& sys)
         sys, "particles", "write_system::sys").cast<toml::value_t::Array>();
 
     os << "particles = [\n";
-    for(const auto& p : ps)
+    for(const auto& item : ps)
     {
-        write_toml_value(os, p);
-        os << ",\n";
+        const auto& particle = item.cast<toml::value_t::Table>();
+        const auto m = toml::get<double>(mjolnir::toml_value_at(
+            particle, "mass", "write_system: particles"));
+        const auto p = toml::get<std::array<double, 3>>(mjolnir::toml_value_at(
+            particle, "position", "write_system: particles"));
+        const auto v = toml::get<std::array<double, 3>>(mjolnir::toml_value_at(
+            particle, "velocity", "write_system: particles"));
+
+        os << "{mass=" << std::setw(7) << std::setprecision(2)
+           << std::fixed << std::right << m;
+        os << ",position=["
+           << std::setw(9) << std::setprecision(4) << std::fixed << std::right
+           << p[0] << ','
+           << std::setw(9) << std::setprecision(4) << std::fixed << std::right
+           << p[1] << ','
+           << std::setw(9) << std::setprecision(4) << std::fixed << std::right
+           << p[2];
+        os << "],velocity=["
+           << std::setw(9) << std::setprecision(4) << std::fixed << std::right
+           << v[0] << ','
+           << std::setw(9) << std::setprecision(4) << std::fixed << std::right
+           << v[1] << ','
+           << std::setw(9) << std::setprecision(4) << std::fixed << std::right
+           << v[2];
+        os << "]},\n";
     }
     os << "]\n";
-
     return ;
 }
 
