4a7dc06bcc4cdc4cccc01b6dfe99c3e9647b4fc7
ToruNiina <niina.toru.68u@gmail.com>
2018-06-09 18:53:51 +0900

    add logging to read_input_file and fix read_boundary

--- mjolnir/input/read_input_file.hpp
+++ mjolnir/input/read_input_file.hpp
@@ -16,6 +16,9 @@ template<typename traitsT>
 std::unique_ptr<SimulatorBase>
 read_parameter(const toml::Table& data)
 {
+    MJOLNIR_GET_DEFAULT_LOGGER();
+    MJOLNIR_SCOPE(read_parameter(const toml::Table& data), 0);
+
     typedef typename traitsT::real_type real_type;
     const auto& parameter = toml_value_at(data, "parameters", "<root>"
             ).template cast<toml::value_t::Table>();
@@ -29,6 +32,11 @@ read_parameter(const toml::Table& data)
     physics<real_type>::vacuum_permittivity = toml::get<real_type>(
             toml_value_at(parameter, "ε0", "[parameters]"));
 
+    MJOLNIR_LOG_INFO("kB = ", physics<real_type>::kB);
+    MJOLNIR_LOG_INFO("NA = ", physics<real_type>::NA);
+    MJOLNIR_LOG_INFO("e  = ", physics<real_type>::e);
+    MJOLNIR_LOG_INFO("ε0 = ", physics<real_type>::vacuum_permittivity);
+
     return read_simulator<traitsT>(data);
 }
 
@@ -36,6 +44,9 @@ template<typename realT>
 std::unique_ptr<SimulatorBase>
 read_boundary(const toml::Table& data)
 {
+    MJOLNIR_GET_DEFAULT_LOGGER();
+    MJOLNIR_SCOPE(read_boundary(const toml::Table& data), 0);
+
     const auto& general = toml_value_at(data, "general", "<root>"
             ).template cast<toml::value_t::Table>();
     const auto boundary = toml::get<std::string>(
@@ -43,13 +54,15 @@ read_boundary(const toml::Table& data)
 
     if(boundary == "Unlimited")
     {
+        MJOLNIR_LOG_INFO("boundary is UnlimitedBoundary");
         return read_parameter<
             SimulatorTraitsBase<realT, UnlimitedBoundary>>(data);
     }
     else if(boundary == "PeriodicCube")
     {
+        MJOLNIR_LOG_INFO("boundary is CubicPeriodicBoudanry");
         return read_parameter<
-            SimulatorTraitsBase<realT, UnlimitedBoundary>>(data);
+            SimulatorTraitsBase<realT, CubicPeriodicBoundary>>(data);
     }
     else
     {
@@ -66,12 +79,19 @@ read_precision(const toml::Table& data)
     const auto prec = toml::get<std::string>(
             toml_value_at(general, "precision", "[general]"));
 
+    MJOLNIR_SET_DEFAULT_LOGGER(std::string(toml::get<std::string>(
+            toml_value_at(general, "file_name", "[general]")) + ".log"));
+    MJOLNIR_GET_DEFAULT_LOGGER();
+    MJOLNIR_SCOPE(read_precision(const toml::Table& data), 0);
+
     if(prec == "double")
     {
+        MJOLNIR_LOG_INFO("precision is double");
         return read_boundary<double>(data);
     }
     else if(prec == "float")
     {
+        MJOLNIR_LOG_INFO("precision is float");
         return read_boundary<float>(data);
     }
     else
