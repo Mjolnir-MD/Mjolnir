4527b78facc779ee9f80443e1beddfbacaa3d651
ToruNiina <niina.toru.68u@gmail.com>
2018-06-11 19:24:45 +0900

    add logger to read_observer

--- mjolnir/input/read_observer.hpp
+++ mjolnir/input/read_observer.hpp
@@ -11,17 +11,23 @@ template<typename traitsT>
 Observer<traitsT>
 read_observer(const toml::Table& data)
 {
+    MJOLNIR_GET_DEFAULT_LOGGER();
+    MJOLNIR_SCOPE(read_observer(), 0);
+
     const auto& general = toml_value_at(data, "general", "<root>"
             ).cast<toml::value_t::Table>();
     const std::string path  = toml::get<std::string>(
             toml_value_at(general, "output_path", "[general]"));
     const std::string fname = toml::get<std::string>(
             toml_value_at(general, "file_name", "[general]"));
+    MJOLNIR_LOG_INFO("path     = ", path);
+    MJOLNIR_LOG_INFO("filename = ", fname);
 
     const auto& simulator = toml_value_at(data, "simulator", "<root>"
             ).cast<toml::value_t::Table>();
     const std::size_t interval = toml::get<std::size_t>(
             toml_value_at(simulator, "save_step", "[simulator]"));
+    MJOLNIR_LOG_INFO("interval = ", mg);
 
     return Observer<traitsT>(path + fname, interval);
 }
