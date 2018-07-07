e97444e60588b109e93ad56bb834d7f109f61666
ToruNiina <niina.toru.68u@gmail.com>
2018-07-07 10:46:00 +0900

    #11 enable to split system input file

--- mjolnir/input/read_system.hpp
+++ mjolnir/input/read_system.hpp
@@ -60,25 +60,13 @@ read_boundary(const toml::Table& boundary)
 }
 
 template<typename traitsT>
-System<traitsT> read_system(const toml::Table& data, std::size_t N)
+System<traitsT> read_system_from_table(const toml::Table& system)
 {
     MJOLNIR_GET_DEFAULT_LOGGER();
-    MJOLNIR_SCOPE(read_system(), 0);
-
-    using real_type  = typename traitsT::real_type;
+    MJOLNIR_SCOPE(read_system_from_table(), 0);
+    using real_type       = typename traitsT::real_type;
     using coordinate_type = typename traitsT::coordinate_type;
 
-    const auto& system_params = toml_value_at(data, "systems", "<root>"
-            ).cast<toml::value_t::Array>();
-    if(system_params.size() <= N)
-    {
-        throw_exception<std::out_of_range>("no enough system definitions: ", N);
-    }
-
-    MJOLNIR_LOG_INFO(system_params.size(), " systems are provided");
-    MJOLNIR_LOG_INFO("using ", N, "-th system");
-
-    const auto& system   = system_params.at(N).cast<toml::value_t::Table>();
     const auto& boundary = toml_value_at(system, "boundary","[systems.boundary]"
             ).cast<toml::value_t::Table>();
     const auto& particles = toml_value_at(system, "particles", "[system]"
@@ -91,9 +79,12 @@ System<traitsT> read_system(const toml::Table& data, std::size_t N)
         using vec_type = std::array<real_type, 3>;
         const auto& params = particles[i].cast<toml::value_t::Table>();
 
-        sys[i].mass     = toml::get<real_type>(toml_value_at(params, "mass", "element of [[system.particles]]"));
-        sys[i].position = toml::get< vec_type>(toml_value_at(params, "position", "element of [[system.particles]]"));
-        sys[i].velocity = toml::get< vec_type>(toml_value_at(params, "velocity", "element of [[system.particles]]"));
+        sys[i].mass     = toml::get<real_type>(
+            toml_value_at(params, "mass", "element of [[system.particles]]"));
+        sys[i].position = toml::get< vec_type>(
+            toml_value_at(params, "position", "element of [[system.particles]]"));
+        sys[i].velocity = toml::get< vec_type>(
+            toml_value_at(params, "velocity", "element of [[system.particles]]"));
         sys[i].force    = coordinate_type(0, 0, 0);
 
         MJOLNIR_LOG_DEBUG("mass     = ", sys[i].mass    );
@@ -111,6 +102,62 @@ System<traitsT> read_system(const toml::Table& data, std::size_t N)
         sys.attribute(attr.first) = attribute;
     }
     return sys;
+}
+
+template<typename traitsT>
+System<traitsT> read_system(const toml::Table& data, std::size_t N)
+{
+    MJOLNIR_GET_DEFAULT_LOGGER();
+    MJOLNIR_SCOPE(read_system(), 0);
+
+    using real_type       = typename traitsT::real_type;
+    using coordinate_type = typename traitsT::coordinate_type;
+
+    const auto& system_params = toml_value_at(data, "systems", "<root>"
+            ).cast<toml::value_t::Array>();
+    if(system_params.size() <= N)
+    {
+        throw_exception<std::out_of_range>("no enough system definitions: ", N);
+    }
+
+    MJOLNIR_LOG_INFO(system_params.size(), " systems are provided");
+    MJOLNIR_LOG_INFO("using ", N, "-th system");
+
+    const auto& system = system_params.at(N).cast<toml::value_t::Table>();
+    if(system.count("file_name") == 1)
+    {
+        if(system.size() != 1)
+        {
+            std::cerr << "WARNING: [[systems]] has `file_name` key. ";
+            std::cerr << "When `file_name` is provided, all settings ";
+            std::cerr << "will be read from the file, so other fields ";
+            std::cerr << "are ignored.\n";
+            MJOLNIR_LOG_WARN("[[systems]] has `file_name` and other settings");
+        }
+        const std::string file_name =
+            toml::get<std::string>(system.at("file_name"));
+        MJOLNIR_LOG_INFO("file_name = ", file_name);
+
+        const auto system_file = toml::parse(file_name);
+        if(system_file.count("systems") == 1)
+        {
+            std::cerr << "WARNING: each [system] should be provided as a root ";
+            std::cerr << "object of file (" << file_name <<").\n";
+
+            if(system_file.at("systems").type() != toml::value_t::Table)
+            {
+                std::cerr << "FATAL  : [systems] in file `" << file_name;
+                std::cerr << "` is not a toml-table.\n";
+                std::cerr << "       : note: [[...]] means array-of-table. ";
+                std::cerr << "please take care.\n";
+                std::exit(1);
+            }
+            return read_system_from_table<traitsT>(system_file.at("systems"
+                    ).template cast<toml::value_t::Table>());
+        }
+        return read_system_from_table<traitsT>(system_file);
+    }
+    return read_system_from_table<traitsT>(system);
 }
 
 }//mjolnir
