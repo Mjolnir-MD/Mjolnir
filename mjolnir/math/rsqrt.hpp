796553c4109180bd00fb3d3315ebeebdb706fd77
ToruNiina <niina.toru.68u@gmail.com>
2018-08-20 14:29:54 +0900

    use sse/avx instructions if available to compute 1/sqrt(x)

--- mjolnir/math/rsqrt.hpp
+++ mjolnir/math/rsqrt.hpp
@@ -1,65 +1,35 @@
 #ifndef MJOLNIR_MATH_RSQRT
 #define MJOLNIR_MATH_RSQRT
-#include <cstdint>
+#include <cmath>
 
-/* Here, functions that calculates reverse square-root is defined. *
- * `rsqrt(x);` returns `1. / sqrt(x)`.                             */
-
-#ifdef MJOLNIR_HAVE_SSE
-#include <xmmintrin.h>
+#if defined(__SSE__)
+#include <x86intrin.h>
+#endif
 
 namespace mjolnir
 {
 
-inline float rsqrt(float x) noexcept
+template<typename realT>
+inline realT rsqrt(realT x) noexcept
 {
-    float r;
-    _mm_store_ss(&r, _mm_rsqrt_ss(_mm_load_ss(&x)));
-    return r * (3.0f - x * r * r) * 0.5f;
+    return realT(1.0) / std::sqrt(x);
 }
 
-inline double rsqrt(double x) noexcept
+#ifdef __SSE__
+template<>
+inline float rsqrt<float>(float x) noexcept
 {
-    const double xhalf = 0.5 * x;
-    float f = static_cast<float>(x);
-    _mm_store_ss(&f, _mm_rsqrt_ss(_mm_load_ss(&f)));
-    double r = static_cast<double>(f);
-    r *= (1.5 - xhalf * r * r);
-    r *= (1.5 - xhalf * r * r);
-    return r * (1.5 - xhalf * r * r);
+    return _mm_cvtss_f32(_mm_rsqrt_ss(_mm_set_ss(x)));
 }
+#endif
+
+#if defined(__AVX512F__) && defined(__SSE2__)
+template<>
+inline double rsqrt<double>(double x) noexcept
+{
+    return _mm_cvtsd_f64(_mm_rsqrt14_sd(_mm_set_sd(x), _mm_undefined_pd()));
+}
+#endif
 
 } // mjolnir
-
-#else // cpu do not have SSE operation
-
-namespace mjolnir
-{
-
-inline float rsqrt(float x) noexcept
-{
-    const float xhalf = 0.5f * x;
-    std::int32_t i = *(reinterpret_cast<std::int32_t*>(&x));
-    i = 0x5f3759df - (i >> 1);
-    x = *(reinterpret_cast<float*>(&i));
-    x *= (1.5f - xhalf * x * x);
-    return x * (1.5f - xhalf * x * x);
-}
-
-inline double rsqrt(double x) noexcept
-{
-    const double xhalf = 0.5 * x;
-    std::int64_t i = *(reinterpret_cast<std::int64_t*>(&x));
-    i = 0x5fe6ec85e7de30daLL - (i >> 1);
-    x = *(reinterpret_cast<double*>(&i));
-    x *= (1.5 - xhalf * x * x);
-    x *= (1.5 - xhalf * x * x);
-    x *= (1.5 - xhalf * x * x);
-    x *= (1.5 - xhalf * x * x);
-    return x;
-}
-
-}// mjolnir
-
-#endif // HAVE_SSE
 #endif // MJOLNIR_MATH_RSQRT
