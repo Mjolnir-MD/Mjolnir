a3bcb5ffdcf85fe51258f2cfb24d5511b4043c55
ToruNiina <niina.toru.68u@gmail.com>
2018-08-29 17:05:00 +0900

    add neighbor_element for memory optimization
    In C++, all the classes have at least 1 byte size even if the class has
    no element. It's because C++ distinguishes object identity by an address.
    There is a way to compress data size. By using Empty Base Optimization
    (EBO), size of base class becomes zero. Based on this technique, the
    memory usage in NeighborList can be optimized when pre-calculated
    parameter is not needed. It uses less memory than the naive
    implementation with std::pair<index, empty_class> (and also it can be
    expected that it makes performance better because of cache locality).

--- mjolnir/core/NeighborList.hpp
+++ mjolnir/core/NeighborList.hpp
@@ -1,13 +1,12 @@
 #ifndef MJOLNIR_NEIGHBOR_LIST_HPP
 #define MJOLNIR_NEIGHBOR_LIST_HPP
 #include <mjolnir/util/range.hpp>
+#include <mjolnir/util/empty.hpp>
+#include <type_traits>
 #include <algorithm>
 #include <iterator>
 #include <utility>
 #include <vector>
-
-namespace mjolnir
-{
 
 //! a container for verlet-list, cell-list, and other spatial indexing methods.
 //! the objectives are
@@ -15,12 +14,125 @@ namespace mjolnir
 //! - store pre-calculated parameters. e.g. epsilon_ij = sqrt(eps_i * eps_j)
 //!   for Lennard-Jones takes relatively large computational cost to obtain.
 //!   calculate it for all the possible pairs to make force calculation fast.
+
+namespace mjolnir
+{
+
+// google with "empty base optimization(EBO)".
+// by using EBO, it compress the object size when `paramT` is a empty class.
+// without this, empty parameter type consumes 8 byte (on x86_64) because
+// std::size_t requires 8-byte alignment.
+
+namespace detail
+{
+// In order to handle `double` or other non-class object, neighbor_element need
+// another layer that checks `std::is_empty`. If empty, it inherits it and EBO
+// removes the overhead. If not empty(like `double`), it keeps storage for the
+// value. `neighbor_elmenet_impl` stores value if `std::true_type` is passed.
+template<typename paramT, bool IsEmpty>
+struct neighbor_element_impl;
+
+template<typename paramT>
+struct neighbor_element_impl<paramT, true> : private paramT // for EBO
+{
+    using parameter_type = paramT;
+
+    neighbor_element_impl(const parameter_type& p) {}
+    neighbor_element_impl(parameter_type&& p)      {}
+
+    parameter_type&       parameter()       noexcept {return *this;}
+    parameter_type const& parameter() const noexcept {return *this;}
+};
+
+template<typename paramT>
+struct neighbor_element_impl<paramT, false>
+{
+    using parameter_type = paramT;
+
+    neighbor_element_impl(const parameter_type& p) : param(p) {}
+    neighbor_element_impl(parameter_type&& p)      : param(std::move(p)) {}
+
+    parameter_type&       parameter()       noexcept {return param;}
+    parameter_type const& parameter() const noexcept {return param;}
+
+    paramT param;
+};
+
+} // detail
+
+template<typename paramT>
+struct neighbor_element
+: private detail::neighbor_element_impl<paramT, std::is_empty<paramT>::value>
+{
+    using base_type =
+        detail::neighbor_element_impl<paramT, std::is_empty<paramT>::value>;
+    using parameter_type = typename base_type::parameter_type;
+
+    neighbor_element(std::size_t idx, const paramT& p)
+        : base_type(p), index(idx)
+    {}
+    neighbor_element(std::size_t idx, paramT&& p)
+        : base_type(std::move(p)), index(idx)
+    {}
+
+    neighbor_element()  = default;
+    ~neighbor_element() = default;
+    neighbor_element(const neighbor_element&) = default;
+    neighbor_element(neighbor_element&&)      = default;
+    neighbor_element& operator=(const neighbor_element&) = default;
+    neighbor_element& operator=(neighbor_element&&)      = default;
+
+    parameter_type&       parameter()       noexcept {return base_type::parameter();}
+    parameter_type const& parameter() const noexcept {return base_type::parameter();}
+
+    std::size_t index;
+};
+
+static_assert(sizeof(std::size_t) == sizeof(neighbor_element<empty_t>));
+
+template<typename paramT>
+inline bool operator==(
+    const neighbor_element<paramT>& lhs, const neighbor_element<paramT>& rhs)
+{
+    return lhs.index == rhs.index;
+}
+template<typename paramT>
+inline bool operator!=(
+    const neighbor_element<paramT>& lhs, const neighbor_element<paramT>& rhs)
+{
+    return lhs.index != rhs.index;
+}
+template<typename paramT>
+inline bool operator<(
+    const neighbor_element<paramT>& lhs, const neighbor_element<paramT>& rhs)
+{
+    return lhs.index < rhs.index;
+}
+template<typename paramT>
+inline bool operator>(
+    const neighbor_element<paramT>& lhs, const neighbor_element<paramT>& rhs)
+{
+    return lhs.index > rhs.index;
+}
+template<typename paramT>
+inline bool operator<=(
+    const neighbor_element<paramT>& lhs, const neighbor_element<paramT>& rhs)
+{
+    return lhs.index <= rhs.index;
+}
+template<typename paramT>
+inline bool operator>=(
+    const neighbor_element<paramT>& lhs, const neighbor_element<paramT>& rhs)
+{
+    return lhs.index >= rhs.index;
+}
+
 template<typename parameterT>
 class NeighborList
 {
   public:
     using parameter_type = parameterT;
-    using neighbor_type  = std::pair<std::size_t, parameter_type>;
+    using neighbor_type  = neighbor_element<parameter_type>;
     using container_type = std::vector<neighbor_type>;
     using range_type     = range<typename container_type::const_iterator>;
 
@@ -83,8 +195,6 @@ class NeighborList
     std::vector<std::pair<std::size_t, std::size_t>> ranges_;
     // consider to use flat_map when a few particle interacts?
 };
-
-// TODO: when no parameter is needed...
 
 } // mjolnir
 #endif// MJOLNIR_NEIGHBOR_LIST_HPP
