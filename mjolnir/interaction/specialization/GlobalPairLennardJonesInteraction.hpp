0cc00e91b4eaa6787da2c3b2e4baf29e1ec5af8c
ToruNiina <niina.toru.68u@gmail.com>
2018-08-30 15:20:13 +0900

    relax specialization condition for GlobalPair<LennardJones>

--- mjolnir/interaction/specialization/GlobalPairLennardJonesInteraction.hpp
+++ mjolnir/interaction/specialization/GlobalPairLennardJonesInteraction.hpp
@@ -8,20 +8,21 @@
 namespace mjolnir
 {
 
-// specialization for Pair<LennardJones>
-template<typename realT, template<typename, typename> class boundaryT,
+// specialization for GlobalPair<LennardJones>
+template<typename realT,      typename traitsT,
          typename partitionT, typename ignored_chainT>
-class GlobalPairInteraction<
-    SimulatorTraits<realT, boundaryT>,
-    LennardJonesPotential<realT, ignored_chainT>,
-    partitionT
-    > final : public GlobalInteractionBase<SimulatorTraits<realT, boundaryT>>
+class GlobalPairInteraction<traitsT,
+    LennardJonesPotential<realT, ignored_chainT>, partitionT
+    > final : public GlobalInteractionBase<traitsT>
 {
   public:
-    using traits_type     = SimulatorTraits<realT, boundaryT>;
+    static_assert(std::is_same<realT, typename traitsT::real_type>::value,
+        "LennardJones::real_type and traits::real_type should be the same");
+
+    using traits_type     = traitsT;
     using potential_type  = LennardJonesPotential<realT, ignored_chainT>;
     using partition_type  = partitionT;
-    using base_type       = GlobalInteractionBase<SimulatorTraits<realT, boundaryT>>;
+    using base_type       = GlobalInteractionBase<traits_type>;
 
     using real_type       = typename base_type::real_type;
     using coordinate_type = typename base_type::coordinate_type;
