ff086f8432086700789d7200b5b0816dac91bc79
ToruNiina <niina.toru.68u@gmail.com>
2018-09-15 22:17:51 +0900

    include math/constants automatically

--- mjolnir/math/math.hpp
+++ mjolnir/math/math.hpp
@@ -2,6 +2,7 @@
 #define MJOLNIR_MATH_MATH_HPP
 
 #include <mjolnir/math/compiletime.hpp>
+#include <mjolnir/math/constants.hpp>
 #include <mjolnir/math/functions.hpp>
 #include <mjolnir/math/Matrix.hpp>
 #include <mjolnir/math/Vector.hpp>
