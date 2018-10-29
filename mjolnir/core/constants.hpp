4a22b25b317248e6e853ce6a9616208918253a63
ToruNiina <niina.toru.68u@gmail.com>
2018-10-29 14:59:26 +0900

    change unit of charge from C to e
    because usually people do things in such a way. charge +1 means
    1x elementary charge, not 1 [C]. Also, while converting vacuum
    permittivity, it exceeds the range that can be represented by a
    single precision floating point.
    
    Also, added conversion coefficient for mol/L to mol/(nm|A)^3.

--- mjolnir/core/constants.hpp
+++ mjolnir/core/constants.hpp
@@ -1,13 +1,12 @@
 #ifndef MJOLNIR_PHYSICAL_CONSTANTS_HPP
 #define MJOLNIR_PHYSICAL_CONSTANTS_HPP
 #include <mjolnir/core/Unit.hpp>
+#include <string>
 
 namespace mjolnir
 {
 namespace physics
 {
-
-// TODO consider units
 
 template<typename realT>
 struct constants
@@ -15,21 +14,33 @@ struct constants
     typedef realT real_type;
     static real_type kB;   // Boltzmann constant
     static real_type NA;   // Avogadro constant
-    static real_type e;    // elementary charge
     static real_type eps0; // vacuum permittivity
+    static real_type conc_coef; // constant to convert (input mol conc) -> mol/L^3
 };
 
-// initialize with SI units. convert it in read_units() function defined in
-// mjolnir/input/read_units.hpp
+// convert them in read_units() function defined in mjolnir/input/read_units.hpp
+// Here, the unit of electric charge is the elementary charge, not coulomb.
+// to convert it, the value of vacuum permittivity is devided by squared
+// elementary chage.
+
+// XXX: there are 2 reasons why the unit of charge is defined as the elementary
+//      charge. First, usualy charge on particle is defined in such a way (+1
+//      means there are 2x elementary charge). Second, the range that single
+//      precision `float` can represent is too small (1e+/-38) to store the
+//      temporary value while the unit conversion. For example, the value of
+//      vacuum permittivity become 6e-42 in some unit system and that exceeds
+//      the range.
 
 template<typename realT>
 realT constants<realT>::kB = unit::constants<realT>::boltzmann_constant;
 template<typename realT>
 realT constants<realT>::NA = unit::constants<realT>::avogadro_constant;
 template<typename realT>
-realT constants<realT>::e  = unit::constants<realT>::elementary_charge;
+realT constants<realT>::eps0 = (unit::constants<realT>::vacuum_permittivity /
+                                unit::constants<realT>::elementary_charge)  /
+                                unit::constants<realT>::elementary_charge;
 template<typename realT>
-realT constants<realT>::eps0 = unit::constants<realT>::vacuum_permittivity;
+realT constants<realT>::conc_coef = 1.0;
 
 } // physics
 } // mjolnir
