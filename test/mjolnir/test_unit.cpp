8f6063aa868baf7397ccbe1bcc1f76af3d6d7a37
ToruNiina <niina.toru.68u@gmail.com>
2018-11-08 12:07:33 +0900

    add test cases for different unit system

--- test/mjolnir/test_unit.cpp
+++ test/mjolnir/test_unit.cpp
@@ -34,8 +34,8 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(unit_conversion_length, Real, test_targets)
     BOOST_TEST(Real(1.0)   * cs::m_to_nm == Real(1e+9), test::tolerance<Real>());
     BOOST_TEST(cs::nm_to_m * cs::m_to_nm == Real(1.0),  test::tolerance<Real>());
 
-    BOOST_TEST(Real(1.0) * cs::angstrom_to_m == Real(1e-10), test::tolerance<Real>());
-    BOOST_TEST(Real(1.0) * cs::m_to_angstrom == Real(1e+10), test::tolerance<Real>());
+    BOOST_TEST(Real(1.0) * cs::angstrom_to_m == Real(1e-10),       test::tolerance<Real>());
+    BOOST_TEST(Real(1.0) * cs::m_to_angstrom == Real(1e+10),       test::tolerance<Real>());
     BOOST_TEST(cs::angstrom_to_m * cs::m_to_angstrom == Real(1.0), test::tolerance<Real>());
 }
 
@@ -44,8 +44,8 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(unit_conversion_energy, Real, test_targets)
     using cs = mjolnir::unit::constants<Real>;
 
     BOOST_TEST(Real(1.0)    * cs::cal_to_J == Real(4.1868), test::tolerance<Real>());
-    BOOST_TEST(Real(4.1868) * cs::J_to_cal == Real(1.0), test::tolerance<Real>());
-    BOOST_TEST(cs::cal_to_J * cs::J_to_cal == Real(1.0), test::tolerance<Real>());
+    BOOST_TEST(Real(4.1868) * cs::J_to_cal == Real(1.0),    test::tolerance<Real>());
+    BOOST_TEST(cs::cal_to_J * cs::J_to_cal == Real(1.0),    test::tolerance<Real>());
 }
 
 BOOST_AUTO_TEST_CASE_TEMPLATE(read_input_angstrom_kcalmol, Real, test_targets)
@@ -62,14 +62,19 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(read_input_angstrom_kcalmol, Real, test_targets)
     units["length"] = "angstrom"_s;
     units["energy"] = "kcal/mol"_s;
 
+    // update units and physical constants here
+    phys::reset();
     mjolnir::read_units<mjolnir::test::traits<Real>>(input);
 
     BOOST_TEST(phys::m_to_length() == unit::m_to_angstrom, test::tolerance<Real>());
     BOOST_TEST(phys::length_to_m() == unit::angstrom_to_m, test::tolerance<Real>());
 
-    // 1[L] = 1e-3 [m^3]
-    BOOST_TEST(phys::L_to_volume() == unit::m_to_angstrom * unit::m_to_angstrom * unit::m_to_angstrom * Real(1e-3), test::tolerance<Real>());
-    BOOST_TEST(phys::volume_to_L() == unit::angstrom_to_m * unit::angstrom_to_m * unit::angstrom_to_m * Real(1e+3), test::tolerance<Real>());
+    BOOST_TEST(phys::L_to_volume() == Real(1e-3) * // 1[L] = 1e-3 [m^3]
+               unit::m_to_angstrom * unit::m_to_angstrom * unit::m_to_angstrom,
+               test::tolerance<Real>());
+    BOOST_TEST(phys::volume_to_L() == Real(1e+3) * // 1[m^3] = 1e+3 [L]
+               unit::angstrom_to_m * unit::angstrom_to_m * unit::angstrom_to_m,
+               test::tolerance<Real>());
 
     // NA [1/mol], kB [J/K], epsilon_0 [C^2/Jm]
     BOOST_TEST(phys::NA()   == unit::avogadro_constant, test::tolerance<Real>());
@@ -78,5 +83,120 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(read_input_angstrom_kcalmol, Real, test_targets)
     BOOST_TEST(phys::eps0() == unit::vacuum_permittivity / unit::elementary_charge /
                                unit::elementary_charge / unit::J_to_cal / Real(1e-3) /
                                phys::NA() / unit::m_to_angstrom,
+                               test::tolerance<Real>());
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(read_input_angstrom_kJmol, Real, test_targets)
+{
+    mjolnir::LoggerManager::set_default_logger("test_unit.log");
+
+    using namespace mjolnir::literals::string_literals;
+    using phys = mjolnir::physics::constants<Real>;
+    using unit = mjolnir::unit::constants<Real>;
+
+
+    auto input  = mjolnir::test::make_empty_input();
+    auto& units = input["units"].cast<toml::value_t::Table>();
+
+    units["length"] = "angstrom"_s;
+    units["energy"] = "kJ/mol"_s;
+
+    // reset units and physical constants here
+    phys::reset();
+    mjolnir::read_units<mjolnir::test::traits<Real>>(input);
+
+    BOOST_TEST(phys::m_to_length() == unit::m_to_angstrom, test::tolerance<Real>());
+    BOOST_TEST(phys::length_to_m() == unit::angstrom_to_m, test::tolerance<Real>());
+
+    BOOST_TEST(phys::L_to_volume() == Real(1e-3) * // 1[L] = 1e-3 [m^3]
+               unit::m_to_angstrom * unit::m_to_angstrom * unit::m_to_angstrom,
+               test::tolerance<Real>());
+    BOOST_TEST(phys::volume_to_L() == Real(1e+3) * // 1[m^3] = 1e+3 [L]
+               unit::angstrom_to_m * unit::angstrom_to_m * unit::angstrom_to_m,
+               test::tolerance<Real>());
+
+    // NA [1/mol], kB [J/K], epsilon_0 [C^2/Jm]
+    BOOST_TEST(phys::NA()   == unit::avogadro_constant, test::tolerance<Real>());
+    BOOST_TEST(phys::kB()   == unit::boltzmann_constant * Real(1e-3) *
+                               phys::NA(), test::tolerance<Real>());
+    BOOST_TEST(phys::eps0() == unit::vacuum_permittivity / unit::elementary_charge /
+                               unit::elementary_charge  / Real(1e-3) /
+                               phys::NA() / unit::m_to_angstrom,
+                               test::tolerance<Real>());
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(read_input_nm_kcalmol, Real, test_targets)
+{
+    mjolnir::LoggerManager::set_default_logger("test_unit.log");
+
+    using namespace mjolnir::literals::string_literals;
+    using phys = mjolnir::physics::constants<Real>;
+    using unit = mjolnir::unit::constants<Real>;
+
+    auto input = mjolnir::test::make_empty_input();
+    auto& units = input["units"].cast<toml::value_t::Table>();
+
+    units["length"] = "nm"_s;
+    units["energy"] = "kcal/mol"_s;
+
+    // update units and physical constants here
+    phys::reset();
+    mjolnir::read_units<mjolnir::test::traits<Real>>(input);
+
+    BOOST_TEST(phys::m_to_length() == unit::m_to_nm, test::tolerance<Real>());
+    BOOST_TEST(phys::length_to_m() == unit::nm_to_m, test::tolerance<Real>());
+
+    BOOST_TEST(phys::L_to_volume() == Real(1e-3) * // 1[L] = 1e-3 [m^3]
+               unit::m_to_nm * unit::m_to_nm * unit::m_to_nm,
+               test::tolerance<Real>());
+    BOOST_TEST(phys::volume_to_L() == Real(1e+3) * // 1[m^3] = 1e+3 [L]
+               unit::nm_to_m * unit::nm_to_m * unit::nm_to_m,
+               test::tolerance<Real>());
+
+    // NA [1/mol], kB [J/K], epsilon_0 [C^2/Jm]
+    BOOST_TEST(phys::NA()   == unit::avogadro_constant, test::tolerance<Real>());
+    BOOST_TEST(phys::kB()   == unit::boltzmann_constant * unit::J_to_cal * Real(1e-3) *
+                               phys::NA(), test::tolerance<Real>());
+    BOOST_TEST(phys::eps0() == unit::vacuum_permittivity / unit::elementary_charge /
+                               unit::elementary_charge / unit::J_to_cal / Real(1e-3) /
+                               phys::NA() / unit::m_to_nm,
+                               test::tolerance<Real>());
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(read_input_nm_kJmol, Real, test_targets)
+{
+    mjolnir::LoggerManager::set_default_logger("test_unit.log");
+
+    using namespace mjolnir::literals::string_literals;
+    using phys = mjolnir::physics::constants<Real>;
+    using unit = mjolnir::unit::constants<Real>;
+
+    auto input  = mjolnir::test::make_empty_input();
+    auto& units = input["units"].cast<toml::value_t::Table>();
+
+    units["length"] = "nm"_s;
+    units["energy"] = "kJ/mol"_s;
+
+    // update units and physical constants here
+    phys::reset();
+    mjolnir::read_units<mjolnir::test::traits<Real>>(input);
+
+    BOOST_TEST(phys::m_to_length() == unit::m_to_nm, test::tolerance<Real>());
+    BOOST_TEST(phys::length_to_m() == unit::nm_to_m, test::tolerance<Real>());
+
+    BOOST_TEST(phys::L_to_volume() == Real(1e-3) * // 1[L] = 1e-3 [m^3]
+               unit::m_to_nm * unit::m_to_nm * unit::m_to_nm,
+               test::tolerance<Real>());
+    BOOST_TEST(phys::volume_to_L() == Real(1e+3) * // 1[m^3] = 1e+3 [L]
+               unit::nm_to_m * unit::nm_to_m * unit::nm_to_m,
+               test::tolerance<Real>());
+
+    // NA [1/mol], kB [J/K], epsilon_0 [C^2/Jm]
+    BOOST_TEST(phys::NA()   == unit::avogadro_constant, test::tolerance<Real>());
+    BOOST_TEST(phys::kB()   == unit::boltzmann_constant * Real(1e-3) *
+                               phys::NA(), test::tolerance<Real>());
+    BOOST_TEST(phys::eps0() == unit::vacuum_permittivity / unit::elementary_charge /
+                               unit::elementary_charge  / Real(1e-3) /
+                               phys::NA() / unit::m_to_nm,
                                test::tolerance<Real>());
 }
