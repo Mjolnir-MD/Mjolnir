1dc53fb5bdab922f822086ba9f119a2f03d5f4df
ToruNiina <niina.toru.68u@gmail.com>
2018-11-12 10:56:34 +0900

    remove unused functions from RNG

--- mjolnir/core/RandomNumberGenerator.hpp
+++ mjolnir/core/RandomNumberGenerator.hpp
@@ -23,14 +23,6 @@ class RandomNumberGenerator
     real_type uniform_real(const real_type min, const real_type max);
     real_type gaussian(const real_type mean, const real_type sigma);
 
-    coordinate_type
-    maxwell_boltzmann(const real_type mass, const real_type temperature,
-                      const real_type kB);
-
-    coordinate_type
-    underdamped_langevin(const real_type mass, const real_type gamma,
-        const real_type dt, const real_type temperature, const real_type kB);
-
   private:
     const std::uint32_t seed_;
     std::mt19937 rng_;
@@ -50,25 +42,6 @@ RandomNumberGenerator<traitsT>::gaussian(
     const real_type mean, const real_type sigma)
 {
     return (std::normal_distribution<real_type>(mean, sigma))(rng_);
-}
-
-template<typename traitsT>
-inline typename RandomNumberGenerator<traitsT>::coordinate_type
-RandomNumberGenerator<traitsT>::maxwell_boltzmann(
-    const real_type m, const real_type T, const real_type kB)
-{
-    std::normal_distribution<real_type> distro(0., std::sqrt(kB * T / m));
-    return coordinate_type(distro(rng_), distro(rng_), distro(rng_));
-}
-
-template<typename traitsT>
-inline typename RandomNumberGenerator<traitsT>::coordinate_type
-RandomNumberGenerator<traitsT>::underdamped_langevin(const real_type m,
-        const real_type g, const real_type dt, const real_type T, const real_type kB)
-{
-    std::normal_distribution<real_type>
-        distro(0., std::sqrt(2 * kB * T * g / (m * dt)));
-    return coordinate_type(distro(rng_), distro(rng_), distro(rng_));
 }
 
 } // mjolnir
