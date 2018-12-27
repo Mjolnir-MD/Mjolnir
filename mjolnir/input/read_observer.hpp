2724427a3c766ff6e87a1fa1382fb5e88b8fb1a6
ToruNiina <niina.toru.68u@gmail.com>
2018-12-28 01:46:39 +0900

    refactor read_observer

--- mjolnir/input/read_observer.hpp
+++ mjolnir/input/read_observer.hpp
@@ -2,7 +2,6 @@
 #define MJOLNIR_READ_OBSERVER
 #include <extlib/toml/toml.hpp>
 #include <mjolnir/core/Observer.hpp>
-#include <mjolnir/util/get_toml_value.hpp>
 #include <mjolnir/util/logger.hpp>
 
 namespace mjolnir
@@ -10,29 +9,21 @@ namespace mjolnir
 
 template<typename traitsT>
 Observer<traitsT>
-read_observer(const toml::Table& data)
+read_observer(const toml::table& root)
 {
     MJOLNIR_GET_DEFAULT_LOGGER();
     MJOLNIR_SCOPE(read_observer(), 0);
 
-    const auto& files =
-        get_toml_value<toml::Table>(data, "files", "<root>");
-    const auto& output =
-        get_toml_value<toml::Table>(files, "output", "[files]");
+    const auto& files  = toml::find<toml::value>(root, "files");
+    const auto& output = toml::find<toml::value>(files, "output");
 
     std::string output_path("./");
-    if(output.count("path") == 1)
+    if(toml::get<toml::table>(output).count("path") == 1)
     {
-        output_path = get_toml_value<std::string>(output, "path", "[files.output]");
+        output_path = toml::find<std::string>(output, "path");
+        if(output_path.back() != '/') {output_path += '/';}
     }
-    if(output_path.back() != '/') {output_path += '/';}
-
-    const auto output_prefix = get_toml_value<std::string>(
-            output, "prefix", "[files.output]");
-
-    MJOLNIR_LOG_INFO("path   = ", output_path);
-    MJOLNIR_LOG_INFO("prefix = ", output_prefix);
-
+    const auto output_prefix = toml::find<std::string>(output, "prefix");
     MJOLNIR_LOG_NOTICE("output files are `", output_path, output_prefix, ".*`");
 
     return Observer<traitsT>(output_path + output_prefix);
