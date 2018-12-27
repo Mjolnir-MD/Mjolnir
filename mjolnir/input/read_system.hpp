e86ad98278bd6eacd51e84d4d5d243ffd01b20f4
ToruNiina <niina.toru.68u@gmail.com>
2018-12-28 01:37:20 +0900

    remove needless header file

--- mjolnir/input/read_system.hpp
+++ mjolnir/input/read_system.hpp
@@ -4,7 +4,6 @@
 #include <mjolnir/core/System.hpp>
 #include <mjolnir/core/BoundaryCondition.hpp>
 #include <mjolnir/util/throw_exception.hpp>
-#include <mjolnir/util/get_toml_value.hpp>
 #include <mjolnir/util/logger.hpp>
 #include <mjolnir/input/read_files_table.hpp>
 
