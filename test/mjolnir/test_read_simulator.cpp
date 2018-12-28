567cf197065649f23ef4cfb1535ded90c96eb907
ToruNiina <niina.toru.68u@gmail.com>
2018-12-28 13:52:09 +0900

    improve test_read_simulator to check the steps

--- test/mjolnir/test_read_simulator.cpp
+++ test/mjolnir/test_read_simulator.cpp
@@ -4,7 +4,7 @@
 #include <test/util/make_empty_input.hpp>
 #include <mjolnir/input/read_simulator.hpp>
 
-BOOST_AUTO_TEST_CASE(read_simulator_from_table)
+BOOST_AUTO_TEST_CASE(read_newtonian_molecular_dynamics_simulator)
 {
     mjolnir::LoggerManager::set_default_logger("test_read_simulator.log");
 
@@ -29,7 +29,28 @@ BOOST_AUTO_TEST_CASE(read_simulator_from_table)
         const auto mdsim = dynamic_cast<mjolnir::MolecularDynamicsSimulator<
             traits_type, mjolnir::VelocityVerletStepper<traits_type>>*>(sim.get());
         BOOST_TEST(static_cast<bool>(mdsim));
+
+        sim->initialize();
+        for(std::size_t i=0; i<99; ++i)
+        {
+            BOOST_TEST(mdsim->time() == i * 0.1, boost::test_tools::tolerance(tol));
+            BOOST_TEST(sim->step()); // check it can step
+        }
+        // at the last (100-th) step, it returns false to stop the simulation.
+        BOOST_TEST(!sim->step());
+        sim->finalize();
     }
+}
+
+BOOST_AUTO_TEST_CASE(read_langevin_molecular_dynamics_simulator)
+{
+    mjolnir::LoggerManager::set_default_logger("test_read_simulator.log");
+
+    using real_type = double;
+    using traits_type = mjolnir::SimulatorTraits<real_type, mjolnir::UnlimitedBoundary>;
+    constexpr real_type tol = 1e-8;
+    auto root = mjolnir::test::make_empty_input();
+
     {
         const toml::table v{
             {"type",          toml::value("Molecular Dynamics")},
@@ -49,8 +70,27 @@ BOOST_AUTO_TEST_CASE(read_simulator_from_table)
         const auto mdsim = dynamic_cast<mjolnir::MolecularDynamicsSimulator<
             traits_type, mjolnir::UnderdampedLangevinStepper<traits_type>>*>(sim.get());
         BOOST_TEST(static_cast<bool>(mdsim));
+
+        sim->initialize();
+        for(std::size_t i=0; i<99; ++i)
+        {
+            BOOST_TEST(mdsim->time() == i * 0.1, boost::test_tools::tolerance(tol));
+            BOOST_TEST(sim->step()); // check it can step
+        }
+        // at the last (100-th) step, it returns false to stop the simulation.
+        BOOST_TEST(!sim->step());
+        sim->finalize();
     }
-    // Steepest Descent
+}
+
+BOOST_AUTO_TEST_CASE(read_steepest_descent_simulator)
+{
+    mjolnir::LoggerManager::set_default_logger("test_read_simulator.log");
+
+    using real_type = double;
+    using traits_type = mjolnir::SimulatorTraits<real_type, mjolnir::UnlimitedBoundary>;
+    constexpr real_type tol = 1e-8;
+    auto root = mjolnir::test::make_empty_input();
     {
         const toml::table v{
             {"type",          toml::value("Steepest Descent")},
@@ -59,7 +99,7 @@ BOOST_AUTO_TEST_CASE(read_simulator_from_table)
             {"step_limit",    toml::value(100)},
             {"save_step",     toml::value(10)},
             {"delta",         toml::value(0.1)},
-            {"threshold",     toml::value(0.1)}
+            {"threshold",     toml::value(0.0)} // it never ends until hit the limit
         };
         root["simulator"] = v;
         const auto sim = mjolnir::read_simulator_from_table<traits_type>(root, v);
@@ -68,8 +108,26 @@ BOOST_AUTO_TEST_CASE(read_simulator_from_table)
         const auto sdsim = dynamic_cast<
             mjolnir::SteepestDescentSimulator<traits_type>*>(sim.get());
         BOOST_TEST(static_cast<bool>(sdsim));
+
+        sim->initialize();
+        for(std::size_t i=0; i<99; ++i)
+        {
+            BOOST_TEST(sim->step()); // check it can step
+        }
+        // at the last (100-th) step, it returns false to stop the simulation.
+        BOOST_TEST(!sim->step());
+        sim->finalize();
     }
-    // Simulated Annealing
+}
+
+BOOST_AUTO_TEST_CASE(read_simulated_annealing_simulator)
+{
+    mjolnir::LoggerManager::set_default_logger("test_read_simulator.log");
+
+    using real_type = double;
+    using traits_type = mjolnir::SimulatorTraits<real_type, mjolnir::UnlimitedBoundary>;
+    constexpr real_type tol = 1e-8;
+    auto root = mjolnir::test::make_empty_input();
     {
         const toml::table v{
             {"type",          toml::value("Simulated Annealing")},
@@ -81,7 +139,7 @@ BOOST_AUTO_TEST_CASE(read_simulator_from_table)
             {"schedule",      toml::value("linear")},
             {"T_begin",       toml::value(300.0)},
             {"T_end",         toml::value( 10.0)},
-            {"each_step",     toml::value( 10)},
+            {"each_step",     toml::value( 1)},
             {"delta_t",       toml::value(0.1)},
             {"seed",          toml::value(12345)},
             {"parameters",    toml::array{}}
@@ -94,5 +152,17 @@ BOOST_AUTO_TEST_CASE(read_simulator_from_table)
             traits_type, mjolnir::UnderdampedLangevinStepper<traits_type>,
             mjolnir::linear_schedule>*>(sim.get());
         BOOST_TEST(static_cast<bool>(sasim));
+
+        sim->initialize();
+        for(std::size_t i=0; i<99; ++i)
+        {
+            BOOST_TEST(sasim->system().attribute("temperature") ==
+                       300.0 * ((100-i) / 100.0) + 10.0 * (i / 100.0),
+                       boost::test_tools::tolerance(tol));
+            BOOST_TEST(sim->step()); // check it can step
+        }
+        // at the last (100-th) step, it returns false to stop the simulation.
+        BOOST_TEST(!sim->step());
+        sim->finalize();
     }
 }
