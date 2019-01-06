b7f5064a12c2fb36d8d30407f566a46a88373059
ToruNiina <niina.toru.68u@gmail.com>
2019-01-06 20:12:02 +0900

    read files.output.format and return ptr

--- mjolnir/input/read_observer.hpp
+++ mjolnir/input/read_observer.hpp
@@ -1,14 +1,16 @@
-#ifndef MJOLNIR_READ_OBSERVER
-#define MJOLNIR_READ_OBSERVER
+#ifndef MJOLNIR_READ_OBSERVER_HPP
+#define MJOLNIR_READ_OBSERVER_HPP
 #include <extlib/toml/toml.hpp>
+#include <mjolnir/core/ObserverBase.hpp>
 #include <mjolnir/core/Observer.hpp>
 #include <mjolnir/util/logger.hpp>
+#include <mjolnir/util/make_unique.hpp>
 
 namespace mjolnir
 {
 
 template<typename traitsT>
-Observer<traitsT>
+std::unique_ptr<ObserverBase<traitsT>>
 read_observer(const toml::table& root)
 {
     MJOLNIR_GET_DEFAULT_LOGGER();
@@ -29,8 +31,25 @@ read_observer(const toml::table& root)
     const auto output_prefix = toml::find<std::string>(output, "prefix");
     MJOLNIR_LOG_NOTICE("output files are `", output_path, output_prefix, ".*`");
 
-    return Observer<traitsT>(output_path + output_prefix, progress_bar);
+    const std::string file_prefix = output_path + output_prefix;
+
+    const auto& format = toml::find<std::string>(output, "format");
+    if(format == "xyz")
+    {
+        using observer_type = Observer<traitsT>;
+        MJOLNIR_LOG_NOTICE("output format is xyz.");
+        return make_unique<observer_type>(file_prefix, progress_bar);
+    }
+    else
+    {
+        throw_exception<std::runtime_error>(toml::format_error("[error] "
+            "mjolnir::read_observer: output format not supported",
+            toml::find(output, "format"), "here", {
+            "expected one of the following.",
+            "- \"xyz\": the simplest ascii format",
+            }));
+    }
 }
 
 } // mjolnir
-#endif// MJOLNIR_READ_OBSERVER
+#endif// MJOLNIR_READ_OBSERVER_HPP
