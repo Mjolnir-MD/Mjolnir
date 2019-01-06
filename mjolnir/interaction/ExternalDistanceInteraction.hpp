6c20cf3606905ea4bf4bbb6a07a886c3c28e581b
ToruNiina <niina.toru.68u@gmail.com>
2019-01-06 14:13:51 +0900

    fix ExternalDistanceInteraction

--- mjolnir/interaction/ExternalDistanceInteraction.hpp
+++ mjolnir/interaction/ExternalDistanceInteraction.hpp
@@ -42,7 +42,6 @@ class ExternalDistanceInteraction final
     {
         this->potential_.update(sys); // update system parameters
         this->shape_.initialize(sys, this->potential_);
-        this->shape_.update(sys);
     }
 
     /*! @brief update parameters (e.g. temperature, ionic strength, ...)  *
@@ -73,8 +72,6 @@ template<typename traitsT, typename potT, typename spaceT>
 void ExternalDistanceInteraction<traitsT, potT, spaceT>::calc_force(
         system_type& sys)
 {
-    this->shape_.update(sys); // update neighbor list...
-
     for(std::size_t i : this->shape_.neighbors())
     {
         const auto& ri = sys[i].position;
