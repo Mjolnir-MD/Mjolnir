62a46f87fe9658b496781153d7276c719c558e9c
ToruNiina <niina.toru.68u@gmail.com>
2019-03-10 17:23:49 +0900

    feat: add utility macro

---
+++ mjolnir/util/macro.hpp
@@ -0,0 +1,21 @@
+#ifndef MJOLNIR_UTIL_MACRO_HPP
+#define MJOLNIR_UTIL_MACRO_HPP
+
+// MJOLNIR_STRINGIZE(hoge) -> "hoge"
+#define MJOLNIR_STRINGIZE_AUX(x) #x
+#define MJOLNIR_STRINGIZE(x)     MJOLNIR_STRINGIZE_AUX(x)
+
+// MJOLNIR_FUNC_NAME, expanded into function name.
+//
+// __PRETTY_FUNCTION__ is too informative. since it shows the template argument,
+// the function name become too long.
+//
+// #if defined(__GNUC__)
+// #  define MJOLNIR_FUNC_NAME __PRETTY_FUNCTION__
+// #elif defined(_MSC_VER)
+// #  define MJOLNIR_FUNC_NAME __FUNCSIG__
+// #else
+#define MJOLNIR_FUNC_NAME __func__
+// #endif
+
+#endif // MJOLNIR_UTIL_MACRO_HPP
