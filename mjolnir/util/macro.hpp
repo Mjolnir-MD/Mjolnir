d25dd1e0830da750aebf754308a51010b8e7a3b5
ToruNiina <niina.toru.68u@gmail.com>
2019-03-11 16:12:33 +0900

    feat: output verbose function name if DEBUG=ON

--- mjolnir/util/macro.hpp
+++ mjolnir/util/macro.hpp
@@ -9,13 +9,19 @@
 //
 // __PRETTY_FUNCTION__ is too informative. since it shows the template argument,
 // the function name become too long.
-//
-// #if defined(__GNUC__)
-// #  define MJOLNIR_FUNC_NAME __PRETTY_FUNCTION__
-// #elif defined(_MSC_VER)
-// #  define MJOLNIR_FUNC_NAME __FUNCSIG__
-// #else
-#define MJOLNIR_FUNC_NAME __func__
+
+#if defined(MJOLNIR_DEBUG)
+// debug mode. print more verbose function name.
+#  if defined(__GNUC__)
+#    define MJOLNIR_FUNC_NAME __PRETTY_FUNCTION__
+#  elif defined(_MSC_VER)
+#    define MJOLNIR_FUNC_NAME __FUNCSIG__
+#  else
+#    define MJOLNIR_FUNC_NAME __func__
+#  endif
+#else // release mode. simplify function names.
+#  define MJOLNIR_FUNC_NAME __func__
+#endif
 // #endif
 
 #endif // MJOLNIR_UTIL_MACRO_HPP
