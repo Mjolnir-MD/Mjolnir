54ea66aaf7eb4d083ee6a767a0f9371b76c246f4
ToruNiina <niina.toru.68u@gmail.com>
2019-03-14 21:10:04 +0900

    feat: allow array for `files.output.format`
    allow the following input.
    ```toml
    [files]
    output.format = ["xyz", "dcd"]
    ```

--- mjolnir/input/read_observer.hpp
+++ mjolnir/input/read_observer.hpp
@@ -12,6 +12,46 @@ namespace mjolnir
 {
 
 template<typename traitsT>
+void add_observer(ObserverContainer<traitsT>& observers,
+                  const toml::value& format, const std::string& file_prefix)
+{
+    MJOLNIR_GET_DEFAULT_LOGGER();
+    MJOLNIR_LOG_FUNCTION();
+
+    // To show the informative error message, here it uses toml::value that
+    // contains file location. But this function assumes that the `format`
+    // contains `toml::string`.
+    assert(format.is(toml::value_t::String));
+
+    if(format == "xyz")
+    {
+        using observer_type = XYZObserver<traitsT>;
+        MJOLNIR_LOG_NOTICE("output xyz format.");
+        observers.push_back(make_unique<observer_type>(file_prefix));
+        return;
+    }
+    if(format == "dcd")
+    {
+        using observer_type = DCDObserver<traitsT>;
+        MJOLNIR_LOG_NOTICE("output dcd format.");
+        observers.push_back(make_unique<observer_type>(file_prefix));
+        return;
+    }
+    else
+    {
+        throw_exception<std::runtime_error>(toml::format_error("[error] "
+            "mjolnir::read_observer: output format not supported", format,
+            "here", {
+                "expected one of the following.",
+                "- \"xyz\": the simplest ascii format",
+                "- \"dcd\": widely used binary format"
+            }));
+    }
+    return ;
+}
+
+
+template<typename traitsT>
 ObserverContainer<traitsT>
 read_observer(const toml::table& root)
 {
@@ -23,7 +63,6 @@ read_observer(const toml::table& root)
 
     const auto progress_bar_enabled = toml::expect<bool>(output, "progress_bar")
                                                         .unwrap_or(true);
-
     std::string output_path("./");
     if(toml::get<toml::table>(output).count("path") == 1)
     {
@@ -31,40 +70,32 @@ read_observer(const toml::table& root)
         if(output_path.back() != '/') {output_path += '/';}
     }
     const auto output_prefix = toml::find<std::string>(output, "prefix");
-    MJOLNIR_LOG_NOTICE("output files are `", output_path, output_prefix, ".*`");
+    MJOLNIR_LOG_NOTICE("output file prefix is `", output_path, output_prefix, '`');
 
     const std::string file_prefix = output_path + output_prefix;
+
+    // ------------------------------------------------------------------------
 
     ObserverContainer<traitsT> observers(progress_bar_enabled);
 
     // Energy is always written to "prefix.ene".
     observers.push_back(make_unique<EnergyObserver<traitsT>>(file_prefix));
 
-    const auto& format = toml::find<std::string>(output, "format");
-    if(format == "xyz")
+    const auto& format = toml::find(output, "format");
+
+    if(format.is(toml::value_t::String))
     {
-        using observer_type = XYZObserver<traitsT>;
-        MJOLNIR_LOG_NOTICE("output format is xyz.");
-        observers.push_back(make_unique<observer_type>(file_prefix));
-        return observers;
+        add_observer(observers, format, file_prefix);
     }
-    if(format == "dcd")
+    else if(format.is(toml::value_t::Array))
     {
-        using observer_type = DCDObserver<traitsT>;
-        MJOLNIR_LOG_NOTICE("output format is dcd.");
-        observers.push_back(make_unique<observer_type>(file_prefix));
-        return observers;
+        const auto fmts = toml::get<toml::array>(format);
+        for(const auto& fmt : fmts)
+        {
+            add_observer(observers, fmt, file_prefix);
+        }
     }
-    else
-    {
-        throw_exception<std::runtime_error>(toml::format_error("[error] "
-            "mjolnir::read_observer: output format not supported",
-            toml::find(output, "format"), "here", {
-            "expected one of the following.",
-            "- \"xyz\": the simplest ascii format",
-            "- \"dcd\": widely used DCD format"
-            }));
-    }
+    return observers;
 }
 
 } // mjolnir
