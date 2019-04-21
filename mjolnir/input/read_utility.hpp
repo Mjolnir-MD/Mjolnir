61203ea539091cc18488155174a99f85e0611b23
ToruNiina <niina.toru.68u@gmail.com>
2019-04-21 15:39:20 +0900

    feat: add read_array to improve array input
    This commit adds a utility function to read an array of parameters.
    
    not only the array itself (like the following code),
    ```toml
    v = [
      {index = 0, a = 1.0, b = 10}
    ]
    ```
    allow table input with size and default value.
    ```toml
    v.size = 100
    v.default = {a = 1.0, b = 10}
    v.values = [
      {index = 10, a = 2.0, b = 42} # overwrite special value
    ]
    ```

---
+++ mjolnir/input/read_utility.hpp
@@ -0,0 +1,156 @@
+#ifndef MJOLNIR_INPUT_READ_UTILITY_HPP
+#define MJOLNIR_INPUT_READ_UTILITY_HPP
+#include <mjolnir/util/throw_exception.hpp>
+#include <extlib/toml/toml.hpp>
+#include <type_traits>
+#include <algorithm>
+#include <stdexcept>
+#include <vector>
+
+namespace mjolnir
+{
+
+// this function reads array of structs from a toml value like the following.
+// ```toml
+// v = [
+//    {index = 0, a = 1.0, b = 100},
+//    {index = 1, a = 1.0, b = 100},
+//    ...
+//    {index =99, a = 1.0, b = 100},
+// ]
+// ```
+// or 
+// ```toml
+// v.size    = 100                    # length
+// v.default = {a = 1.0, b = 100}     # default value
+// v.values  = [
+//     {index = 10, a = 1.0, b = 100} # overwrite special values
+// ]
+// ```
+//
+// In order to read a table like `{a = 1.0, b = 100}` as a struct like
+// `std::pair<double, double>`, we need to pass a function that converts
+// `toml::value` to `std::pair<double, double>`. So the function receives
+// `reader(const toml::value&) -> T`.
+//
+// Thus the reader MUST ignore a key named "index".
+//
+// Since we need to convert several kinds of tables that has different keys
+// into the same struct like `std::pair<double, double>`, `toml::from` is
+// not helpful because it can only be implemented once for one struct.
+template<typename T, typename F>
+std::vector<T> read_array(const toml::value& v, F&& reader)
+{
+    static_assert(std::is_default_constructible<T>::value, "");
+    if(v.is_array())
+    {
+        const auto& arr = toml::get<toml::array>(v);
+        if(arr.empty())
+        {
+            return std::vector<T>{};
+        }
+
+        std::vector<T> vec(arr.size());
+        if(arr.front().is_table())
+        {
+            std::string msg;
+            bool maybe_exhaustive = true;
+            std::vector<std::size_t> idxs; idxs.reserve(arr.size());
+
+            for(const toml::value& item : arr)
+            {
+                const auto i = toml::find<std::size_t>(item, "index");
+                if(i >= vec.size())
+                {
+                    // the index exceeds the size of the array. it cannot be
+                    // exhaustive. break the loop and throw an error.
+                    maybe_exhaustive = false;
+                    msg = toml::format_error("the index exceeds the number of "
+                                             "parameters", item, "here");
+                    break;
+                }
+                vec.at(i) = reader(item);
+                idxs.push_back(i);
+            }
+
+            if(maybe_exhaustive)
+            {
+                // check all the elements are correctly initialized
+                if(!std::is_sorted(idxs.begin(), idxs.end()))
+                {
+                    std::sort(idxs.begin(), idxs.end());
+                }
+                const auto uniqued = std::unique(idxs.begin(), idxs.end());
+                if(uniqued == idxs.end())
+                {
+                    // all the values are initialized. the values are okay.
+                    return vec;
+                }
+                // otherwise, not okay. raise an error.
+                std::ostringstream oss;
+                oss << "indexes ";
+                for(auto iter = uniqued; iter!=idxs.end(); ++iter)
+                {
+                    oss << *iter << ' ';
+                }
+                oss << "are overlapping.";
+                msg = oss.str();
+            }
+            throw_exception<std::runtime_error>(toml::format_error("[error] "
+                "mjolnir::read_array: values are not exhaustive.",
+                v, "index may overlap or exceeds an range.", {msg}));
+        }
+        // otherwise, format error. throw an exception.
+    }
+    else if(v.is_table())
+    {
+        // v.size    = 100                    # length
+        // v.default = {a = 1.0, b = 100}     # default value
+        // v.values  = [
+        //     {index = 10, a = 1.0, b = 100} # overwrite special values
+        // ]
+        const auto& tab = toml::get<toml::table>(v);
+        if(tab.count("size") == 1 && tab.count("default") == 1)
+        {
+            const auto len  = toml::find<std::size_t>(v, "size");
+            const auto dflt = reader(toml::find<toml::value>(v, "default"));
+            std::vector<T> vec(len, dflt);
+
+            if(tab.count("values") == 1)
+            {
+                for(const auto& sp_val : toml::find<toml::array>(v, "values"))
+                {
+                    const auto idx = toml::find<std::size_t>(sp_val, "index");
+                    if(idx >= len)
+                    {
+                        throw_exception<std::runtime_error>(toml::format_error(
+                            "[error] mjolnir::read_array: the index exceeds the"
+                            " length of array", sp_val, "here"));
+                    }
+                    vec.at(idx) = reader(sp_val);
+                }
+            }
+            return vec;
+        }
+        // otherwise (if no `size` and `default` exist), raise format error.
+    }
+    throw_exception<std::runtime_error>(toml::format_error("[error] "
+        "mjolnir::read_array: invalid format for an array of parameters",
+        v, "here", {"expected array of parameters. the format is:",
+        "key = [ # write all parameters explicitly",
+        "    {index=0, a=1.0, b=100},",
+        "    ...",
+        "]",
+        "or",
+        "key.size    = 100",
+        "key.default = {a=1.0, b=100}",
+        "key.values  = [",
+        "    {index = 10, a = 2.0, b=10}, # overwrite parameter for i = 10",
+        "    ...",
+        "]"
+        }));
+}
+
+
+} // mjolnir
+#endif// MJOLNIR_INPUT_READ_UTILITY_HPP
---
+++ test/mjolnir/test_read_utility.cpp
@@ -0,0 +1,198 @@
+#define BOOST_TEST_MODULE "test_bond_angle_interaction"
+#ifdef BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+#else
+#include <boost/test/included/unit_test.hpp>
+#endif
+
+#include <mjolnir/input/read_utility.hpp>
+
+BOOST_AUTO_TEST_CASE(test_read_array_from_table)
+{
+    const auto reader = [](const toml::value& v) -> std::pair<double, int> {
+        const auto a = toml::find<double>(v, "a");
+        const auto b = toml::find<int   >(v, "b");
+        return std::make_pair(a, b);
+    };
+
+    using namespace toml::literals;
+    {
+        const toml::value v = u8R"(
+            size    = 10
+            default = {a = 1.0, b = 10}
+            )"_toml;
+
+        const auto result =
+            mjolnir::read_array<std::pair<double, int>>(v, reader);
+
+        BOOST_TEST(result.size()  == 10u);
+        for(const auto& item : result)
+        {
+            BOOST_TEST(item.first  == 1.0);
+            BOOST_TEST(item.second ==  10);
+        }
+    }
+    {
+        const toml::value v = u8R"(
+            size    = 10
+            default = {a = 1.0, b = 10}
+            values  = [
+                # overwrite special values
+                {index = 0, a = 3.14, b = 42  },
+                {index = 5, b = 54,   a = 2.71},
+            ]
+            )"_toml;
+
+        const auto result =
+            mjolnir::read_array<std::pair<double, int>>(v, reader);
+
+        BOOST_TEST(result.size()  == 10u);
+        for(std::size_t i=0; i<10; ++i)
+        {
+            const auto& item = result.at(i);
+            if(i == 0)
+            {
+                BOOST_TEST(item.first  == 3.14);
+                BOOST_TEST(item.second ==   42);
+            }
+            else if(i == 5)
+            {
+                BOOST_TEST(item.first  == 2.71);
+                BOOST_TEST(item.second ==   54);
+            }
+            else
+            {
+                BOOST_TEST(item.first  == 1.0);
+                BOOST_TEST(item.second ==  10);
+            }
+        }
+    }
+    {
+        const toml::value v = u8R"(
+            size = 10
+            # no default value
+            )"_toml;
+
+        bool thrown = false;
+        try
+        {
+            const auto result =
+                mjolnir::read_array<std::pair<double, int>>(v, reader);
+        }
+        catch(const std::runtime_error& re)
+        {
+            std::cout << "what: " << re.what() << std::endl;
+            thrown = true;
+        }
+        BOOST_TEST(thrown);
+    }
+    {
+        const toml::value v = u8R"(
+            # no size
+            default = {a = 1.0, b = 10}
+            )"_toml;
+
+        bool thrown = false;
+        try
+        {
+            const auto result =
+                mjolnir::read_array<std::pair<double, int>>(v, reader);
+        }
+        catch(const std::runtime_error& re)
+        {
+            std::cout << "what: " << re.what() << std::endl;
+            thrown = true;
+        }
+        BOOST_TEST(thrown);
+    }
+    {
+        const toml::value v = u8R"(
+            size    = 10
+            default = {a = 1.0, b = 10}
+            values  = [
+                {index = 100, a = 10.0, b = 42} # index exceeds the size
+            ]
+            )"_toml;
+
+        bool thrown = false;
+        try
+        {
+            const auto result =
+                mjolnir::read_array<std::pair<double, int>>(v, reader);
+        }
+        catch(const std::runtime_error& re)
+        {
+            std::cout << "what: " << re.what() << std::endl;
+            thrown = true;
+        }
+        BOOST_TEST(thrown);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(test_read_array_from_array)
+{
+    const auto reader = [](const toml::value& v) -> std::pair<double, int> {
+        const auto a = toml::find<double>(v, "a");
+        const auto b = toml::find<int   >(v, "b");
+        return std::make_pair(a, b);
+    };
+
+    using namespace toml::literals;
+    {
+        const toml::value v = u8R"([
+                {index = 0, a = 1.00, b = 10},
+                {index = 1, a = 3.14, b = 42},
+                {index = 2, a = 2.71, b = 54},
+            ])"_toml;
+
+        const auto result =
+            mjolnir::read_array<std::pair<double, int>>(v, reader);
+
+        BOOST_TEST(result.size()  == 3u);
+        BOOST_TEST(result.at(0).first     == 1.00);
+        BOOST_TEST(result.at(0).second    ==   10);
+        BOOST_TEST(result.at(1).first     == 3.14);
+        BOOST_TEST(result.at(1).second    ==   42);
+        BOOST_TEST(result.at(2).first     == 2.71);
+        BOOST_TEST(result.at(2).second    ==   54);
+    }
+
+    {
+        const toml::value v = u8R"([
+                {index = 100, a = 1.00, b = 10}, # range exceeds
+            ])"_toml;
+
+        bool thrown = false;
+        try
+        {
+            const auto result =
+                mjolnir::read_array<std::pair<double, int>>(v, reader);
+        }
+        catch(const std::runtime_error& re)
+        {
+            std::cout << "what: " << re.what() << std::endl;
+            thrown = true;
+        }
+        BOOST_TEST(thrown);
+    }
+
+    {
+        const toml::value v = u8R"([
+                {index = 0, a = 1.00, b = 10}, # range [0, 1] not exhaustive
+                {index = 0, a = 1.00, b = 10}, # because it is also 0-th value
+            ])"_toml;
+
+        bool thrown = false;
+        try
+        {
+            const auto result =
+                mjolnir::read_array<std::pair<double, int>>(v, reader);
+        }
+        catch(const std::runtime_error& re)
+        {
+            std::cout << "what: " << re.what() << std::endl;
+            thrown = true;
+        }
+        BOOST_TEST(thrown);
+    }
+}
