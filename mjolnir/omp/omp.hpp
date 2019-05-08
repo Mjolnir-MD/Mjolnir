de7c1dd6a061d1e1393548a7b4fbb89580d9ead9
ToruNiina <niina.toru.68u@gmail.com>
2019-05-08 12:02:22 +0900

    feat: add `concurrency` to [simulator]

---
+++ mjolnir/omp/omp.hpp
@@ -0,0 +1,27 @@
+#ifndef MJOLNIR_OMP_OMP_HPP
+#define MJOLNIR_OMP_OMP_HPP
+
+// This file is a meta-header file that just includes everything that are needed
+// to use OpenMP implementation. This file is introduced in order to make it
+// simple to turn on/off OpenMP support by preprocessor macro, like in the
+// following way.
+//
+// ```cpp
+// #ifdef WITH_OPENMP
+// #include <mjolnir/omp/omp.hpp>
+// #endif
+// ```
+//
+// By doing this, the general, standalone implementation does not need to
+// consider what files should be included to use OpenMP.
+
+#include <mjolnir/omp/OpenMPSimulatorTraits.hpp>
+#include <mjolnir/omp/BondLengthInteraction.hpp>
+#include <mjolnir/omp/ContactInteraction.hpp>
+#include <mjolnir/omp/BondAngleInteraction.hpp>
+#include <mjolnir/omp/DihedralAngleInteraction.hpp>
+#include <mjolnir/omp/RandomNumberGenerator.hpp>
+#include <mjolnir/omp/System.hpp>
+#include <mjolnir/omp/UnderdampedLangevinIntegrator.hpp>
+
+#endif// MJOLNIR_OMP_OMP_HPP
