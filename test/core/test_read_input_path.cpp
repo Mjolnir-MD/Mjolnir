378adb09cbabbf0443ef15b2341b95c22b18a754
ToruNiina <niina.toru.68u@gmail.com>
2019-06-13 15:54:40 +0900

    test: add test for read_output_path

--- test/core/test_read_input_path.cpp
+++ test/core/test_read_input_path.cpp
@@ -1,4 +1,4 @@
-#define BOOST_TEST_MODULE "test_read_input_path"
+#define BOOST_TEST_MODULE "test_read_path"
 
 #ifdef BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
@@ -45,6 +45,47 @@ BOOST_AUTO_TEST_CASE(read_input_path)
 
         const auto input_path = mjolnir::read_input_path(t);
         BOOST_TEST(input_path == "./");
+    }
+}
+
+BOOST_AUTO_TEST_CASE(read_output_path)
+{
+    mjolnir::LoggerManager::set_default_logger("test_read_output_path.log");
+    {
+        using namespace toml::literals;
+        const toml::table t = toml::get<toml::table>(u8R"(
+            files.output.path = "./output"
+        )"_toml);
+
+        const auto output_path = mjolnir::read_output_path(t);
+        BOOST_TEST(output_path == "./output/");
+    }
+    {
+       using namespace toml::literals;
+        const toml::table t = toml::get<toml::table>(u8R"(
+            files.output.path = "./output/"
+        )"_toml);
+
+        const auto output_path = mjolnir::read_output_path(t);
+        BOOST_TEST(output_path == "./output/");
+    }
+    {
+       using namespace toml::literals;
+        const toml::table t = toml::get<toml::table>(u8R"(
+            files.output = {}
+        )"_toml);
+
+        const auto output_path = mjolnir::read_output_path(t);
+        BOOST_TEST(output_path == "./");
+    }
+    {
+       using namespace toml::literals;
+        const toml::table t = toml::get<toml::table>(u8R"(
+            files = {}
+        )"_toml);
+
+        const auto output_path = mjolnir::read_output_path(t);
+        BOOST_TEST(output_path == "./");
     }
 }
 
