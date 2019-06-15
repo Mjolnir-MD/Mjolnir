fee172f2d8af1b3076a9b572d6b863c8c58ccf6d
ToruNiina <niina.toru.68u@gmail.com>
2019-06-15 13:52:33 +0900

    feat: make error message clearer

--- mjolnir/input/utility.hpp
+++ mjolnir/input/utility.hpp
@@ -24,10 +24,15 @@ find_parameter(const toml::value& params, const toml::value& env,
     {
         // search inside of `env`
         const std::string& var = p.as_string();
+        if(env.is_uninitialized())
+        {
+            throw std::out_of_range(toml::format_error("[error] named variable \""_s +
+                var + "\" used but no env is defined"_s, params, "used here"_s));
+        }
         if(!env.is_table() || env.as_table().count(var) != 1)
         {
-            throw std::out_of_range(toml::format_error("[error] named variable "_s +
-                var + " does not exists"_s, env, "in this table"_s));
+            throw std::out_of_range(toml::format_error("[error] named variable \""_s +
+                var + "\" does not exists"_s, env, "in this table"_s));
         }
         return toml::get<T>(env.as_table().at(var));
     }
@@ -43,18 +48,27 @@ find_parameter(const toml::value& params, const toml::value& /* env */,
 {
     if(!params.is_table() || params.as_table().count(name) != 1)
     {
-        throw std::out_of_range(toml::format_error("[error] value "_s + name +
-            " does not exists"_s, params, "in this table"_s));
+        throw std::out_of_range(toml::format_error("[error] value \""_s + name +
+            "\" does not exists"_s, params, "in this table"_s));
     }
     const toml::value& p = params.as_table().at(name);
     return toml::get<T>(p);
 }
 
-// The previous versions of Mjolnir allow to use unicode character when defining
-// a parameter. But since it is a bit ambiguous because the same character might
-// appear in the unicode table several time with different styles, so I decided
-// to deprecate them. This function was introduced to support those multi-named
-// parameters but is planned to be removed in v1.4.0.
+// The current version of Mjolnir allow to use unicode character when defining
+// a parameter. But since it is a bit ambiguous because of some reasons. E.g.
+// - The same character might appear in the unicode table several time with
+//   different styles
+// - When the different parameters appear in different names, it is ofcourse
+//   ambiguous which one to be used.
+//
+// So I decided to deprecate them. At first I thought that a unicode names
+// ware covenient to reduce the size of input file. But now, since env is
+// introduced, it is more effective for reducing the file size. So if it works
+// nice after the next release, I will deprecate unicode names and warn if used.
+//
+// This function was introduced to support those
+// multi-named parameters but is planned to be removed in the later release.
 template<typename T>
 typename std::enable_if<negation<std::is_same<T, std::string>>::value, T>::type
 find_parameter(const toml::value& params, const toml::value& env,
@@ -66,27 +80,32 @@ find_parameter(const toml::value& params, const toml::value& env,
     if(!params.is_table() || (params.as_table().count(name1) == 0 &&
                               params.as_table().count(name2) == 0))
     {
-        throw std::out_of_range(toml::format_error("[error] value "_s + name1 +
-            " or "_s + name2 + " does not exists"_s, params, "in this table"_s));
+        throw std::out_of_range(toml::format_error("[error] value \""_s + name1 +
+            "\" or \""_s + name2 + "\" does not exists"_s, params, "in this table"_s));
     }
     // name1 has priority.
     const toml::value& p = (params.as_table().count(name1) == 1) ?
                             params.as_table().at(name1)          :
                             params.as_table().at(name2)          ;
 
-    if(params.as_table().count(name2) == 1)
-    {
-        MJOLNIR_LOG_WARN("deprecated name \"", name2, "\" is used. Use \"",
-                         name1, "\" instead.");
-    }
+//     if(params.as_table().count(name2) == 1)
+//     {
+//         MJOLNIR_LOG_WARN("deprecated name \"", name2, "\" is used. Use \"",
+//                          name1, "\" instead.");
+//     }
     if(p.is_string())
     {
         // search inside of `env`
         const std::string& var = p.as_string();
+        if(env.is_uninitialized())
+        {
+            throw std::out_of_range(toml::format_error("[error] named variable \""_s +
+                var + "\" used but no env is defined"_s, params, "used here"_s));
+        }
         if(!env.is_table() || env.as_table().count(var) != 1)
         {
-            throw std::out_of_range(toml::format_error("[error] named variable "_s +
-                var + " does not exists"_s, env, "in this table"_s));
+            throw std::out_of_range(toml::format_error("[error] named variable \""_s +
+                var + "\" does not exists"_s, env, "in this table"_s));
         }
         return toml::get<T>(env.as_table().at(var));
     }
