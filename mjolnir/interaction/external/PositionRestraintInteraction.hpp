d423afe741423498fe8ba5bed5335a3ec8a1c47c
ToruNiina <niina.toru.68u@gmail.com>
2019-07-03 13:22:51 +0900

    refactor: re-use harmonic potential for restraints

--- mjolnir/interaction/external/PositionRestraintInteraction.hpp
+++ mjolnir/interaction/external/PositionRestraintInteraction.hpp
@@ -3,33 +3,39 @@
 #include <mjolnir/core/ExternalForceInteractionBase.hpp>
 #include <mjolnir/math/math.hpp>
 #include <mjolnir/util/string.hpp>
+#include <tuple>
 
 namespace mjolnir
 {
 
-//! @brief Interaction between particles and points. 
-template<typename traitsT, typename potentialT>
+// Interaction between particles and fixed points.
+//
+// XXX
+// It re-uses local potentials that can be found in mjolnir/potential/local/*,
+// e.g. HarmonicPotential, GaussianPotential, and so on.
+//
+template<typename traitsT, typename local_potentialT>
 class PositionRestraintInteraction final
     : public ExternalForceInteractionBase<traitsT>
 {
   public:
     using traits_type     = traitsT;
-    using potential_type  = potentialT;
+    using potential_type  = local_potentialT;
     using base_type       = ExternalForceInteractionBase<traitsT>;
     using real_type       = typename base_type::real_type;
     using coordinate_type = typename base_type::coordinate_type;
     using system_type     = typename base_type::system_type;
     using boundary_type   = typename base_type::boundary_type;
 
-    // list of {pair of a particle-id and a position at which
-    //          the particle would be restrained}
-    using shape_type = std::vector<std::pair<std::size_t, coordinate_type>>;
+    using potential_container_type = std::vector<
+            //        {particle-id, fixed point,    potential class}
+            std::tuple<std::size_t, coordinate_type, potential_type>
+        >;
 
   public:
 
-    PositionRestraintInteraction(
-        shape_type shape, potential_type pot)
-        : shape_(std::move(shape)), potential_(std::move(pot))
+    PositionRestraintInteraction(potential_container_type&& pots)
+        : potentials_(std::move(pots))
     {}
     ~PositionRestraintInteraction() override = default;
 
@@ -41,14 +47,20 @@ class PositionRestraintInteraction final
     void initialize(const system_type& sys) override
     {
         // update system parameters such as temperature, ionic-strength, etc.,
-        this->potential_.update(sys);
+        for(auto& pot : potentials_)
+        {
+            std::get<2>(pot).update(sys);
+        }
         return;
     }
 
     void update(const system_type& sys) override
     {
         // update system parameters such as temperature, ionic-strength, etc.
-        this->potential_.update(sys);
+        for(auto& pot : potentials_)
+        {
+            std::get<2>(pot).update(sys);
+        }
         return;
     }
 
@@ -56,29 +68,32 @@ class PositionRestraintInteraction final
     void update_margin(const real_type, const system_type&) override {return;}
 
     std::string name() const override
-    {return "PositionRestraint:"_s + potential_.name();}
+    {return "PositionRestraint:"_s + potential_type::name();}
+
+    // for tests
+    potential_container_type const& potentials() const noexcept {return potentials_;}
 
   private:
 
-    shape_type     shape_;
-    potential_type potential_;
+    potential_container_type potentials_;
 };
 
 template<typename traitsT, typename potT>
 void PositionRestraintInteraction<traitsT, potT>::calc_force(
         system_type& sys) const noexcept
 {
-    for(const auto& pidpos : this->shape_)
+    for(const auto& pots : this->potentials_)
     {
-        const auto  pid = pidpos.first;  // particle index
-        const auto& pos = pidpos.second; // restrained position
+        const auto  pid = std::get<0>(pots); // particle index
+        const auto& pos = std::get<1>(pots); // restrained position
+        const auto& pot = std::get<2>(pots); // potential form
 
         const auto& r_i = sys.position(pid);
         const auto  dr  = sys.adjust_direction(pos - r_i);
 
         const auto rlen = math::rlength(dr); // 1 / |dr|
         const auto dist = math::length_sq(dr) * rlen;
-        const auto dV   = this->potential_.derivative(pid, dist);
+        const auto dV   = pot.derivative(dist);
         if(dV == 0.0){continue;}
 
         sys.force(pid) += (dV * rlen) * dr;
@@ -92,16 +107,17 @@ PositionRestraintInteraction<traitsT, potT>::calc_energy(
         const system_type& sys) const noexcept
 {
     real_type E = 0.0;
-    for(const auto& pidpos : this->shape_)
+    for(const auto& pots : this->potentials_)
     {
-        const auto  pid = pidpos.first;  // particle index
-        const auto& pos = pidpos.second; // restrained position
+        const auto  pid = std::get<0>(pots); // particle index
+        const auto& pos = std::get<1>(pots); // restrained position
+        const auto& pot = std::get<2>(pots); // potential form
 
         const auto& r_i = sys.position(pid);
         const auto  dr  = sys.adjust_direction(pos - r_i);
 
         const auto dist = math::length(dr);
-        E += this->potential_.potential(pid, dist);
+        E += pot.potential(dist);
     }
     return E;
 }
