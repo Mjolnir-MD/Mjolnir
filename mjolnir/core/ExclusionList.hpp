1e5ce55b96b48c7612dfda8c7402d3ee8e6e3d62
ToruNiina <niina.toru.68u@gmail.com>
2019-07-08 23:59:07 +0900

    fix: always ignore interaction between the same p
    Since GlobalPairInteraction ignores it, this has no effect.
    But still it is not an intended behavior, so should be fixed.

--- mjolnir/core/ExclusionList.hpp
+++ mjolnir/core/ExclusionList.hpp
@@ -97,7 +97,7 @@ class ExclusionList
             for(std::size_t i=0; i<N; ++i)
             {
                 const std::size_t first = idx;
-                std::vector<std::size_t> ignored_particles;
+                std::vector<std::size_t> ignored_particles{i};
                 for(const auto& connection : pot.ignore_within())
                 {
                     const std::size_t dist = connection.second;
