5d2f6bb8ff659126e1ef3a2a8442ebbffac9cf2d
ToruNiina <niina.toru.68u@gmail.com>
2019-07-12 11:53:36 +0900

    test: add ignore_molecule|group to test_read_*

--- test/core/test_read_uniform_lennard_jones_potential.cpp
+++ test/core/test_read_uniform_lennard_jones_potential.cpp
@@ -203,3 +203,136 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(read_uniform_lennard_jones_env, T, test_types)
         BOOST_TEST(g.participants().at(4)  == 5u);
     }
 }
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(read_uniform_lennard_jones_ignore_self, T, test_types)
+{
+    mjolnir::LoggerManager::set_default_logger("test_read_uniform_lennard_jones.log");
+    using real_type = T;
+
+    // a dummy system for testing `initialize` method
+    using traits_type   = mjolnir::SimulatorTraits<real_type, mjolnir::UnlimitedBoundary>;
+    using boundary_type = typename traits_type::boundary_type;
+    mjolnir::System<traits_type> sys(10, boundary_type{});
+
+    {
+        using namespace toml::literals;
+        const auto v = u8R"(
+            interaction = "Pair"
+            potential   = "UniformLennardJones"
+            spatial_partition.type  = "CellList"
+            ignore.molecule         = "Self"
+            ignore.particles_within.bond    = 3
+            ignore.particles_within.contact = 1
+            sigma   = 2.0
+            epsilon = 1.5
+        )"_toml;
+
+        auto g = mjolnir::read_uniform_lennard_jones_potential<real_type>(v);
+
+        const auto ignore_within = g.ignore_within();
+        const std::map<std::string, std::size_t> within(
+                ignore_within.begin(), ignore_within.end());
+
+        BOOST_TEST(g.ignore_within().size() == 2u);
+        BOOST_TEST(within.at("bond")    == 3ul);
+        BOOST_TEST(within.at("contact") == 1ul);
+
+        BOOST_TEST( g.is_ignored_molecule(0, 0));
+        BOOST_TEST(!g.is_ignored_molecule(0, 1));
+        BOOST_TEST( g.is_ignored_molecule(1, 1));
+    }
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(read_uniform_lennard_jones_ignore_others, T, test_types)
+{
+    mjolnir::LoggerManager::set_default_logger("test_read_uniform_lennard_jones.log");
+    using real_type = T;
+
+    // a dummy system for testing `initialize` method
+    using traits_type   = mjolnir::SimulatorTraits<real_type, mjolnir::UnlimitedBoundary>;
+    using boundary_type = typename traits_type::boundary_type;
+    mjolnir::System<traits_type> sys(10, boundary_type{});
+
+    {
+        using namespace toml::literals;
+        const auto v = u8R"(
+            interaction = "Pair"
+            potential   = "UniformLennardJones"
+            spatial_partition.type  = "CellList"
+            ignore.molecule         = "Others"
+            ignore.particles_within.bond    = 3
+            ignore.particles_within.contact = 1
+            sigma   = 2.0
+            epsilon = 1.5
+        )"_toml;
+
+        auto g = mjolnir::read_uniform_lennard_jones_potential<real_type>(v);
+
+        const auto ignore_within = g.ignore_within();
+        const std::map<std::string, std::size_t> within(
+                ignore_within.begin(), ignore_within.end());
+
+        BOOST_TEST(g.ignore_within().size() == 2u);
+        BOOST_TEST(within.at("bond")    == 3ul);
+        BOOST_TEST(within.at("contact") == 1ul);
+
+        BOOST_TEST(!g.is_ignored_molecule(0, 0));
+        BOOST_TEST( g.is_ignored_molecule(0, 1));
+        BOOST_TEST(!g.is_ignored_molecule(1, 1));
+    }
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(read_uniform_lennard_jones_ignore_group, T, test_types)
+{
+    mjolnir::LoggerManager::set_default_logger("test_read_uniform_lennard_jones.log");
+    using real_type = T;
+
+    // a dummy system for testing `initialize` method
+    using traits_type   = mjolnir::SimulatorTraits<real_type, mjolnir::UnlimitedBoundary>;
+    using boundary_type = typename traits_type::boundary_type;
+    mjolnir::System<traits_type> sys(10, boundary_type{});
+
+    {
+        using namespace toml::literals;
+        const auto v = u8R"(
+            interaction = "Pair"
+            potential   = "UniformLennardJones"
+            spatial_partition.type  = "CellList"
+            ignore.molecule         = "Nothing"
+            ignore.particles_within.bond    = 3
+            ignore.particles_within.contact = 1
+            ignore.group.inter      = [
+                ["protein1", "protein2"], # between these
+                ["protein1", "protein3"],
+            ]
+            sigma   = 2.0
+            epsilon = 1.5
+        )"_toml;
+
+        auto g = mjolnir::read_uniform_lennard_jones_potential<real_type>(v);
+
+        const auto ignore_within = g.ignore_within();
+        const std::map<std::string, std::size_t> within(
+                ignore_within.begin(), ignore_within.end());
+
+        BOOST_TEST(g.ignore_within().size() == 2u);
+        BOOST_TEST(within.at("bond")    == 3ul);
+        BOOST_TEST(within.at("contact") == 1ul);
+
+        BOOST_TEST(!g.is_ignored_molecule(0, 0));
+        BOOST_TEST(!g.is_ignored_molecule(0, 1));
+        BOOST_TEST(!g.is_ignored_molecule(1, 1));
+
+        BOOST_TEST(!g.is_ignored_group("protein1", "protein1"));
+        BOOST_TEST( g.is_ignored_group("protein1", "protein2"));
+        BOOST_TEST( g.is_ignored_group("protein1", "protein3"));
+
+        BOOST_TEST( g.is_ignored_group("protein2", "protein1"));
+        BOOST_TEST(!g.is_ignored_group("protein2", "protein2"));
+        BOOST_TEST(!g.is_ignored_group("protein2", "protein3"));
+
+        BOOST_TEST( g.is_ignored_group("protein3", "protein1"));
+        BOOST_TEST(!g.is_ignored_group("protein3", "protein2"));
+        BOOST_TEST(!g.is_ignored_group("protein3", "protein3"));
+    }
+}
