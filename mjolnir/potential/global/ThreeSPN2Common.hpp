d807ed7d2d8880ae6ae51cff092c460d025f362c
ToruNiina <niina.toru.68u@gmail.com>
2019-07-22 13:38:47 +0900

    refactor: reorder enumerators for 3SPN2

--- mjolnir/potential/global/ThreeSPN2Common.hpp
+++ mjolnir/potential/global/ThreeSPN2Common.hpp
@@ -6,14 +6,14 @@ namespace mjolnir
 {
 
 // X is for default parameter
-enum class base_kind       : std::uint8_t {A,  T,  C,  G,  X};
-enum class base_pair_kind  : std::uint8_t {AT, TA, CG, GC};
+enum class base_kind       : std::uint8_t {A=0, T=1, G=2, C=3,  X};
+enum class base_pair_kind  : std::uint8_t {AT, TA, GC, CG};
 enum class base_stack_kind : std::uint8_t
 {
-    AA =  0, AT =  1, AC =  2, AG =  3,
-    TA =  4, TT =  5, TC =  6, TG =  7,
-    CA =  8, CT =  9, CC = 10, CG = 11,
-    GA = 12, GT = 13, GC = 14, GG = 15,
+    AA =  0, AT =  1, AG =  2, AC =  3,
+    TA =  4, TT =  5, TG =  6, TC =  7,
+    GA =  8, GT =  9, GG = 10, GC = 11,
+    CA = 12, CT = 13, CG = 14, CC = 15
 };
 enum class cross_stack_kind: std::uint8_t
 {
@@ -26,15 +26,15 @@ enum class cross_stack_kind: std::uint8_t
     //           Bi3   Bj5
     //
     // sense 5 -- antisense 5 (Bi - Bj5)
-    AA5 =  0, AT5 =  1, AC5 =  2, AG5 =  3,
-    TA5 =  4, TT5 =  5, TC5 =  6, TG5 =  7,
-    CA5 =  8, CT5 =  9, CC5 = 10, CG5 = 11,
-    GA5 = 12, GT5 = 13, GC5 = 14, GG5 = 15,
+    AA5 =  0, AT5 =  1, AG5 =  2, AC5 =  3,
+    TA5 =  4, TT5 =  5, TG5 =  6, TC5 =  7,
+    GA5 =  8, GT5 =  9, GG5 = 10, GC5 = 11,
+    CA5 = 12, CT5 = 13, CG5 = 14, CC5 = 15,
     // sense 3 -- antisense 3 (Bj - Bi3)
-    AA3 = 16, AT3 = 17, AC3 = 18, AG3 = 19,
-    TA3 = 20, TT3 = 21, TC3 = 22, TG3 = 23,
-    CA3 = 24, CT3 = 25, CC3 = 26, CG3 = 27,
-    GA3 = 28, GT3 = 29, GC3 = 30, GG3 = 31
+    AA3 = 16, AT3 = 17, AG3 = 18, AC3 = 19,
+    TA3 = 20, TT3 = 21, TG3 = 22, TC3 = 23,
+    GA3 = 24, GT3 = 25, GG3 = 26, GC3 = 27,
+    CA3 = 28, CT3 = 29, CG3 = 30, CC3 = 31
 };
 
 } // mjolnir
