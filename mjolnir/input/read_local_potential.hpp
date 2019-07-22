fedf7ff33c96d4dad6642efe9364aca4df00066b
ToruNiina <niina.toru.68u@gmail.com>
2019-07-22 20:22:21 +0900

    Revert "refactor: distinguish potential and parameter type"
    This reverts commit 5c6e108c16dbb79a5ae83b4738e07785d1c14cdf.

--- mjolnir/input/read_local_potential.hpp
+++ mjolnir/input/read_local_potential.hpp
@@ -237,8 +237,8 @@ template<typename potentialT> struct read_local_potential_impl;
 template<typename realT>
 struct read_local_potential_impl<HarmonicPotential<realT>>
 {
-    using result_type = HarmonicPotential<realT>;
-    static result_type invoke(const toml::value& param, const toml::value& env)
+    static HarmonicPotential<realT>
+    invoke(const toml::value& param, const toml::value& env)
     {
         return read_harmonic_potential<realT>(param, env);
     }
@@ -246,8 +246,8 @@ struct read_local_potential_impl<HarmonicPotential<realT>>
 template<typename realT>
 struct read_local_potential_impl<GoContactPotential<realT>>
 {
-    using result_type = GoContactPotential<realT>;
-    static result_type invoke(const toml::value& param, const toml::value& env)
+    static GoContactPotential<realT>
+    invoke(const toml::value& param, const toml::value& env)
     {
         return read_go_contact_potential<realT>(param, env);
     }
@@ -255,8 +255,8 @@ struct read_local_potential_impl<GoContactPotential<realT>>
 template<typename realT>
 struct read_local_potential_impl<GaussianPotential<realT>>
 {
-    using result_type = GaussianPotential<realT>;
-    static result_type invoke(const toml::value& param, const toml::value& env)
+    static GaussianPotential<realT>
+    invoke(const toml::value& param, const toml::value& env)
     {
         return read_gaussian_potential<realT>(param, env);
     }
@@ -264,8 +264,8 @@ struct read_local_potential_impl<GaussianPotential<realT>>
 template<typename realT>
 struct read_local_potential_impl<PeriodicGaussianPotential<realT>>
 {
-    using result_type = PeriodicGaussianPotential<realT>;
-    static result_type invoke(const toml::value& param, const toml::value& env)
+    static PeriodicGaussianPotential<realT>
+    invoke(const toml::value& param, const toml::value& env)
     {
         return read_periodic_gaussian_potential<realT>(param, env);
     }
@@ -273,8 +273,8 @@ struct read_local_potential_impl<PeriodicGaussianPotential<realT>>
 template<typename realT>
 struct read_local_potential_impl<FlexibleLocalAnglePotential<realT>>
 {
-    using result_type = FlexibleLocalAnglePotential<realT>;
-    static result_type invoke(const toml::value& param, const toml::value& env)
+    static FlexibleLocalAnglePotential<realT>
+    invoke(const toml::value& param, const toml::value& env)
     {
         return read_flexible_local_angle_potential<realT>(param, env);
     }
@@ -282,8 +282,8 @@ struct read_local_potential_impl<FlexibleLocalAnglePotential<realT>>
 template<typename realT>
 struct read_local_potential_impl<ClementiDihedralPotential<realT>>
 {
-    using result_type = ClementiDihedralPotential<realT>;
-    static result_type invoke(const toml::value& param, const toml::value& env)
+    static ClementiDihedralPotential<realT>
+    invoke(const toml::value& param, const toml::value& env)
     {
         return read_clementi_dihedral_potential<realT>(param, env);
     }
@@ -291,8 +291,8 @@ struct read_local_potential_impl<ClementiDihedralPotential<realT>>
 template<typename realT>
 struct read_local_potential_impl<FlexibleLocalDihedralPotential<realT>>
 {
-    using result_type = FlexibleLocalDihedralPotential<realT>;
-    static result_type invoke(const toml::value& param, const toml::value& env)
+    static FlexibleLocalDihedralPotential<realT>
+    invoke(const toml::value& param, const toml::value& env)
     {
         return read_flexible_local_dihedral_potential<realT>(param, env);
     }
@@ -300,8 +300,8 @@ struct read_local_potential_impl<FlexibleLocalDihedralPotential<realT>>
 template<typename realT>
 struct read_local_potential_impl<CosinePotential<realT>>
 {
-    using result_type = CosinePotential<realT>;
-    static result_type invoke(const toml::value& param, const toml::value& env)
+    static CosinePotential<realT>
+    invoke(const toml::value& param, const toml::value& env)
     {
         return read_cosine_potential<realT>(param, env);
     }
@@ -329,8 +329,7 @@ struct read_local_potential_impl<ThreeSPN2BaseStackingPotential<realT>>
 // this function reads particle indices on which the potential will be applied
 // and returns pairs of [indices, potential parameters].
 template<std::size_t N, typename potentialT>
-std::vector<std::pair<std::array<std::size_t, N>,
-    typename detail::read_local_potential_impl<potentialT>::result_type>>
+std::vector<std::pair<std::array<std::size_t, N>, potentialT>>
 read_local_potential(const toml::value& local)
 {
     MJOLNIR_GET_DEFAULT_LOGGER();
