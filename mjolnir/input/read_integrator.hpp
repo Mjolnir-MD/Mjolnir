e5ced0714fd1cf16934b372be3257fa6e3f3067b
ToruNiina <niina.toru.68u@gmail.com>
2019-09-05 01:03:11 +0900

    feat: check keys in [simulator.integrator] section

--- mjolnir/input/read_integrator.hpp
+++ mjolnir/input/read_integrator.hpp
@@ -4,6 +4,7 @@
 #include <mjolnir/core/VelocityVerletIntegrator.hpp>
 #include <mjolnir/core/UnderdampedLangevinIntegrator.hpp>
 #include <mjolnir/core/BAOABLangevinIntegrator.hpp>
+#include <mjolnir/input/utility.hpp>
 #include <mjolnir/util/logger.hpp>
 
 namespace mjolnir
@@ -15,7 +16,6 @@ read_velocity_verlet_integrator(const toml::value& simulator)
 {
     MJOLNIR_GET_DEFAULT_LOGGER();
     MJOLNIR_LOG_FUNCTION();
-
     using real_type = typename traitsT::real_type;
 
     const real_type delta_t = toml::find<real_type>(simulator, "delta_t");
@@ -36,6 +36,8 @@ read_underdamped_langevin_integrator(const toml::value& simulator)
     MJOLNIR_LOG_INFO("delta_t = ", delta_t);
 
     const auto& integrator = toml::find(simulator, "integrator");
+
+    check_keys_available(integrator, {"type"_s, "seed"_s, "parameters"_s});
 
     const auto seed       = toml::find<std::uint32_t>(integrator, "seed");
     const auto parameters = toml::find<toml::array  >(integrator, "parameters");
@@ -67,6 +69,8 @@ read_BAOAB_langevin_integrator(const toml::value& simulator)
     MJOLNIR_LOG_INFO("delta_t = ", delta_t);
 
     const auto& integrator = toml::find(simulator, "integrator");
+
+    check_keys_available(integrator, {"type"_s, "seed"_s, "parameters"_s});
 
     const auto seed       = toml::find<std::uint32_t>(integrator, "seed");
     const auto parameters = toml::find<toml::array  >(integrator, "parameters");
