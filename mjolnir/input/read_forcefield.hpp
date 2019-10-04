2f8b6c914cfa89c884b181932bfc50e688c38255
ToruNiina <niina.toru.68u@gmail.com>
2019-10-04 20:50:31 +0900

    refactor: reduce codes from read_forcefield
    using read_table_from_file

--- mjolnir/input/read_forcefield.hpp
+++ mjolnir/input/read_forcefield.hpp
@@ -6,6 +6,7 @@
 #include <mjolnir/input/read_local_forcefield.hpp>
 #include <mjolnir/input/read_global_forcefield.hpp>
 #include <mjolnir/input/read_external_forcefield.hpp>
+#include <mjolnir/input/read_table_from_file.hpp>
 #include <mjolnir/input/read_path.hpp>
 #include <mjolnir/input/utility.hpp>
 
@@ -50,64 +51,45 @@ read_forcefield_from_table(const toml::value& ff, const std::string& input_path)
         read_external_forcefield<traitsT>(std::move(ffexternal), input_path));
 }
 
-
 template<typename traitsT>
 ForceField<traitsT>
-read_forcefield(const toml::value& root, std::size_t N)
+read_forcefield(const toml::value& root, const std::size_t N)
 {
     MJOLNIR_GET_DEFAULT_LOGGER();
     MJOLNIR_LOG_FUNCTION();
-    if(N != 0) {MJOLNIR_LOG_NOTICE("reading ", N, "-th [[forcefield]].");}
-    else       {MJOLNIR_LOG_NOTICE("reading [[forcefield]].");}
 
-    const auto& ffs = toml::find<toml::array>(root, "forcefields");
-    if(ffs.size() <= N)
+    const auto ff = read_table_from_file(root, "forcefields", N);
+    check_keys_available(ff, {"local"_s, "global"_s, "external"_s, "name"_s});
+
+    if(ff.as_table().count("name") == 1)
     {
-        throw_exception<std::out_of_range>("[error] mjolnir::read_forcefield: "
-            "no enough forcefield definitions: ", N, " is required, but only ",
-            ffs.size(), " defined.");
+        MJOLNIR_LOG_NOTICE("forcefield \"", toml::find<std::string>(ff, "name"),
+                           "\" found");
     }
-    MJOLNIR_LOG_INFO(ffs.size(), " forcefields are provided");
-    MJOLNIR_LOG_INFO("using ", N, "-th forcefield");
+
+    toml::array fflocal, ffglobal, ffexternal;
+    if(ff.as_table().count("local") == 1)
+    {
+        MJOLNIR_LOG_INFO("LocalForceField found");
+        fflocal = toml::find<toml::array>(ff, "local");
+    }
+    if(ff.as_table().count("global") == 1)
+    {
+        MJOLNIR_LOG_INFO("GlobalForceField found");
+        ffglobal = toml::find<toml::array>(ff, "global");
+    }
+    if(ff.as_table().count("external") == 1)
+    {
+        MJOLNIR_LOG_INFO("ExternalForceField found");
+        ffexternal = toml::find<toml::array>(ff, "external");
+    }
 
     const auto input_path = read_input_path(root);
-    const auto& ff = ffs.at(N);
-    if(ff.as_table().count("file_name") == 1)
-    {
-        MJOLNIR_LOG_SCOPE(if(ff.as_table().count("file_name") == 1));
 
-        const auto file_name = toml::find<std::string>(ff, "file_name");
-        MJOLNIR_LOG_NOTICE("forcefield is defined in ", input_path, file_name);
-        if(ff.as_table().size() != 1)
-        {
-            MJOLNIR_LOG_WARN("[[forcefields]] has \"file_name\" and other keys.");
-            MJOLNIR_LOG_WARN("When \"file_name\" is provided, other values are "
-                             "ignored because those are read from the specified"
-                             " file (", input_path, file_name, ").");
-        }
-
-        MJOLNIR_LOG_NOTICE("reading ", input_path, file_name, " ...");
-        const auto ff_file = toml::parse(input_path + file_name);
-        MJOLNIR_LOG_NOTICE(" done.");
-
-        if(ff_file.as_table().count("forcefields") != 1)
-        {
-            throw_exception<std::out_of_range>("[error] mjolnir::read_forcefields: "
-                "table [forcefields] not found in the toml file\n --> ",
-                input_path, file_name, "\n | the file should define [forcefield] "
-                "table and define values in it.");
-        }
-        if(ff_file.as_table().at("forcefields").is_array())
-        {
-            return read_forcefield_from_table<traitsT>(
-                toml::find<toml::array>(ff_file, "forcefields").front(),
-                input_path);
-        }
-        return read_forcefield_from_table<traitsT>(
-                toml::find(ff_file, "forcefields"), input_path);
-    }
-    // all-in-one file
-    return read_forcefield_from_table<traitsT>(ffs.at(N), input_path);
+    return ForceField<traitsT>(
+        read_local_forcefield<traitsT>(std::move(fflocal), input_path),
+        read_global_forcefield<traitsT>(std::move(ffglobal), input_path),
+        read_external_forcefield<traitsT>(std::move(ffexternal), input_path));
 }
 
 #ifdef MJOLNIR_SEPARATE_BUILD
