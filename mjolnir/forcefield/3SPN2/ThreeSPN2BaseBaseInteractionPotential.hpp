d54a2b6b8737e240da0043ea945f959e56ca1da1
ToruNiina <niina.toru.68u@gmail.com>
2019-10-29 22:46:15 +0900

    feat: update type aliases in potential classes

--- mjolnir/forcefield/3SPN2/ThreeSPN2BaseBaseInteractionPotential.hpp
+++ mjolnir/forcefield/3SPN2/ThreeSPN2BaseBaseInteractionPotential.hpp
@@ -68,7 +68,7 @@ class ThreeSPN2BaseBaseInteractionPotential
     using connection_kind_type = typename topology_type::connection_kind_type;
     using ignore_molecule_type = IgnoreMolecule<molecule_id_type>;
     using ignore_group_type    = IgnoreGroup   <group_id_type>;
-    using exclusion_list_type  = ExclusionList;
+    using exclusion_list_type  = ExclusionList <traits_type>;
 
     static constexpr std::size_t invalid() noexcept
     {
--- mjolnir/potential/global/ExcludedVolumePotential.hpp
+++ mjolnir/potential/global/ExcludedVolumePotential.hpp
@@ -47,7 +47,7 @@ class ExcludedVolumePotential
     using connection_kind_type = typename topology_type::connection_kind_type;
     using ignore_molecule_type = IgnoreMolecule<molecule_id_type>;
     using ignore_group_type    = IgnoreGroup   <group_id_type>;
-    using exclusion_list_type  = ExclusionList;
+    using exclusion_list_type  = ExclusionList <traits_type>;
 
     static constexpr real_type default_cutoff() noexcept
     {
