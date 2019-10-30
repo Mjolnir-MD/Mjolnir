621dff305ef40fad1132e8122a122d6ecf5e8744
ToruNiina <niina.toru.68u@gmail.com>
2019-10-30 13:47:41 +0900

    feat: add template parameter to ExclusionList

--- mjolnir/forcefield/PWMcos/PWMcosPotential.hpp
+++ mjolnir/forcefield/PWMcos/PWMcosPotential.hpp
@@ -128,7 +128,7 @@ class PWMcosPotential
     using connection_kind_type = typename topology_type::connection_kind_type;
     using ignore_molecule_type = IgnoreMolecule<molecule_id_type>;
     using ignore_group_type    = IgnoreGroup   <group_id_type>;
-    using exclusion_list_type  = ExclusionList;
+    using exclusion_list_type  = ExclusionList<traits_type>;
 
     static constexpr std::uint32_t invalid() noexcept
     {
