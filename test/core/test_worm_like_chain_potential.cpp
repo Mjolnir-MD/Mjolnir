dad15d0b7ccfb7d5060735d500e47082fc717d65
yutaka <p7dea87c7e4n7j@gmail.com>
2019-11-15 13:48:24 +0900

    test: add test for worm-like chain potential

---
+++ test/core/test_worm_like_chain_potential.cpp
@@ -0,0 +1,69 @@
+#define BOOST_TEST_MODULE "test_worm_like_chain_potential"
+
+#ifdef BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+#else
+#include <boost/test/included/unit_test.hpp>
+#endif
+
+#include <mjolnir/potential/local/WormLikeChainPotential.hpp>
+
+BOOST_AUTO_TEST_CASE(WormLikeChain_double)
+{
+    mjolnir::LoggerManager::set_default_logger("test_worm_like_chain_potential.log");
+
+    using real_type = double;
+    constexpr std::size_t N = 900;
+    constexpr real_type   h = 1e-6;
+    const real_type       p = 3.9;
+    const real_type      lc = 19.0;
+
+    mjolnir::WormLikeChainPotential<real_type> wormlikechain(p, lc);
+
+    const real_type x_min = lc * 0.0;
+    const real_type x_max = lc * 0.9;
+    const real_type dx = (x_max - x_min) / N;
+
+    for(std::size_t i=0; i<N; ++i)
+    {
+        const real_type x = x_min + dx * i;
+        const real_type pot1 = wormlikechain.potential(x + h);
+        const real_type pot2 = wormlikechain.potential(x - h);
+        const real_type dpot = (pot1 - pot2) / (2.0 * h);
+        const real_type deri = wormlikechain.derivative(x);
+
+        if(std::abs(dpot) > h && std::abs(deri) > h)
+        {
+            BOOST_TEST(dpot == deri, boost::test_tools::tolerance(h));
+        }
+    }
+}
+
+BOOST_AUTO_TEST_CASE(WormLikeChain_float)
+{
+    using real_type = float;
+    constexpr std::size_t N = 900;
+    constexpr real_type   h = 1e-3;
+    const real_type       p = 3.9;
+    const real_type      lc = 19.0;
+
+    mjolnir::WormLikeChainPotential<real_type> wormlikechain(p, lc);
+
+    const real_type x_min = lc * 0.0;
+    const real_type x_max = lc * 0.9;
+    const real_type dx = (x_max - x_min) / N;
+
+    for(std::size_t i=0; i<N; ++i)
+    {
+        const real_type x = x_min + dx * i;
+        const real_type pot1 = wormlikechain.potential(x + h);
+        const real_type pot2 = wormlikechain.potential(x - h);
+        const real_type dpot = (pot1 - pot2) / (2.0 * h);
+        const real_type deri = wormlikechain.derivative(x);
+
+        if(std::abs(dpot) > h && std::abs(deri) > h)
+        {
+            BOOST_TEST(dpot == deri, boost::test_tools::tolerance(h));
+        }
+    }
+}
