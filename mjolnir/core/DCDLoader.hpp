107fc518150956c23e196ab18a8956d06b73710d
ToruNiina <niina.toru.68u@gmail.com>
2019-11-27 14:47:19 +0900

    refactor: try to avoid false positive of linter

--- mjolnir/core/DCDLoader.hpp
+++ mjolnir/core/DCDLoader.hpp
@@ -22,10 +22,9 @@ class DCDLoader final : public LoaderBase<traitsT>
 
   public:
 
-    explicit DCDLoader(const std::string& filename)
-        : base_type(), filename_(filename),
-          file_(filename_, std::ios::binary | std::ios::in),
-          has_unitcell_(false)
+    explicit DCDLoader(const std::string& filename) : base_type(),
+        filename_(filename), file_(filename_, std::ios::binary | std::ios::in),
+        has_unitcell_(false)
     {
         if(!file_.good())
         {
