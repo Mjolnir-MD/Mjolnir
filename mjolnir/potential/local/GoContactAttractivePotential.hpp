c23c1766462094a8be54370e5cdd170bf01a1bf6
ToruNiina <niina.toru.68u@gmail.com>
2019-11-30 13:23:42 +0900

    fix: add initialize function to potential

--- mjolnir/potential/local/GoContactAttractivePotential.hpp
+++ mjolnir/potential/local/GoContactAttractivePotential.hpp
@@ -47,6 +47,9 @@ class GoContactAttractivePotential
     }
 
     template<typename T>
+    void initialize(const System<T>&) const noexcept {return;}
+
+    template<typename T>
     void update(const System<T>&) const noexcept {return;}
 
     static const char* name() noexcept {return "GoContactAttractive";}
--- mjolnir/potential/local/GoContactRepulsivePotential.hpp
+++ mjolnir/potential/local/GoContactRepulsivePotential.hpp
@@ -44,6 +44,9 @@ class GoContactRepulsivePotential
     }
 
     template<typename T>
+    void initialize(const System<T>&) const noexcept {return;}
+
+    template<typename T>
     void update(const System<T>&) const noexcept {return;}
 
     static const char* name() noexcept {return "GoContactRepulsive";}
