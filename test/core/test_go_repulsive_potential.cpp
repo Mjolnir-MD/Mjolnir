19312dcbb014bf84a623732ab79758ad1e6be5a2
ToruNiina <niina.toru.68u@gmail.com>
2019-11-30 13:06:07 +0900

    test: add test for Attractive/Repulsive Go

---
+++ test/core/test_go_repulsive_potential.cpp
@@ -0,0 +1,88 @@
+#define BOOST_TEST_MODULE "test_repulsive_gocontact_potential"
+
+#ifdef BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+#else
+#include <boost/test/included/unit_test.hpp>
+#endif
+
+#include <mjolnir/potential/local/GoContactRepulsivePotential.hpp>
+#include <iomanip>
+
+BOOST_AUTO_TEST_CASE(GoContactRepulsive_double)
+{
+    using real_type = double;
+    constexpr std::size_t N = 1000;
+    constexpr real_type   h = 1e-6;
+
+    const real_type e  = 1.0;
+    const real_type r0 = 5.0;
+
+    mjolnir::GoContactRepulsivePotential<real_type> pot(e, r0);
+
+    {
+        const real_type x_min = 0.8 * r0;
+        const real_type x_max = pot.cutoff();
+        const real_type dx = (x_max - x_min) / N;
+        for(std::size_t i=0; i<N; ++i)
+        {
+            const real_type x    = x_min + i * dx;
+            const real_type pot1 = pot.potential(x + h);
+            const real_type pot2 = pot.potential(x - h);
+            const real_type dpot = (pot1 - pot2) / (2 * h);
+            const real_type deri = pot.derivative(x);
+
+            BOOST_TEST(dpot == deri, boost::test_tools::tolerance(h));
+        }
+    }
+    {
+        const real_type x_min = pot.cutoff() + 1.01;
+        const real_type x_max = 2 * pot.cutoff();
+        const real_type dx = (x_max - x_min) / N;
+        for(std::size_t i=0; i<N; ++i)
+        {
+            const real_type x = x_min + i * dx;
+            BOOST_TEST(pot.potential(x)  == 0.0, boost::test_tools::tolerance(h));
+            BOOST_TEST(pot.derivative(x) == 0.0, boost::test_tools::tolerance(h));
+        }
+    }
+}
+
+BOOST_AUTO_TEST_CASE(GoContactRepulsive_float)
+{
+    using real_type = float;
+    constexpr std::size_t N = 100;
+    constexpr real_type   h = 1e-3;
+
+    const real_type e  = 1.0;
+    const real_type r0 = 5.0;
+
+    mjolnir::GoContactRepulsivePotential<real_type> pot(e, r0);
+    {
+        const real_type x_min = 0.8f * r0;
+        const real_type x_max = pot.cutoff();
+        const real_type dx = (x_max - x_min) / N;
+        for(std::size_t i=0; i<N; ++i)
+        {
+            const real_type x    = x_min + i * dx;
+            const real_type pot1 = pot.potential(x + h);
+            const real_type pot2 = pot.potential(x - h);
+            const real_type dpot = (pot1 - pot2) / (2 * h);
+            const real_type deri = pot.derivative(x);
+
+            BOOST_TEST_MESSAGE("i = " << i << ", x = " << x);
+            BOOST_TEST(dpot == deri, boost::test_tools::tolerance(1e-2f));
+        }
+    }
+    {
+        const real_type x_min = pot.cutoff() + 1.01;
+        const real_type x_max = 2 * pot.cutoff();
+        const real_type dx = (x_max - x_min) / N;
+        for(std::size_t i=0; i<N; ++i)
+        {
+            const real_type x = x_min + i * dx;
+            BOOST_TEST(pot.potential(x)  == 0.0, boost::test_tools::tolerance(h));
+            BOOST_TEST(pot.derivative(x) == 0.0, boost::test_tools::tolerance(h));
+        }
+    }
+}
