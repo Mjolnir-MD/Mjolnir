dbf8cb7611da23007b5224f498d98fc9e7bc3f62
Yutaka Murata <p7dea87c7e4n7j@gmail.com>
2019-12-17 11:50:03 +0900

    fix: meaningless test case, x smaller than 0.

--- test/core/test_worm_like_chain_potential.cpp
+++ test/core/test_worm_like_chain_potential.cpp
@@ -49,11 +49,11 @@ BOOST_AUTO_TEST_CASE(WormLikeChain_float)
 
     mjolnir::WormLikeChainPotential<real_type> wormlikechain(p, lc);
 
-    const real_type x_min = lc * 0.0;
+    const real_type x_min = 0.0;
     const real_type x_max = lc * 0.9;
     const real_type dx = (x_max - x_min) / N;
 
-    for(std::size_t i=0; i<N; ++i)
+    for(std::size_t i=1; i<N; ++i)
     {
         const real_type x = x_min + dx * i;
         const real_type pot1 = wormlikechain.potential(x + h);
