b986fbfa8bde7ada791cd5e380e0e8fd4f5b0987
Yutaka Murata <p7dea87c7e4n7j@gmail.com>
2019-12-19 17:25:42 +0900

    fix: meaningless test case in double.
    Co-Authored-By: Toru Niina <niina.toru.68u@gmail.com>

--- test/core/test_worm_like_chain_potential.cpp
+++ test/core/test_worm_like_chain_potential.cpp
@@ -24,7 +24,7 @@ BOOST_AUTO_TEST_CASE(WormLikeChain_double)
     const real_type x_max = lc * 0.9;
     const real_type dx = (x_max - x_min) / N;
 
-    for(std::size_t i=0; i<N; ++i)
+    for(std::size_t i=1; i<N; ++i)
     {
         const real_type x = x_min + dx * i;
         const real_type pot1 = wormlikechain.potential(x + h);
