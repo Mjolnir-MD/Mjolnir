d75dcf6e841e0f6e46a6163e3a1169b2df6c7f9a
ToruNiina <niina.toru.68u@gmail.com>
2020-02-07 21:36:07 +0900

    feat: add Matrix::ctor from toml::value

--- mjolnir/math/Matrix.hpp
+++ mjolnir/math/Matrix.hpp
@@ -1,5 +1,6 @@
 #ifndef MJOLNIR_MATH_MATRIX_HPP
 #define MJOLNIR_MATH_MATRIX_HPP
+#include <extlib/toml/toml.hpp>
 #include <mjolnir/util/type_traits.hpp>
 #include <algorithm>
 #include <array>
@@ -58,6 +59,13 @@ class Matrix
         static_assert(sizeof...(Ts) == total_size, "");
         static_assert(conjunction<
                 std::is_convertible<Ts, value_type> ...>::value, "");
+    }
+
+    template<typename Com, template<typename ...> class Tab,
+                           template<typename ...> class Arr>
+    Matrix(const toml::basic_value<Com, Tab, Arr>& tml)
+    {
+        values_ = toml::get<storage_type>(tml);
     }
 
     template<typename T>
