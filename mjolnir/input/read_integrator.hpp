48d8eb52285c433a4b801447382f072c33fc9e18
yutaka <p7dea87c7e4n7j@gmail.com>
2020-05-12 14:06:06 +0900

    feat: add read GBAOABLangevinIntegrator method

--- mjolnir/input/read_integrator.hpp
+++ mjolnir/input/read_integrator.hpp
@@ -4,6 +4,7 @@
 #include <mjolnir/core/VelocityVerletIntegrator.hpp>
 #include <mjolnir/core/UnderdampedLangevinIntegrator.hpp>
 #include <mjolnir/core/BAOABLangevinIntegrator.hpp>
+#include <mjolnir/core/gBAOABLangevinIntegrator.hpp>
 #include <mjolnir/core/SystemMotionRemover.hpp>
 #include <mjolnir/input/utility.hpp>
 #include <mjolnir/util/logger.hpp>
@@ -146,6 +147,78 @@ read_BAOAB_langevin_integrator(const toml::value& simulator)
         MJOLNIR_LOG_INFO("idx = ", idx, ", gamma = ", gm);
     }
     return BAOABLangevinIntegrator<traitsT>(delta_t, std::move(gamma),
+            read_system_motion_remover<traitsT>(simulator));
+}
+
+template<typename traitsT>
+GBAOABLangevinIntegrator<traitsT>
+read_gBAOAB_langevin_integrator(const toml::value& simulator)
+{
+    MJOLNIR_GET_DEFAULT_LOGGER();
+    MJOLNIR_LOG_FUNCTION();
+    using real_type                = typename traitsT::real_type;
+    using indices_t                = std::array<std::size_t, 2>;
+
+    const real_type delta_t = toml::find<real_type>(simulator, "delta_t");
+    MJOLNIR_LOG_INFO("delta_t = ", delta_t);
+
+    const auto& integrator = toml::find(simulator, "integrator");
+    check_keys_available(integrator,
+            {"type"_s, "seed"_s, "parameters"_s, "remove"_s, "env"_s});
+
+    const auto& env = integrator.contains("env") ?
+                      integrator.at("env") : toml::value{};
+
+    const auto& parameters = toml::find(integrator, "parameters");
+    check_keys_available(parameters, {"gammas"_s, "constraints"_s});
+
+    const auto gammas      = toml::find<toml::array>(parameters, "gammas");
+    const auto constraints = toml::find<toml::array>(parameters, "constraints");
+
+    // Read idx gamma pair part in parameters
+    // Temporarily make a vector of idx gamma pair to check index duplication.
+    std::vector<std::pair<std::size_t, real_type>> idx_gammas;
+    idx_gammas.reserve(parameters.size());
+    for(const auto& params : gammas)
+    {
+        const auto offset = find_parameter_or<std::int64_t>(params, env, "offset", 0);
+        const auto idx    = toml::find<std::size_t>(params, "index") + offset;
+        const auto gm     = find_parameter<real_type>(params, env, "gamma", u8"γ");
+
+        idx_gammas.emplace_back(idx, gm);
+    }
+    check_parameter_overlap(env, parameters, idx_gammas);
+
+    std::vector<real_type> gamma(gammas.size());
+    for(const auto& idx_gamma : idx_gammas)
+    {
+        const auto idx = idx_gamma.first;
+        const auto  gm = idx_gamma.second;
+        if(gamma.size() <= idx)
+        {
+            gamma.resize(idx+1, real_type(0.0));
+        }
+        gamma.at(idx) = gm;
+
+        MJOLNIR_LOG_INFO("idx = ", idx, ", gamma = ", gm);
+    }
+
+    std::vector<std::pair<indices_t, real_type>> indices_v0s;
+    indices_v0s.reserve(constraints.size());
+    for(const auto& params : constraints)
+    {
+        const auto offset  = find_parameter_or<std::int64_t>(params, env, "offset", 0);
+        auto indices = find_parameter<indices_t>(params, env, "indices");
+        for(auto& i : indices) { i += offset;}
+        const auto v0      = find_parameter<real_type>(params, env, "v0");
+
+        MJOLNIR_LOG_INFO_NO_LF("idxs = ", indices, ", ");
+        indices_v0s.emplace_back(indices, v0);
+
+    }
+    // TODO : check parameter overlap in indices_v0s.
+
+    return GBAOABLangevinIntegrator<traitsT>(delta_t, std::move(gamma), std::move(indices_v0s),
             read_system_motion_remover<traitsT>(simulator));
 }
 
