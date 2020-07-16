c9782e2284535502248950fd8ac9a6eb4860088e
yutaka <p7dea87c7e4n7j@gmail.com>
2020-07-16 16:49:39 +0900

    feat: GlobalStoichiometricInteraction and Potential for that.

--- mjolnir/forcefield/stoichiometric/GlobalStoichiometricInteraction.hpp
+++ mjolnir/forcefield/stoichiometric/GlobalStoichiometricInteraction.hpp
@@ -18,22 +18,53 @@ class GlobalStoichiometricInteraction final : public GlobalInteractionBase<trait
 {
   public:
 
-    using traits_type    = traitsT;
-    using base_type      = GlobalInteractionBase<traits_type>;
-    using real_type      = typename base_type::real_type;
-    using system_type    = typename base_type::system_type;
-    using topology_type  = typename base_type::topology_type;
-    using potential_type = GlobalStoichiometricInteractionPotential<traits_type>;
-    using partition_type = SpatialPartition<traitsT, potential_type>;
+    using traits_type     = traitsT;
+    using base_type       = GlobalInteractionBase<traits_type>;
+    using index_type      = std::size_t;
+    using real_type       = typename base_type::real_type;
+    using coordinate_type = typename base_type::coordinate_type;
+    using system_type     = typename base_type::system_type;
+    using topology_type   = typename base_type::topology_type;
+    using potential_type  = GlobalStoichiometricInteractionPotential<traits_type>;
+    using partition_type  = SpatialPartition<traitsT, potential_type>;
 
   public:
-    GlobalStoichiometricInteraction(potential_type&& pot, partition_type&& part)
-        : potential_(std::move(pot)), partition_(std::move(part))
-    {}
+    GlobalStoichiometricInteraction(potential_type&& pot, partition_type&& part,
+        real_type epsilon,
+        std::size_t first_coef,
+        std::size_t second_coef,
+        std::vector<std::size_t> first_kind_particles,
+        std::vector<std::size_t> second_kind_particles)
+        : potential_(std::move(pot)), partition_(std::move(part)),
+          epsilon_(epsilon), epsilon_2_(2.0 * epsilon),
+          first_coef_(first_coef),
+          second_coef_(second_coef),
+          first_kind_particles_(first_kind_particles),
+          second_kind_particles_(second_kind_particles)
+    {
+        std::size_t first_kind_particles_num  = first_kind_particles.size();
+        std::size_t second_kind_particles_num = second_kind_particles.size();
+        pot_derivs_buff_.resize(first_kind_particles_num);
+        potentials_buff_.resize(first_kind_particles_num);
+        for(auto iter=potentials_buff_.begin(); iter!=potentials_buff_.end(); ++iter)
+        {
+            (*iter).resize(second_kind_particles_num, 0.0);
+        }
+        pot_sum_for_first_   .resize(first_kind_particles_num );
+        pot_sum_for_second_  .resize(second_kind_particles_num);
+        pot_deriv_sum_for_first_ .resize(first_kind_particles_num );
+        pot_deriv_sum_for_second_.resize(second_kind_particles_num);
+    }
     ~GlobalStoichiometricInteraction() {}
 
+    /*! @brief initialize spatial partition (e.g. CellList)                   *
+     *  @details before calling `calc_(force|energy)`, this should be called. */
     void initialize(const system_type& sys, const topology_type& topol) override
     {
+        MJOLNIR_GET_DEFAULT_LOGGER();
+        MJOLNIR_LOG_FUNCTION();
+        MJOLNIR_LOG_INFO("potential is ", this->name());
+        partition_.initialize(sys, this->potential_);
     }
 
     void update(const system_type&, const topology_type&) override
@@ -58,28 +89,201 @@ class GlobalStoichiometricInteraction final : public GlobalInteractionBase<trait
     base_type* clone() const override
     {
         return new GlobalStoichiometricInteraction(
-                potential_type(potential_), partition_type(partition_));
+                potential_type(potential_), partition_type(partition_),
+                epsilon_, first_coef_, second_coef_,
+                first_kind_particles_, second_kind_particles_);
+    }
+
+  private:
+    real_type activation_func(const real_type x) const noexcept
+    {
+        // This function correspond to
+        //                     0             (x < -1)
+        // sigma(x) =  1/4 (2 + 3x - x^3)    (-1 <= x <= 1)
+        //                     1             (1 < x)
+
+        if(x < -1.0){return 0;}
+        if(1.0 < x) {return 1;}
+        const real_type x_2 = x * x;
+        const real_type x_3 = x_2 * x;
+        return (2.0 + 3.0 * x - x_3) * 0.25;
+    }
+
+    real_type deriv_activation_func(const real_type x) const
+    {
+        if(x < -1.0 || 1.0 < x){return 0;}
+        return 0.75 - 0.75 * x * x;
     }
 
   private:
 
-    potential_type potential_;
-    partition_type partition_;
+    real_type                           epsilon_;
+    real_type                           epsilon_2_;
+    std::size_t                         first_coef_;
+    std::size_t                         second_coef_;
+    std::vector<std::size_t>            first_kind_particles_;
+    std::vector<std::size_t>            second_kind_particles_;
+    potential_type                      potential_;
+    partition_type                      partition_;
+
+    // -----------------------------------------------------------------------
+    // Variables for buffering intermediate value.
+    // These value can change in calc_force and calc_energy function.
+    mutable std::vector<std::vector<real_type>>       potentials_buff_;
+    mutable std::vector<std::vector<coordinate_type>> pot_derivs_buff_;
+    mutable std::vector<real_type>                    pot_sum_for_first_;
+    mutable std::vector<real_type>                    pot_sum_for_second_;
+    // sum of derivation of potential function for specific first particle.
+    mutable std::vector<coordinate_type>              pot_deriv_sum_for_first_;
+    // sum of derivation of potential function for specific second particle.
+    mutable std::vector<coordinate_type>              pot_deriv_sum_for_second_;
+    // buffer container for pre calculated value of activation function for each particle.
+    mutable std::vector<real_type>                    activated_for_first_;
+    mutable std::vector<real_type>                    activated_for_second_;
+    // buffer container for pre calculated derivation value of activation function
+    // for each particle.
+    mutable std::vector<real_type>                    act_deriv_for_first_;
+    mutable std::vector<real_type>                    act_deriv_for_second_;
+    // temporary force container for calc_force.
+    mutable coordinate_type                           tmp_coord_first_;
+    mutable coordinate_type                           tmp_coord_second_;
 };
 
 template<typename traitsT>
-void GlobalStoichiometricInteraction<traitsT>::calc_force(
-        system_type& sys) const noexcept
+void GlobalStoichiometricInteraction<traitsT>::calc_force(system_type& sys) const noexcept
 {
-    return ;
+    MJOLNIR_GET_DEFAULT_LOGGER_DEBUG();
+    MJOLNIR_LOG_FUNCTION_DEBUG();
+
+    std::fill(pot_sum_for_first_ .begin(), pot_sum_for_first_ .end(), 0.0);
+    std::fill(pot_sum_for_second_.begin(), pot_sum_for_second_.end(), 0.0);
+    std::fill(pot_deriv_sum_for_first_ .begin(), pot_deriv_sum_for_first_ .end(),
+              math::make_coordinate<coordinate_type>(0.0, 0.0, 0.0));
+    std::fill(pot_deriv_sum_for_second_.begin(), pot_deriv_sum_for_second_.end(),
+              math::make_coordinate<coordinate_type>(0.0, 0.0, 0.0));
+    const std::size_t first_kind_particle_num  = first_kind_particles_.size();
+    const std::size_t second_kind_particle_num = second_kind_particles_.size();
+
+    // pre calculation for pot_sum and pot_deriv_sum for each particle.
+    for(std::size_t idx_first=0; idx_first<first_kind_particle_num; ++idx_first)
+    {
+        const index_type i = first_kind_particles_[idx_first];
+        std::vector<coordinate_type>& derivs_buff_first = pot_derivs_buff_[idx_first];
+        std::vector<real_type>&       pots_buff_first   = potentials_buff_[idx_first];
+        for(std::size_t idx_second=0; idx_second<second_kind_particle_num; ++idx_second)
+        {
+            const index_type j = second_kind_particles_[idx_second];
+            const coordinate_type rij =
+               sys.adjust_direction(sys.position(j) - sys.position(i));
+            const real_type       l2    = math::length_sq(rij); // |rij|^2
+            const real_type       rl    = math::rsqrt(l2);      // 1 / |rij|
+            const coordinate_type e     = rij * rl;             // rij / |rij|
+            const real_type       l     = l2 * rl;
+            const coordinate_type deriv = potential_.derivative(l) * e;
+            const real_type       pot   = potential_.potential(l);
+            derivs_buff_first[idx_second]         =  deriv;
+            pot_deriv_sum_for_first_[idx_first]   += deriv;
+            pot_deriv_sum_for_second_[idx_second] -= deriv;
+            pots_buff_first[idx_second]       =  pot;
+            pot_sum_for_first_[idx_first]     += pot;
+            pot_sum_for_second_[idx_second]   += pot;
+        }
+    }
+
+    // pre calculation for the value of activated function for each particle.
+    for(std::size_t idx_first=0; idx_first<first_kind_particle_num; ++idx_first)
+    {
+        const real_type  x = 2.0 * (first_coef_ + 0.5 - pot_sum_for_first_[idx_first]);
+        activated_for_first_[idx_first] = activation_func(x);
+        act_deriv_for_first_[idx_first] = deriv_activation_func(x);
+    }
+    for(std::size_t idx_second=0; idx_second<second_kind_particle_num; ++idx_second)
+    {
+        const real_type  x = 2.0 * (second_coef_ + 0.5 - pot_sum_for_second_[idx_second]);
+        activated_for_second_[idx_second] = activation_func(x);
+        act_deriv_for_second_[idx_second] = deriv_activation_func(x);
+    }
+
+    // force calculation
+    for(std::size_t idx_first=0; idx_first<first_kind_particle_num; ++idx_first)
+    {
+        const std::vector<real_type>&       pots_buff_first       = potentials_buff_[idx_first];
+        const std::vector<coordinate_type>& pot_derivs_buff_first = pot_derivs_buff_[idx_first];
+        for(std::size_t idx_second=0; idx_second<second_kind_particle_num; ++idx_second)
+        {
+            const real_type pots_buff_first_second = pots_buff_first[idx_second];
+            const real_type activated_for_first    = activated_for_first_[idx_first];
+            const real_type activated_for_second   = activated_for_second_[idx_second];
+            const coordinate_type& pot_derivs_buff_first_second = pot_derivs_buff_first[idx_second];
+            const real_type term1_coef =
+                activated_for_second * pots_buff_first_second *
+                pot_sum_for_first_[idx_first] * act_deriv_for_first_[idx_first];
+            const real_type term2_coef =
+                activated_for_first * pots_buff_first_second *
+                pot_sum_for_second_[idx_second] * act_deriv_for_second_[idx_second];
+            const real_type term3_coef = activated_for_first * activated_for_second;
+
+            const index_type i = first_kind_particles_[idx_first];
+            const index_type j = first_kind_particles_[idx_second];
+            sys.force(i) += epsilon_2_ *
+                            (term1_coef * pot_deriv_sum_for_first_[idx_first] +
+                            (term2_coef + term3_coef) * pot_derivs_buff_first_second);
+            sys.force(j) -= epsilon_2_ *
+                            (term2_coef * pot_deriv_sum_for_second_[idx_second] +
+                            (term1_coef + term3_coef) * pot_derivs_buff_first_second);
+        }
+    }
+
+    return;
 }
 
 template<typename traitsT>
 typename GlobalStoichiometricInteraction<traitsT>::real_type
-GlobalStoichiometricInteraction<traitsT>::calc_energy(
-        const system_type& sys) const noexcept
+GlobalStoichiometricInteraction<traitsT>::calc_energy(const system_type& sys) const noexcept
 {
-    return 0.0;
+    MJOLNIR_GET_DEFAULT_LOGGER_DEBUG();
+    MJOLNIR_LOG_FUNCTION_DEBUG();
+
+    const std::size_t first_kind_particle_num  = first_kind_particles_ .size();
+    std::size_t second_kind_particle_num = second_kind_particles_.size();
+
+    std::fill(pot_sum_for_first_ .begin(), pot_sum_for_first_ .end(), 0.0);
+    std::fill(pot_sum_for_second_.begin(), pot_sum_for_second_.end(), 0.0);
+    for(std::size_t idx_first=0; idx_first<first_kind_particle_num; ++idx_first)
+    {
+        const index_type i = first_kind_particles_[idx_first];
+        std::vector<real_type>& pots_buff_first = potentials_buff_[idx_first];
+        for(std::size_t idx_second=0; idx_second<second_kind_particle_num; ++idx_second)
+        {
+            const index_type j = second_kind_particles_[idx_second];
+            const coordinate_type rij =
+                sys.adjust_direction(sys.position(j) - sys.position(i));
+            const real_type l2  = math::length_sq(rij);
+            const real_type l   = math::rsqrt(l2);
+            const real_type pot = potential_.potential(l);
+            pots_buff_first    [idx_second] =  pot;
+            pot_sum_for_first_ [idx_first ] += pot;
+            pot_sum_for_second_[idx_second] += pot;
+        }
+    }
+
+    real_type retval(0.0);
+    for(std::size_t idx_first=0; idx_first<first_kind_particle_num; ++idx_first)
+    {
+        const std::vector<real_type>& pots_buff_first = potentials_buff_[idx_first];
+        for(std::size_t idx_second=0; idx_second<second_kind_particle_num; ++idx_second)
+        {
+            const real_type x_for_first  =
+                2.0 * (first_coef_  + 0.5 - pot_sum_for_first_[idx_first]);
+            const real_type x_for_second =
+                2.0 * (second_coef_ + 0.5 - pot_sum_for_second_[idx_second]);
+            retval += activation_func(x_for_first) * activation_func(x_for_second) *
+                      pots_buff_first[idx_second];
+        }
+    }
+    retval *= -epsilon_;
+
+    return retval;
 }
 } // mjolnir
 
--- mjolnir/forcefield/stoichiometric/GlobalStoichiometricInteractionPotential.hpp
+++ mjolnir/forcefield/stoichiometric/GlobalStoichiometricInteractionPotential.hpp
@@ -10,9 +10,11 @@ template<typename traitsT>
 class GlobalStoichiometricInteractionPotential
 {
   public:
+
     using traits_type         = traitsT;
     using real_type           = typename traits_type::real_type;
-    using parameter_type      = real_type;
+    using system_type         = System<traits_type>;
+    using parameter_type      = real_type; // \delta r
     using pair_parameter_type = parameter_type;
 
     // ------------------------------------------------------------------------
@@ -26,67 +28,109 @@ class GlobalStoichiometricInteractionPotential
     using exclusion_list_type  = ExclusionList <traits_type>;
 
   public:
-    GlobalStoichiometricInteractionPotential(
+    GlobalStoichiometricInteractionPotential(real_type v0,
+        std::vector<std::size_t>&& first_kind_participants,
+        std::vector<std::size_t>&& second_kind_participants,
         const std::map<connection_kind_type, std::size_t>& exclusions,
         ignore_molecule_type ignore_mol, ignore_group_type ignore_grp)
-        : exclusion_list_(exclusions, std::move(ignore_mol), std::move(ignore_grp))
-    {}
+        : v0_(v0), inv_v0_(1.0 / v0),
+          participants_(first_kind_participants),
+          first_kind_participants_num_(first_kind_participants.size()),
+          second_kind_participants_num_(second_kind_participants.size()),
+          exclusion_list_(exclusions, std::move(ignore_mol), std::move(ignore_grp))
+    {
+        participants_.insert(participants_.end(),
+                             second_kind_participants.begin(), second_kind_participants.end());
+    }
     ~GlobalStoichiometricInteractionPotential() = default;
     GlobalStoichiometricInteractionPotential(const GlobalStoichiometricInteractionPotential&) = default;
     GlobalStoichiometricInteractionPotential(GlobalStoichiometricInteractionPotential&&) = default;
     GlobalStoichiometricInteractionPotential& operator=(const GlobalStoichiometricInteractionPotential&) = default;
     GlobalStoichiometricInteractionPotential& operator=(GlobalStoichiometricInteractionPotential&&) = default;
 
-    // this value will be stored in NeighborList.
     pair_parameter_type prepare_params(std::size_t i, std::size_t j) const noexcept
     {
-        return pair_parameter_type(0);
+        return v0_;
     }
 
-    real_type max_cutoff_length() const
+    real_type potential(const real_type r) const noexcept
     {
-        return *(std::max_element(parameters_.cbegin(), parameters_.cend()));
+        if(v0_ < r){return 0.0;}
+
+        const real_type r_inv_v0   = r * inv_v0_;
+        const real_type r_inv_v0_2 = r_inv_v0 * r_inv_v0;
+        const real_type r_inv_v0_3 = r_inv_v0_2 * r_inv_v0;
+        return 1.0 - 3.0 * r_inv_v0_2 + 2.0 * r_inv_v0_3;
+    }
+
+    real_type derivative(const real_type r) const noexcept
+    {
+        if(v0_ < r){return 0.0;}
+
+        const real_type r_inv_v0   = r * inv_v0_;
+        const real_type r_inv_v0_2 = r_inv_v0 * r_inv_v0;
+        return 6.0 * (r_inv_v0_2 - r_inv_v0);
+    }
+
+    real_type max_cutoff_length() const noexcept
+    {
+        return 2.0 * v0_;
+    }
+
+    void initalize(const system_type& sys, const topology_type& topol) noexcept
+    {
+        MJOLNIR_GET_DEFAULT_LOGGER();
+        MJOLNIR_LOG_FUNCTION();
+
+        this->update(sys, topol);
+        return;
+    }
+
+    void update(const system_type& sys, const topology_type& topol) noexcept
+    {
+        MJOLNIR_GET_DEFAULT_LOGGER();
+        MJOLNIR_LOG_FUNCTION();
+
+        // update exclusion list based on sys.topology()
+        exclusion_list_.make(sys, topol);
     }
 
     // -----------------------------------------------------------------------
     // for spatial partitions
-    //
-    // Here, the default implementation uses Newton's 3rd law to reduce
-    // calculation. For an interacting pair (i, j), forces applied to i and j
-    // are equal in magnitude and opposite in direction. So, if a pair (i, j) is
-    // listed, (j, i) is not needed.
-    //     See implementation of VerletList, CellList and GlobalPairInteraction
-    // for more details about the usage of these functions.
 
-    std::vector<std::size_t> const& participants() const noexcept {return participants_;}
+    const std::vector<std::size_t>& participants() const noexcept {return participants_;}
 
     range<typename std::vector<std::size_t>::const_iterator>
     leading_participants() const noexcept
     {
-        return make_range(participants_.begin(), std::prev(participants_.end()));
+        return make_range(participants_.begin(), 
+                          participants_.begin() + first_kind_participants_num_);
     }
     range<typename std::vector<std::size_t>::const_iterator>
-    possible_partners_of(const std::size_t participant_idx,
+    possible_partners_of(const std::size_t /*participant_idx*/,
                          const std::size_t /*particle_idx*/) const noexcept
     {
-        return make_range(participants_.begin() + participant_idx + 1,
-                          participants_.end());
+        return make_range(participants_.begin() + first_kind_participants_num_ + 1,
+                          std::prev(participants_.end()));
     }
     bool has_interaction(const std::size_t i, const std::size_t j) const noexcept
     {
-        return (i < j) && !exclusion_list_.is_excluded(i, j);
+        return !exclusion_list_.is_excluded(i, j);
     }
 
+    // -----------------------------------------------------------------------
     // used by Observer.
-    static const char* name() noexcept {return "ExcludedVolume";}
+    static const char* name() noexcept {return "GlobalStoichiometricInteraction";}
 
   private:
 
-    std::vector<parameter_type> parameters_;
-    std::vector<std::size_t>         participants_;
+    real_type                v0_;
+    real_type                inv_v0_;
+    std::vector<std::size_t> participants_;
+    std::size_t              first_kind_participants_num_;
+    std::size_t              second_kind_participants_num_;
 
     exclusion_list_type exclusion_list_;
-
 };
 
 } // mjolnir
