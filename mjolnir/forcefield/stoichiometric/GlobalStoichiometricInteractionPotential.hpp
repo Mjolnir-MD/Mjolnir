29c6fc9003e6c43e3419b5903033e12ee9d8e7f0
yutaka <p7dea87c7e4n7j@gmail.com>
2021-03-16 21:34:04 +0900

    fix: remove interaction between leading_participants

--- mjolnir/forcefield/stoichiometric/GlobalStoichiometricInteractionPotential.hpp
+++ mjolnir/forcefield/stoichiometric/GlobalStoichiometricInteractionPotential.hpp
@@ -129,7 +129,22 @@ class GlobalStoichiometricInteractionPotential
     }
     bool has_interaction(const std::size_t i, const std::size_t j) const noexcept
     {
-        return !exclusion_list_.is_excluded(i, j);
+        // i and j need to be contained in different leading or following participants.
+        const auto lead_part = leading_participants();
+        const auto foll_part = following_participants();
+        if(std::find(lead_part.begin(), lead_part.end(), i) != lead_part.end())
+        {
+            if(std::find(foll_part.begin(), foll_part.end(), j) != foll_part.end())
+            {
+                return !exclusion_list_.is_excluded(i, j);
+            }
+        }
+        else if(std::find(lead_part.begin(), lead_part.end(), j) != lead_part.end())
+        {
+            return !exclusion_list_.is_excluded(i, j);
+        }
+
+        return false;
     }
 
     // -----------------------------------------------------------------------
