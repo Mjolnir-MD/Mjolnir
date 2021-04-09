4421366ad97d47026311cc6406fa8a583f6d61db
yutaka <p7dea87c7e4n7j@gmail.com>
2021-04-09 15:31:39 +0900

    test: expand cover range and add total force zero test

--- test/core/test_global_stoichiometric_interaction.cpp
+++ test/core/test_global_stoichiometric_interaction.cpp
@@ -66,7 +66,7 @@ BOOST_AUTO_TEST_CASE(GlobalStoichiometricInteraction_one_on_one_double)
     system.group(3) = "NONE";
 
     system.position(0) = coord_type( 1.0, 0.0, 0.0);
-    system.position(1) = coord_type( 0.0, 0.0, 0.0);
+    system.position(1) = coord_type(0.05,0.05,0.05); // to avoid completely overlap other particle
     system.position(2) = coord_type( 0.0, 1.0, 0.0);
     system.position(3) = coord_type(-1.0,-1.0, 0.0);
     system.velocity(0) = coord_type( 0.0, 0.0, 0.0);
@@ -104,7 +104,7 @@ BOOST_AUTO_TEST_CASE(GlobalStoichiometricInteraction_one_on_one_double)
     for(std::size_t idx=0; idx<system.size(); ++idx)
     {
         {
-            for(std::size_t step=0; step<10; ++step)
+            for(std::size_t step=0; step<20; ++step)
             {
                 // ----------------------------------------------------------------
                 // reset positions
@@ -128,12 +128,12 @@ BOOST_AUTO_TEST_CASE(GlobalStoichiometricInteraction_one_on_one_double)
                 // central difference
                 const auto dE = (E1 - E0) * 0.5;
 
-                BOOST_TEST(-dE / dr == mjolnir::math::X(system.force(idx)),
+                BOOST_TEST((-dE / dr) - mjolnir::math::X(system.force(idx)) == .0,
                            boost::test_tools::tolerance(tol));
             }
         }
         {
-            for(std::size_t step=0; step<10; ++step) {
+            for(std::size_t step=0; step<20; ++step) {
                 // ----------------------------------------------------------------
                 // reset positions
                 system = init;
@@ -155,12 +155,12 @@ BOOST_AUTO_TEST_CASE(GlobalStoichiometricInteraction_one_on_one_double)
                 // central difference
                 const auto dE = (E1 - E0) * 0.5;
 
-                BOOST_TEST(-dE / dr == mjolnir::math::Y(system.force(idx)),
+                BOOST_TEST((-dE / dr) - mjolnir::math::Y(system.force(idx)) == .0,
                            boost::test_tools::tolerance(tol));
             }
         }
         {
-            for(std::size_t step=0; step<10; ++step)
+            for(std::size_t step=0; step<20; ++step)
             {
                 // ----------------------------------------------------------------
                 // reset positions
@@ -184,17 +184,17 @@ BOOST_AUTO_TEST_CASE(GlobalStoichiometricInteraction_one_on_one_double)
                 // central difference
                 const auto dE = (E1 - E0) * 0.5;
 
-                BOOST_TEST(-dE / dr == mjolnir::math::Z(system.force(idx)),
+                BOOST_TEST((-dE / dr) - mjolnir::math::Z(system.force(idx)) == .0,
                            boost::test_tools::tolerance(tol));
             }
         }
     }
 }
 
-BOOST_AUTO_TEST_CASE(GlobalStoichiometricInteraction_total_force_double)
+BOOST_AUTO_TEST_CASE(GlobalStoichiometricInteraction_random_configuration_double)
 {
     mjolnir::LoggerManager::set_default_logger(
-        "test_global_stoichiometric_interaction_total_force_double.log");
+        "test_global_stoichiometric_interaction_random_configuration_double.log");
 
     using traits_type   = mjolnir::SimulatorTraits<double, mjolnir::UnlimitedBoundary>;
     using real_type     = traits_type::real_type;
@@ -210,13 +210,14 @@ BOOST_AUTO_TEST_CASE(GlobalStoichiometricInteraction_total_force_double)
 
     // set parameters for test
     constexpr real_type tol = 1e-4;
+    constexpr real_type dr  = 1e-5;
 
     // set parameters for system
     real_type particle_radius   = 1.0;
     real_type interaction_range = 1.0;
     real_type epsilon           = 1.0;
-    std::size_t particle_a_num  = 5;
-    std::size_t particle_b_num  = 5;
+    std::size_t particle_a_num  = 20;
+    std::size_t particle_b_num  = 20;
 
     // generate systems, interactions and potentials.
     std::size_t first_coef  = 1;
@@ -226,64 +227,149 @@ BOOST_AUTO_TEST_CASE(GlobalStoichiometricInteraction_total_force_double)
 
     // generate system configuration
     std::mt19937 mt(123456789);
-    real_type box_edge = 2.0;
+    real_type box_edge = 3.0;
     std::uniform_real_distribution<real_type> uni(-box_edge, box_edge);
 
-    // particle A setup
-    for(std::size_t i = 0; i < particle_a_num; ++i)
+    for(std::size_t trial_idx=0; trial_idx<100; ++trial_idx)
     {
-        system.mass(i)  = 1.0;
-        system.rmass(i) = 1.0;
-        system.name(i)  = "A";
-        system.group(i) = "NONE";
-        system.position(i) = coord_type(uni(mt), uni(mt), uni(mt));
-        system.velocity(i) = coord_type(0, 0, 0);
-        system.force(i)    = coord_type(0, 0, 0);
+        // particle A setup
+        for(std::size_t i = 0; i < particle_a_num; ++i)
+        {
+            system.mass(i)  = 1.0;
+            system.rmass(i) = 1.0;
+            system.name(i)  = "A";
+            system.group(i) = "NONE";
+            system.position(i) = coord_type(uni(mt), uni(mt), uni(mt));
+            system.velocity(i) = coord_type(0, 0, 0);
+            system.force(i)    = coord_type(0, 0, 0);
+        }
+
+        // particle B setup
+        for(std::size_t i = particle_a_num; i < particle_a_num+particle_b_num; ++i)
+        {
+            system.mass(i)  = 1.0;
+            system.rmass(i) = 1.0;
+            system.name(i)  = "B";
+            system.group(i) = "NONE";
+            system.position(i) = coord_type(uni(mt), uni(mt), uni(mt));
+            system.velocity(i) = coord_type(0, 0, 0);
+            system.force(i)    = coord_type(0, 0, 0);
+        }
+
+        std::vector<std::size_t> particle_a_arr(particle_a_num);
+        std::iota(particle_a_arr.begin(), particle_a_arr.end(), 0);
+        std::vector<std::size_t> particle_b_arr(particle_b_num);
+        std::iota(particle_b_arr.begin(), particle_b_arr.end(), particle_a_num);
+        potential_type potential = potential_type(particle_radius, interaction_range,
+            std::move(particle_a_arr), std::move(particle_b_arr),
+            {}, typename potential_type::ignore_molecule_type("Nothing"),
+                typename potential_type::ignore_group_type   ({})
+            );
+
+        interaction_type interaction = interaction_type(potential_type{potential},
+            mjolnir::SpatialPartition<traits_type, potential_type>(
+                mjolnir::make_unique<partition_type>()),
+            epsilon, first_coef, second_coef);
+
+        topology_type topology = topology_type(particle_a_num+particle_b_num);
+
+        topology.construct_molecules();
+        interaction.initialize(system, topology);
+
+        const system_type init = system;
+
+        // check equality of differential and force of each particle
+        for(std::size_t idx=0; idx<system.size(); ++idx)
+        {
+            {
+                // ----------------------------------------------------------------
+                // reset positions
+                system = init;
+                // move particle for test
+
+                // calc U(x-dx)
+                const auto E0 = interaction.calc_energy(system);
+
+                mjolnir::math::X(system.position(idx)) += dr;
+
+                // calc F(x)
+                interaction.calc_force(system);
+
+                mjolnir::math::X(system.position(idx)) += dr;
+
+                // calc U(x+dx)
+                const auto E1 = interaction.calc_energy(system);
+
+                // central difference
+                const auto dE = (E1 - E0) * 0.5;
+
+                BOOST_TEST((-dE / dr) - mjolnir::math::X(system.force(idx)) == .0,
+                           boost::test_tools::tolerance(tol));
+            }
+            {
+                // ----------------------------------------------------------------
+                // reset positions
+                system = init;
+                // move particle for test
+
+                // calc U(x-dx)
+                const auto E0 = interaction.calc_energy(system);
+
+                mjolnir::math::Y(system.position(idx)) += dr;
+
+                // calc F(x)
+                interaction.calc_force(system);
+
+                mjolnir::math::Y(system.position(idx)) += dr;
+
+                // calc U(x+dx)
+                const auto E1 = interaction.calc_energy(system);
+
+                // central difference
+                const auto dE = (E1 - E0) * 0.5;
+
+                BOOST_TEST((-dE / dr) - mjolnir::math::Y(system.force(idx)) == .0,
+                           boost::test_tools::tolerance(tol));
+            }
+            {
+                // ----------------------------------------------------------------
+                // reset positions
+                system = init;
+                // move particle for test
+
+                // calc U(x-dx)
+                const auto E0 = interaction.calc_energy(system);
+
+                mjolnir::math::Z(system.position(idx)) += dr;
+
+                // calc F(x)
+                interaction.calc_force(system);
+
+                mjolnir::math::Z(system.position(idx)) += dr;
+
+                // calc U(x+dx)
+                const auto E1 = interaction.calc_energy(system);
+
+                // central difference
+                const auto dE = (E1 - E0) * 0.5;
+
+                BOOST_TEST((-dE / dr) - mjolnir::math::Z(system.force(idx)) == .0,
+                           boost::test_tools::tolerance(tol));
+            }
+        }
+
+        // check total force is 0
+        system = init;
+        interaction.calc_force(system);
+        coord_type total_force(0.0, 0.0, 0.0);
+        for(std::size_t idx=0; idx<system.size(); ++idx)
+        {
+            total_force += system.force(idx);
+        }
+        BOOST_TEST(mjolnir::math::X(total_force) == 0.0, boost::test_tools::tolerance(tol));
+        BOOST_TEST(mjolnir::math::Y(total_force) == 0.0, boost::test_tools::tolerance(tol));
+        BOOST_TEST(mjolnir::math::Z(total_force) == 0.0, boost::test_tools::tolerance(tol));
     }
-
-    // particle B setup
-    for(std::size_t i = particle_a_num; i < particle_a_num+particle_b_num; ++i)
-    {
-        system.mass(i)  = 1.0;
-        system.rmass(i) = 1.0;
-        system.name(i)  = "B";
-        system.group(i) = "NONE";
-        system.position(i) = coord_type(uni(mt), uni(mt), uni(mt));
-        system.velocity(i) = coord_type(0, 0, 0);
-        system.force(i)    = coord_type(0, 0, 0);
-
-    }
-
-    std::vector<std::size_t> particle_a_arr(particle_a_num);
-    std::iota(particle_a_arr.begin(), particle_a_arr.end(), 0);
-    std::vector<std::size_t> particle_b_arr(particle_b_num);
-    std::iota(particle_b_arr.begin(), particle_b_arr.end(), particle_a_num);
-    potential_type potential = potential_type(particle_radius, interaction_range,
-        std::move(particle_a_arr), std::move(particle_b_arr),
-        {}, typename potential_type::ignore_molecule_type("Nothing"),
-            typename potential_type::ignore_group_type   ({})
-        );
-
-    interaction_type interaction = interaction_type(potential_type{potential},
-        mjolnir::SpatialPartition<traits_type, potential_type>(
-            mjolnir::make_unique<partition_type>()),
-        epsilon, first_coef, second_coef);
-
-    topology_type topology = topology_type(500);
-
-    topology.construct_molecules();
-    interaction.initialize(system, topology);
-
-    // check total force is 0
-    interaction.calc_force(system);
-    coord_type total_force(0.0, 0.0, 0.0);
-    for(std::size_t idx=0; idx<system.size(); ++idx)
-    {
-        total_force += system.force(idx);
-    }
-    BOOST_TEST(mjolnir::math::X(total_force) == 0.0, boost::test_tools::tolerance(tol));
-    BOOST_TEST(mjolnir::math::Y(total_force) == 0.0, boost::test_tools::tolerance(tol));
-    BOOST_TEST(mjolnir::math::Z(total_force) == 0.0, boost::test_tools::tolerance(tol));
 }
 
 BOOST_AUTO_TEST_CASE(GlobalStoichiometricInteraction_one_on_two_double)
@@ -409,7 +495,7 @@ BOOST_AUTO_TEST_CASE(GlobalStoichiometricInteraction_one_on_two_double)
                 // central difference
                 const auto dE = (E1 - E0) * 0.5;
 
-                BOOST_TEST(-dE / dr == mjolnir::math::X(system.force(idx)),
+                BOOST_TEST((-dE/dr) - mjolnir::math::X(system.force(idx)) == .0,
                            boost::test_tools::tolerance(tol));
             }
         }
@@ -436,7 +522,7 @@ BOOST_AUTO_TEST_CASE(GlobalStoichiometricInteraction_one_on_two_double)
                 // central difference
                 const auto dE = (E1 - E0) * 0.5;
 
-                BOOST_TEST(-dE / dr == mjolnir::math::Y(system.force(idx)),
+                BOOST_TEST((-dE/dr) - mjolnir::math::Y(system.force(idx)) == .0,
                            boost::test_tools::tolerance(tol));
             }
         }
@@ -465,7 +551,7 @@ BOOST_AUTO_TEST_CASE(GlobalStoichiometricInteraction_one_on_two_double)
                 // central difference
                 const auto dE = (E1 - E0) * 0.5;
 
-                BOOST_TEST(-dE / dr == mjolnir::math::Z(system.force(idx)),
+                BOOST_TEST((-dE/dr) - mjolnir::math::Z(system.force(idx)) == .0,
                            boost::test_tools::tolerance(tol));
             }
         }
