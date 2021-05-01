d85fb3b0467220bef8c5f0b5981f4c9f2767ec50
ToruNiina <niina.toru.68u@gmail.com>
2021-05-01 15:19:45 +0900

    feat: add MBasinContact

---
+++ mjolnir/forcefield/local/MBasinAttractivePotential.cpp
@@ -0,0 +1,11 @@
+#include <mjolnir/forcefield/local/MBasinAttractivePotential.hpp>
+
+#ifndef MJOLNIR_SEPARATE_BUILD
+#error "MJOLNIR_SEPARATE_BUILD flag is required"
+#endif
+
+namespace mjolnir
+{
+template class MBasinAttractivePotential<double>;
+template class MBasinAttractivePotential<float>;
+} // mjolnir
