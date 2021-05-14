ec6dfe0d0e49532bd8cf26e7a6694c1b27d8e19d
yutaka <p7dea87c7e4n7j@gmail.com>
2021-05-14 20:13:30 +0900

    feat: add UniformCubicFunction case to read global interaction

--- mjolnir/input/read_global_interaction.hpp
+++ mjolnir/input/read_global_interaction.hpp
@@ -161,6 +161,16 @@ read_global_pair_interaction(const toml::value& global)
                 std::move(pot_para.first), std::move(pot_para.second),
                 read_spatial_partition<traitsT, potential_t>(global));
     }
+    else if(potential == "UniformCubicFunction")
+    {
+        MJOLNIR_LOG_NOTICE("-- potential function is UniformCubicFunction");
+        using potential_t   = UniformCubicFunctionPotential<traitsT>;
+        using interaction_t = GlobalPairInteraction<traitsT, potential_t>;
+
+        return make_unique<interaction_t>(
+            read_uniform_cubic_function_potential<traitsT>(global),
+            read_spatial_partition<traitsT, potential_t>(global));
+    }
     else
     {
         throw_exception<std::runtime_error>(toml::format_error("[error] "
@@ -173,7 +183,8 @@ read_global_pair_interaction(const toml::value& global)
             "- \"LennardJones\"         : famous r^12 - r^6 potential",
             "- \"UniformLennardJones\"  : famous r^12 - r^6 potential with uniform parameters",
             "- \"3SPN2ExcludedVolume\"  : excluded volume for 3SPN2 DNA model",
-            "- \"iSoLFAttractive\"      : attractive potential for iSoLF lipid model"
+            "- \"iSoLFAttractive\"      : attractive potential for iSoLF lipid model",
+            "- \"UniformCubicFunction\" : cubic function potential with uniform paramters"
             }));
     }
 }
