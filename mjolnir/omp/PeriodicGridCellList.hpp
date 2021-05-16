e02ac4d05876dd530a5afecc7d715a2049ee18c0
ToruNiina <niina.toru.68u@gmail.com>
2021-05-16 14:07:14 +0900

    feat: enable to use CellList with NPT integrator
    check box size in cell list and adjust cell width. It changes the number
    of cells if needed.

--- mjolnir/omp/PeriodicGridCellList.hpp
+++ mjolnir/omp/PeriodicGridCellList.hpp
@@ -80,92 +80,21 @@ class PeriodicGridCellList<OpenMPSimulatorTraits<realT, boundaryT>, potentialT>
         MJOLNIR_LOG_INFO(pot.name(), " cutoff = ", max_cutoff);
 
         this->lower_bound_ = sys.boundary().lower_bound();
-        const auto system_size = sys.boundary().width();
+        this->system_size_ = sys.boundary().width();
 
-        this->dim_x_ = std::max<std::size_t>(3, std::floor(math::X(system_size) * r_x_));
-        this->dim_y_ = std::max<std::size_t>(3, std::floor(math::Y(system_size) * r_y_));
-        this->dim_z_ = std::max<std::size_t>(3, std::floor(math::Z(system_size) * r_z_));
+        const auto dim_x = std::max<std::size_t>(3, std::floor(math::X(this->system_size_) * r_x_));
+        const auto dim_y = std::max<std::size_t>(3, std::floor(math::Y(this->system_size_) * r_y_));
+        const auto dim_z = std::max<std::size_t>(3, std::floor(math::Z(this->system_size_) * r_z_));
 
-        MJOLNIR_LOG_INFO("dimension = ", dim_x_, 'x', dim_y_, 'x', dim_z_);
+        // allocate list of cells and set connectivity between them
+        this->construct_cells(dim_x, dim_y, dim_z);
 
-        if(dim_x_ == 3 || dim_y_ == 3 || dim_z_ == 3)
-        {
-            MJOLNIR_LOG_WARN("system size is too small (",
-                    dim_x_, 'x', dim_y_, 'x', dim_z_,
-                    "). This might cause a problem. Please check the cutoff ratio"
-                    " and the box size!");
-        }
+        // adjust cell width
+        this->r_x_ = real_type(1) / (math::X(this->system_size_) / this->dim_x_);
+        this->r_y_ = real_type(1) / (math::Y(this->system_size_) / this->dim_y_);
+        this->r_z_ = real_type(1) / (math::Z(this->system_size_) / this->dim_z_);
 
-        // it may expand cell a bit (to fit system range)
-        this->r_x_ = 1.0 / (math::X(system_size) / this->dim_x_);
-        this->r_y_ = 1.0 / (math::Y(system_size) / this->dim_y_);
-        this->r_z_ = 1.0 / (math::Z(system_size) / this->dim_z_);
-
-        MJOLNIR_LOG_DEBUG("reciplocal width of cells in x coordinate = ", r_x_);
-        MJOLNIR_LOG_DEBUG("reciplocal width of cells in y coordinate = ", r_y_);
-        MJOLNIR_LOG_DEBUG("reciplocal width of cells in z coordinate = ", r_z_);
-
-        this->cell_list_.resize(dim_x_ * dim_y_ * dim_z_);
-
-        const int dimx = dim_x_;
-        const int dimy = dim_y_;
-        const int dimz = dim_z_;
-
-#pragma omp parallel for
-        for(int x = 0; x < dimx; ++x)
-        {
-            for(int y = 0; y < dimy; ++y)
-            {
-            for(int z = 0; z < dimz; ++z)
-            {
-                auto& cell = this->cell_list_[calc_index(x, y, z)];
-
-                const std::size_t x_prev = (x ==        0) ? dimx - 1 : x - 1;
-                const std::size_t x_next = (x == dimx - 1) ?        0 : x + 1;
-                const std::size_t y_prev = (y ==        0) ? dimy - 1 : y - 1;
-                const std::size_t y_next = (y == dimy - 1) ?        0 : y + 1;
-                const std::size_t z_prev = (z ==        0) ? dimz - 1 : z - 1;
-                const std::size_t z_next = (z == dimz - 1) ?        0 : z + 1;
-
-                cell.second[ 0] = calc_index(x_prev, y_prev, z_prev);
-                cell.second[ 1] = calc_index(x,      y_prev, z_prev);
-                cell.second[ 2] = calc_index(x_next, y_prev, z_prev);
-                cell.second[ 3] = calc_index(x_prev, y,      z_prev);
-                cell.second[ 4] = calc_index(x,      y,      z_prev);
-                cell.second[ 5] = calc_index(x_next, y,      z_prev);
-                cell.second[ 6] = calc_index(x_prev, y_next, z_prev);
-                cell.second[ 7] = calc_index(x,      y_next, z_prev);
-                cell.second[ 8] = calc_index(x_next, y_next, z_prev);
-
-                cell.second[ 9] = calc_index(x_prev, y_prev, z);
-                cell.second[10] = calc_index(x,      y_prev, z);
-                cell.second[11] = calc_index(x_next, y_prev, z);
-                cell.second[12] = calc_index(x_prev, y,      z);
-                cell.second[13] = calc_index(x,      y,      z);
-                cell.second[14] = calc_index(x_next, y,      z);
-                cell.second[15] = calc_index(x_prev, y_next, z);
-                cell.second[16] = calc_index(x,      y_next, z);
-                cell.second[17] = calc_index(x_next, y_next, z);
-
-                cell.second[18] = calc_index(x_prev, y_prev, z_next);
-                cell.second[19] = calc_index(x,      y_prev, z_next);
-                cell.second[20] = calc_index(x_next, y_prev, z_next);
-                cell.second[21] = calc_index(x_prev, y,      z_next);
-                cell.second[22] = calc_index(x,      y,      z_next);
-                cell.second[23] = calc_index(x_next, y,      z_next);
-                cell.second[24] = calc_index(x_prev, y_next, z_next);
-                cell.second[25] = calc_index(x,      y_next, z_next);
-                cell.second[26] = calc_index(x_next, y_next, z_next);
-
-                auto uniq = std::unique(cell.second.begin(), cell.second.end());
-                assert(uniq == cell.second.end());
-                for(auto i : cell.second)
-                {
-                    assert(i <= cell_list_.size());
-                }
-            }
-            }
-        }
+        // construct neighbor list using cells
         this->make(neighbors, sys, pot);
         return;
     }
@@ -173,6 +102,50 @@ class PeriodicGridCellList<OpenMPSimulatorTraits<realT, boundaryT>, potentialT>
     void make(neighbor_list_type& neighbor_list,
               const system_type& sys, const potential_type& pot) override
     {
+        // first check the system size because the box size might change under NPT.
+        // If it does not match exactly, it means we may need to reconstruct the
+        // cell list.
+        //
+        // If the dimension in each direction is the same, then we don't need to
+        // re-construct the cell list because the number of cells and their
+        // connectivity are kept.
+        {
+            const auto current_lower_bound = sys.boundary().lower_bound();
+            const auto current_system_size = sys.boundary().width();
+            if(math::X(lower_bound_) != math::X(current_lower_bound) ||
+               math::Y(lower_bound_) != math::Y(current_lower_bound) ||
+               math::Z(lower_bound_) != math::Z(current_lower_bound) ||
+               math::X(system_size_) != math::X(current_system_size) ||
+               math::Y(system_size_) != math::Y(current_system_size) ||
+               math::Z(system_size_) != math::Z(current_system_size) )
+            {
+                this->lower_bound_ = current_lower_bound;
+                this->system_size_ = current_system_size;
+
+                const auto dim_x = std::max<std::size_t>(3, std::floor(math::X(system_size_) * r_x_));
+                const auto dim_y = std::max<std::size_t>(3, std::floor(math::Y(system_size_) * r_y_));
+                const auto dim_z = std::max<std::size_t>(3, std::floor(math::Z(system_size_) * r_z_));
+
+                // if the number of cells changes, we need to reconstruct the
+                // cell size.
+                if(dim_x != dim_x_ || dim_y != dim_y_ || dim_z != dim_z_)
+                {
+                    // the number of the cells changes and redefine connections.
+                    // Also, member variable dim_x_, y_, z_ are updated
+                    this->construct_cells(dim_x, dim_y, dim_z);
+                }
+
+                // update this regardless of the number of cells
+                this->r_x_ = 1.0 / (math::X(system_size_) / this->dim_x_);
+                this->r_y_ = 1.0 / (math::Y(system_size_) / this->dim_y_);
+                this->r_z_ = 1.0 / (math::Z(system_size_) / this->dim_z_);
+
+                MJOLNIR_LOG_DEBUG("reciplocal width of cells in x coordinate = ", r_x_);
+                MJOLNIR_LOG_DEBUG("reciplocal width of cells in y coordinate = ", r_y_);
+                MJOLNIR_LOG_DEBUG("reciplocal width of cells in z coordinate = ", r_z_);
+            }
+        }
+
         // `participants` is a list that contains indices of particles that are
         // related to the potential.
         const auto& participants = pot.participants();
@@ -406,6 +379,90 @@ class PeriodicGridCellList<OpenMPSimulatorTraits<realT, boundaryT>, potentialT>
         return;
     }
 
+    void construct_cells(const std::size_t dim_x, const std::size_t dim_y,
+                         const std::size_t dim_z)
+    {
+        MJOLNIR_GET_DEFAULT_LOGGER();
+        MJOLNIR_LOG_FUNCTION();
+
+        this->dim_x_ = dim_x;
+        this->dim_y_ = dim_y;
+        this->dim_z_ = dim_z;
+
+        MJOLNIR_LOG_INFO("dimension = ", dim_x_, 'x', dim_y_, 'x', dim_z_);
+
+        if(dim_x_ == 3 || dim_y_ == 3 || dim_z_ == 3)
+        {
+            MJOLNIR_LOG_WARN("system size is too small (",
+                    dim_x_, 'x', dim_y_, 'x', dim_z_,
+                    "). This might cause a problem. Please check the cutoff ratio"
+                    " and the box size!");
+        }
+
+        this->cell_list_.resize(dim_x_ * dim_y_ * dim_z_);
+
+        const int dimx = dim_x_;
+        const int dimy = dim_y_;
+        const int dimz = dim_z_;
+
+#pragma omp parallel for
+        for(int x = 0; x < dimx; ++x)
+        {
+            for(int y = 0; y < dimy; ++y)
+            {
+                for(int z = 0; z < dimz; ++z)
+                {
+                    auto& cell = this->cell_list_[calc_index(x, y, z)];
+
+                    const std::size_t x_prev = (x ==        0) ? dimx - 1 : x - 1;
+                    const std::size_t x_next = (x == dimx - 1) ?        0 : x + 1;
+                    const std::size_t y_prev = (y ==        0) ? dimy - 1 : y - 1;
+                    const std::size_t y_next = (y == dimy - 1) ?        0 : y + 1;
+                    const std::size_t z_prev = (z ==        0) ? dimz - 1 : z - 1;
+                    const std::size_t z_next = (z == dimz - 1) ?        0 : z + 1;
+
+                    cell.second[ 0] = calc_index(x_prev, y_prev, z_prev);
+                    cell.second[ 1] = calc_index(x,      y_prev, z_prev);
+                    cell.second[ 2] = calc_index(x_next, y_prev, z_prev);
+                    cell.second[ 3] = calc_index(x_prev, y,      z_prev);
+                    cell.second[ 4] = calc_index(x,      y,      z_prev);
+                    cell.second[ 5] = calc_index(x_next, y,      z_prev);
+                    cell.second[ 6] = calc_index(x_prev, y_next, z_prev);
+                    cell.second[ 7] = calc_index(x,      y_next, z_prev);
+                    cell.second[ 8] = calc_index(x_next, y_next, z_prev);
+
+                    cell.second[ 9] = calc_index(x_prev, y_prev, z);
+                    cell.second[10] = calc_index(x,      y_prev, z);
+                    cell.second[11] = calc_index(x_next, y_prev, z);
+                    cell.second[12] = calc_index(x_prev, y,      z);
+                    cell.second[13] = calc_index(x,      y,      z);
+                    cell.second[14] = calc_index(x_next, y,      z);
+                    cell.second[15] = calc_index(x_prev, y_next, z);
+                    cell.second[16] = calc_index(x,      y_next, z);
+                    cell.second[17] = calc_index(x_next, y_next, z);
+
+                    cell.second[18] = calc_index(x_prev, y_prev, z_next);
+                    cell.second[19] = calc_index(x,      y_prev, z_next);
+                    cell.second[20] = calc_index(x_next, y_prev, z_next);
+                    cell.second[21] = calc_index(x_prev, y,      z_next);
+                    cell.second[22] = calc_index(x,      y,      z_next);
+                    cell.second[23] = calc_index(x_next, y,      z_next);
+                    cell.second[24] = calc_index(x_prev, y_next, z_next);
+                    cell.second[25] = calc_index(x,      y_next, z_next);
+                    cell.second[26] = calc_index(x_next, y_next, z_next);
+
+                    auto uniq = std::unique(cell.second.begin(), cell.second.end());
+                    assert(uniq == cell.second.end());
+                    for(auto i : cell.second)
+                    {
+                        assert(i <= cell_list_.size());
+                    }
+                }
+            }
+        }
+        return;
+    }
+
   private:
 
     real_type   cutoff_;
@@ -419,6 +476,7 @@ class PeriodicGridCellList<OpenMPSimulatorTraits<realT, boundaryT>, potentialT>
     std::size_t dim_z_;
 
     coordinate_type           lower_bound_;
+    coordinate_type           system_size_; // size of the boundary condition. in NPT, we need to check it
     cell_list_type            cell_list_;
     cell_index_container_type index_by_cell_;
     cell_index_container_type index_by_cell_buf_;
