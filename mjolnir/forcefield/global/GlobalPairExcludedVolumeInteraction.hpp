66a047f04bad798cfb01ee8437579ba3524ed5af
ToruNiina <niina.toru.68u@gmail.com>
2021-05-22 13:05:30 +0900

    feat: update specialized ExcludedVolume

--- mjolnir/forcefield/global/GlobalPairExcludedVolumeInteraction.hpp
+++ mjolnir/forcefield/global/GlobalPairExcludedVolumeInteraction.hpp
@@ -14,7 +14,7 @@ namespace mjolnir
 template<typename realT, template<typename, typename> class boundaryT>
 class GlobalPairInteraction<
     SimulatorTraits<realT, boundaryT>,
-    ExcludedVolumePotential<SimulatorTraits<realT, boundaryT>>
+    ExcludedVolumePotential<realT>
     > final : public GlobalInteractionBase<SimulatorTraits<realT, boundaryT>>
 {
   public:
@@ -26,16 +26,16 @@ class GlobalPairInteraction<
     using system_type     = typename base_type::system_type;
     using topology_type   = typename base_type::topology_type;
     using boundary_type   = typename base_type::boundary_type;
-    using potential_type  = ExcludedVolumePotential<traits_type>;
+    using potential_type  = ExcludedVolumePotential<real_type>;
     using partition_type  = SpatialPartition<traits_type, potential_type>;
+    using parameter_list_type = ParameterList<traits_type, potential_type>;
 
   public:
-    GlobalPairInteraction()           = default;
-    ~GlobalPairInteraction() override {}
 
-    GlobalPairInteraction(potential_type&& pot, partition_type&& part)
-        : potential_(std::move(pot)), partition_(std::move(part))
+    GlobalPairInteraction(parameter_list_type&& para, partition_type&& part)
+        : parameters_(std::move(para)), partition_(std::move(part))
     {}
+    ~GlobalPairInteraction() override {}
 
     /*! @brief initialize spatial partition (e.g. CellList)                   *
      *  @details before calling `calc_(force|energy)`, this should be called. */
@@ -44,8 +44,9 @@ class GlobalPairInteraction<
         MJOLNIR_GET_DEFAULT_LOGGER();
         MJOLNIR_LOG_FUNCTION();
         MJOLNIR_LOG_INFO("potential is ", this->name());
-        this->potential_.initialize(sys, topol);
-        this->partition_.initialize(sys, this->potential_);
+
+        this->parameters_.initialize(sys, topol);
+        this->partition_ .initialize(sys, parameters_.cref());
     }
 
     /*! @brief update parameters (e.g. temperature, ionic strength, ...)  *
@@ -57,48 +58,43 @@ class GlobalPairInteraction<
         MJOLNIR_GET_DEFAULT_LOGGER();
         MJOLNIR_LOG_FUNCTION();
         MJOLNIR_LOG_INFO("potential is ", this->name());
-        this->potential_.update(sys, topol);
-        // potential update may change the cutoff length!
-        this->partition_.initialize(sys, this->potential_);
+
+        this->parameters_.update(sys, topol);
+        this->partition_.initialize(sys, this->parameters_.cref());
     }
 
     void reduce_margin(const real_type dmargin, const system_type& sys) override
     {
-        this->partition_.reduce_margin(dmargin, sys, this->potential_);
+        this->partition_.reduce_margin(dmargin, sys, this->parameters_.cref());
         return;
     }
     void scale_margin(const real_type scale, const system_type& sys) override
     {
-        this->partition_.scale_margin(scale, sys, this->potential_);
+        this->partition_.scale_margin(scale, sys, this->parameters_.cref());
         return;
     }
 
     void calc_force(system_type& sys) const noexcept override
     {
-        MJOLNIR_GET_DEFAULT_LOGGER_DEBUG();
-        MJOLNIR_LOG_FUNCTION_DEBUG();
-
-        const auto cutoff_ratio    = potential_.cutoff_ratio();
+        const auto cutoff_ratio    = potential_type::cutoff_ratio;
         const auto cutoff_ratio_sq = cutoff_ratio * cutoff_ratio;
-        const auto epsilon12       = 12 * potential_.epsilon();
 
-        const auto leading_participants = this->potential_.leading_participants();
-        for(std::size_t idx=0; idx<leading_participants.size(); ++idx)
+        const auto leading_participants = this->parameters_.leading_participants();
+        for(const std::size_t i : leading_participants)
         {
-            const auto i = leading_participants[idx];
             for(const auto& ptnr : this->partition_.partners(i))
             {
-                const auto  j     = ptnr.index;
-                const auto& param = ptnr.parameter(); // sum of radius
+                const auto  j   = ptnr.index;
+                const auto& pot = ptnr.potential();
 
                 const coordinate_type rij =
                     sys.adjust_direction(sys.position(i), sys.position(j));
                 const real_type l_sq = math::length_sq(rij);
 
-                const real_type sigma_sq = param * param;
+                const real_type sigma_sq = pot.sigma() * pot.sigma();
                 if(sigma_sq * cutoff_ratio_sq < l_sq) {continue;}
 
-                MJOLNIR_LOG_DEBUG("calculating force between ", i, " and ", j);
+                const real_type epsilon12 = real_type(12) * pot.epsilon();
 
                 const real_type rcp_l_sq = real_type(1) / l_sq;
                 const real_type s2l2     = sigma_sq * rcp_l_sq;
@@ -119,81 +115,68 @@ class GlobalPairInteraction<
 
     real_type calc_energy(const system_type& sys) const noexcept override
     {
-        MJOLNIR_GET_DEFAULT_LOGGER_DEBUG();
-        MJOLNIR_LOG_FUNCTION_DEBUG();
+        real_type energy(0);
 
-        real_type E(0);
-
-        const auto cutoff_ratio    = potential_.cutoff_ratio();
+        const auto cutoff_ratio    = potential_type::cutoff_ratio;
         const auto cutoff_ratio_sq = cutoff_ratio * cutoff_ratio;
-        const auto coef_at_cutoff  = potential_.coef_at_cutoff();
-        const auto epsilon         = potential_.epsilon();
+        const auto coef_at_cutoff  = potential_type::coef_at_cutoff;
 
-        const auto leading_participants = this->potential_.leading_participants();
-        for(std::size_t idx=0; idx<leading_participants.size(); ++idx)
+        const auto leading_participants = this->parameters_.leading_participants();
+        for(const std::size_t i : leading_participants)
         {
-            const auto i = leading_participants[idx];
             for(const auto& ptnr : this->partition_.partners(i))
             {
-                const auto  j     = ptnr.index;
-                const auto& param = ptnr.parameter();
+                const auto  j   = ptnr.index;
+                const auto& pot = ptnr.potential();
 
                 const coordinate_type rij =
                     sys.adjust_direction(sys.position(i), sys.position(j));
                 const real_type l_sq = math::length_sq(rij);
 
-                const real_type sigma_sq = param * param;
+                const real_type sigma_sq = pot.sigma() * pot.sigma();
                 if(sigma_sq * cutoff_ratio_sq < l_sq) {continue;}
-
-                MJOLNIR_LOG_DEBUG("calculating energy between ", i, " and ", j);
 
                 const real_type s2l2 = sigma_sq / l_sq;
                 const real_type s6l6 = s2l2 * s2l2 * s2l2;
 
-                E += epsilon * (s6l6 * s6l6 - coef_at_cutoff);
+                energy += pot.epsilon() * (s6l6 * s6l6 - coef_at_cutoff);
             }
         }
-        return E;
+        return energy;
     }
 
     real_type calc_force_and_energy(system_type& sys) const noexcept override
     {
-        MJOLNIR_GET_DEFAULT_LOGGER_DEBUG();
-        MJOLNIR_LOG_FUNCTION_DEBUG();
-
-        const auto coef_at_cutoff  = potential_.coef_at_cutoff();
-        const auto cutoff_ratio    = potential_.cutoff_ratio();
+        const auto coef_at_cutoff  = potential_type::coef_at_cutoff;
+        const auto cutoff_ratio    = potential_type::cutoff_ratio;
         const auto cutoff_ratio_sq = cutoff_ratio * cutoff_ratio;
-        const auto epsilon         = potential_.epsilon();
-        const auto epsilon12       = 12 * epsilon;
 
         real_type energy = 0;
-        const auto leading_participants = this->potential_.leading_participants();
-        for(std::size_t idx=0; idx<leading_participants.size(); ++idx)
+        const auto leading_participants = this->parameters_.leading_participants();
+        for(const std::size_t i : leading_participants)
         {
-            const auto i = leading_participants[idx];
             for(const auto& ptnr : this->partition_.partners(i))
             {
-                const auto  j     = ptnr.index;
-                const auto& param = ptnr.parameter(); // sum of radius
+                const auto  j   = ptnr.index;
+                const auto& pot = ptnr.potential();
 
                 const coordinate_type rij =
                     sys.adjust_direction(sys.position(i), sys.position(j));
                 const real_type l_sq = math::length_sq(rij);
 
-                const real_type sigma_sq = param * param;
+                const real_type sigma_sq = pot.sigma() * pot.sigma();
                 if(sigma_sq * cutoff_ratio_sq < l_sq) {continue;}
-
-                MJOLNIR_LOG_DEBUG("calculating force between ", i, " and ", j);
 
                 const real_type rcp_l_sq = real_type(1) / l_sq;
                 const real_type s2l2     = sigma_sq * rcp_l_sq;
                 const real_type s6l6     = s2l2 * s2l2 * s2l2;
 
+                const real_type epsilon   = pot.epsilon();
+                const real_type epsilon12 = real_type(12) * epsilon;
+
                 energy += epsilon * (s6l6 * s6l6 - coef_at_cutoff);
 
-                const coordinate_type f = rij *
-                    (-epsilon12 * s6l6 * s6l6 * rcp_l_sq);
+                const auto f = rij * (-epsilon12 * s6l6 * s6l6 * rcp_l_sq);
 
                 sys.force(i) += f;
                 sys.force(j) -= f;
@@ -209,13 +192,12 @@ class GlobalPairInteraction<
 
     base_type* clone() const override
     {
-        return new GlobalPairInteraction(
-                potential_type(potential_), partition_type(partition_));
+        return new GlobalPairInteraction(*this);
     }
 
   private:
 
-    potential_type potential_;
+    parameter_list_type parameters_;
     partition_type partition_;
 };
 
@@ -230,10 +212,10 @@ namespace mjolnir
 // ============================================================================
 // exv
 // CellList
-extern template class GlobalPairInteraction<SimulatorTraits<double, UnlimitedBoundary>       , ExcludedVolumePotential<SimulatorTraits<double, UnlimitedBoundary>       >>;
-extern template class GlobalPairInteraction<SimulatorTraits<float,  UnlimitedBoundary>       , ExcludedVolumePotential<SimulatorTraits<float,  UnlimitedBoundary>       >>;
-extern template class GlobalPairInteraction<SimulatorTraits<double, CuboidalPeriodicBoundary>, ExcludedVolumePotential<SimulatorTraits<double, CuboidalPeriodicBoundary>>>;
-extern template class GlobalPairInteraction<SimulatorTraits<float,  CuboidalPeriodicBoundary>, ExcludedVolumePotential<SimulatorTraits<float,  CuboidalPeriodicBoundary>>>;
+extern template class GlobalPairInteraction<SimulatorTraits<double, UnlimitedBoundary>       , ExcludedVolumePotential<double>>;
+extern template class GlobalPairInteraction<SimulatorTraits<float,  UnlimitedBoundary>       , ExcludedVolumePotential<float >>;
+extern template class GlobalPairInteraction<SimulatorTraits<double, CuboidalPeriodicBoundary>, ExcludedVolumePotential<double>>;
+extern template class GlobalPairInteraction<SimulatorTraits<float,  CuboidalPeriodicBoundary>, ExcludedVolumePotential<float >>;
 } // mjolnir
 #endif // MJOLNIR_SEPARATE_BUILD
 
