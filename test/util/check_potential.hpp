3cb626e3c0574ce0a622c45328a4e3fd68db2d8b
ToruNiina <niina.toru.68u@gmail.com>
2021-06-19 18:37:32 +0900

    test: update test_lennard_jones_potential

--- test/util/check_potential.hpp
+++ test/util/check_potential.hpp
@@ -39,6 +39,32 @@ void check_potential(const Potential& pot,
     }
 }
 
+template<typename Potential>
+void check_potential(const Potential& pot,
+        const typename Potential::parameter_type& para,
+        const typename Potential::real_type x_min,
+        const typename Potential::real_type x_max,
+        const typename Potential::real_type tol,
+        const typename Potential::real_type h,
+        const std::size_t N)
+{
+    using real_type = typename Potential::real_type;
+    const real_type dx = (x_max - x_min) / N;
+
+    for(std::size_t i=1; i<N; ++i)
+    {
+        const real_type x = x_min + i * dx;
+        const real_type pot1 = pot.potential(x + h, para);
+        const real_type pot2 = pot.potential(x - h, para);
+        const real_type dpot = (pot1 - pot2) / (2 * h);
+
+        const real_type deri = pot.derivative(x, para);
+
+        BOOST_TEST_MESSAGE("x = " << x);
+        BOOST_TEST(dpot == deri, boost::test_tools::tolerance(tol));
+    }
+}
+
 } // test
 } // mjolnir
 #endif// MJOLNIR_TEST_UTIL_CHECK_POTENTIAL_HPP
