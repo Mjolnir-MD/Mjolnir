c39cf190570d6e8ec60d2162385b79c6a01db337
ToruNiina <niina.toru.68u@gmail.com>
2021-07-13 14:49:09 +0900

    test: add check_net_force to check_force

--- test/util/check_force.hpp
+++ test/util/check_force.hpp
@@ -3,6 +3,11 @@
 #include <mjolnir/math/Vector.hpp>
 #include <mjolnir/core/System.hpp>
 
+#include <mjolnir/core/LocalInteractionBase.hpp>
+#include <mjolnir/core/GlobalInteractionBase.hpp>
+#include <type_traits>
+#include <test/util/clear_system.hpp>
+
 #ifdef BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 #else
@@ -14,6 +19,47 @@ namespace mjolnir
 namespace test
 {
 
+// Check if the sum of the forces is zero, only if the interaction is internal.
+
+template<typename traitsT, typename Interaction>
+typename std::enable_if<
+    std::is_base_of< LocalInteractionBase<traitsT>, Interaction>::value ||
+    std::is_base_of<GlobalInteractionBase<traitsT>, Interaction>::value>::type
+check_net_force(System<traitsT> sys,
+                const Interaction& interaction,
+                const typename traitsT::real_type tol)
+{
+    using real_type       = typename traitsT::real_type;
+    using coordinate_type = typename traitsT::coordinate_type;
+    clear_force(sys);
+
+    sys.preprocess_forces();
+    interaction.calc_force(sys);
+    sys.postprocess_forces();
+
+    coordinate_type f_tot = math::make_coordinate<coordinate_type>(0,0,0);
+    for(const auto& f : sys.forces())
+    {
+        f_tot += f;
+    }
+
+    BOOST_TEST(mjolnir::math::X(f_tot) == real_type(0), boost::test_tools::tolerance(tol));
+    BOOST_TEST(mjolnir::math::Y(f_tot) == real_type(0), boost::test_tools::tolerance(tol));
+    BOOST_TEST(mjolnir::math::Z(f_tot) == real_type(0), boost::test_tools::tolerance(tol));
+    return;
+}
+template<typename traitsT, typename Interaction>
+typename std::enable_if<
+    ! std::is_base_of< LocalInteractionBase<traitsT>, Interaction>::value &&
+    ! std::is_base_of<GlobalInteractionBase<traitsT>, Interaction>::value>::type
+check_net_force(System<traitsT>,
+                const Interaction&,
+                const typename traitsT::real_type)
+{
+    // external force can have net acceleration
+    return ;
+}
+
 // This checks force applied to each particle and the numerical difference of
 // the corresponding energy
 
@@ -23,6 +69,19 @@ void check_force(const System<traitsT>& init,
                  const typename traitsT::real_type tol,
                  const typename traitsT::real_type dr)
 {
+    using real_type = typename traitsT::real_type;
+
+    for(const auto& f : init.forces())
+    {
+        BOOST_TEST_REQUIRE(mjolnir::math::X(f) == real_type(0));
+        BOOST_TEST_REQUIRE(mjolnir::math::Y(f) == real_type(0));
+        BOOST_TEST_REQUIRE(mjolnir::math::Z(f) == real_type(0));
+    }
+    for(std::size_t i=0; i<9; ++i)
+    {
+        BOOST_TEST_REQUIRE(init.virial()[i] == real_type(0));
+    }
+
     System<traitsT> sys(init);
 
     for(std::size_t idx=0; idx<sys.size(); ++idx)
@@ -99,6 +158,12 @@ void check_force(const System<traitsT>& init,
             BOOST_TEST(-dE == dr * mjolnir::math::Z(sys.force(idx)),
                        boost::test_tools::tolerance(tol));
         }
+    }
+
+    // check if virial is not calculated in calc_force
+    for(std::size_t i=0; i<9; ++i)
+    {
+        BOOST_TEST_REQUIRE(init.virial()[i] == real_type(0));
     }
 }
 
