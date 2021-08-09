4fe1ee9683fb1a702ec02ee8c0fbc2b507674c3f
ToruNiina <niina.toru.68u@gmail.com>
2021-08-09 22:40:10 +0900

    fix: fix G-JF and simplify the code

--- mjolnir/core/GJFNVTLangevinIntegrator.hpp
+++ mjolnir/core/GJFNVTLangevinIntegrator.hpp
@@ -181,7 +181,7 @@ GJFNVTLangevinIntegrator<traitsT>::step(const real_type time,
     real_type largest_disp2(0);
     for(std::size_t i=0; i<sys.size(); ++i)
     {
-        const auto rm  = sys.rmass(i);  // reciprocal mass
+        const auto rm  = sys.rmass(i);        // reciprocal mass
         const auto r2m = real_type(0.5) * rm; // 1 / 2m
 
         auto&       p = sys.position(i);
@@ -189,24 +189,19 @@ GJFNVTLangevinIntegrator<traitsT>::step(const real_type time,
         auto&       f = sys.force(i);
         auto&    beta = this->noise_[i]; // TODO: refactor
         const auto& b = this->bs_[i];
+        const auto& a = this->vel_coefs_[i];
 
-        // beta^(n+1) = N(0, sqrt(2alpha kBT dt))
-        // v^(n+1/2)  = v^(n) + dt/2m f^(n) + beta^(n+1) / 2m
-        // r^(n+1)    = r^(n) + b dt v^(n+1/2)
-        // f^(n+1)    = -dU/dr|r^(n+1)
-        // v^(n+1)    = (1 - alpha*b*dt/m) * v^(n+1/2) + dt/2m f^(n+1) + beta^(n+1) / 2m
+        // r_n+1 =  r_n + bdtv_n + bdt^2/2m f_n + bdt/2m beta_n+1
+        // v_n+1 = av_n + dt/2m (af_n + f_n+1) + b/m beta_n+1
 
-        const auto dp = (b * dt_) * (v + r2m * (halfdt_ * f + beta));
+        beta = this->gen_R(rng) * betas_[i]; // beta_n+1
 
-        p  = sys.adjust_position(p + dp);
+        const auto dp = (b * dt_) * (v + r2m * (dt_ * f + beta));
 
-        beta = this->gen_R(rng) * betas_[i] * rm;
-        v += ((dt_ * rm) * f + beta) * real_type(0.5);
+        p = sys.adjust_position(p + dp);
+        v = a * (v + (dt_ * r2m) * f) + b * rm * beta;
 
-        v *= vel_coefs_[i];
-
-        // reset force
-        f = math::make_coordinate<coordinate_type>(0, 0, 0);
+        f = math::make_coordinate<coordinate_type>(0, 0, 0); // reset force
 
         // collect largest displacement
         largest_disp2 = std::max(largest_disp2, math::length_sq(dp));
@@ -217,14 +212,14 @@ GJFNVTLangevinIntegrator<traitsT>::step(const real_type time,
         auto& param = params_for_dynvar_.at(key);
         auto& var = kv.second;
 
-        const auto next_x = var.x() + param.b * dt_ * (var.v() +
-            0.5 / var.m() * (halfdt_ * var.f() + param.noise));
+        param.noise = rng.gaussian() * param.beta;
 
-        param.noise = rng.gaussian() * param.beta / var.m();
+        const auto next_x = var.x() + (param.b * dt_) * (var.v() +
+            (real_type(0.5) / var.m()) * (dt_ * var.f() + param.noise));
 
-        real_type next_v = var.v() +
-            (dt_ / var.m() * var.f() + param.noise) * real_type(0.5);
-        next_v *= param.vel_coef;
+        const auto next_v = param.vel_coef * (var.v() +
+                (real_type(0.5) * dt_ / var.m()) * var.f()) +
+                (param.b / var.m()) * param.noise;
 
         var.update(next_x, next_v, real_type(0));
     }
@@ -240,18 +235,15 @@ GJFNVTLangevinIntegrator<traitsT>::step(const real_type time,
         const auto  rm   = sys.rmass(i);  // reciprocal math
         const auto& f    = sys.force(i);
         auto&       v    = sys.velocity(i);
-        const auto& beta = this->noise_[i];
 
-        v += ((dt_ * rm) * f + beta) * real_type(0.5);
+        v += (dt_ * real_type(0.5) * rm) * f;
     }
     for(auto& kv : sys.variables()) // assume there are only a few dynvars
     {
-        const auto& key = kv.first;
-        const auto& param = params_for_dynvar_.at(key);
         auto& var = kv.second;
 
-        const auto next_v = var.v() + real_type(0.5) *
-            (dt_ / var.m() * var.f() + param.noise);
+        const auto next_v = var.v() +
+            (dt_ * real_type(0.5) / var.m()) * var.f();
 
         var.update(var.x(), next_v, var.f());
     }
