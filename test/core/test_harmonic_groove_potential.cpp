782e6da287e6c2158c676801deb1e18a143a6d44
yutakasi634 <p7dea87c7e4n7j@gmail.com>
2022-10-12 20:05:36 +0900

    test: add test for harmonic potential for external distance interaction

---
+++ test/core/test_harmonic_groove_potential.cpp
@@ -0,0 +1,48 @@
+#define BOOST_TEST_MODULE "test_harmonic_groove_potential"
+
+#ifdef BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+#else
+#include <boost/test/included/unit_test.hpp>
+#endif
+
+#include <mjolnir/forcefield/external/HarmonicGroovePotential.hpp>
+
+BOOST_AUTO_TEST_CASE(HarmonicGroovePotential_double)
+{
+    using real_type = double;
+    constexpr static std::size_t N   = 10000;
+    constexpr static real_type   h   = 1e-6;
+    constexpr static real_type   tol = 1e-5;
+
+    const real_type k  = 1.0;
+    const real_type v0 = 1.0;
+    const std::vector<std::pair<std::size_t, std::pair<real_type, real_type>>>
+       k_v0{{0u, {k, v0}}};
+
+    mjolnir::HarmonicGroovePotential<real_type> hg(k_v0);
+
+    const real_type test_range = 10.0;
+    const real_type z_min = v0 - test_range * 0.5;
+    const real_type z_max = v0 + test_range * 0.5;
+    const real_type dz    = (z_max - z_min) / N;
+
+    real_type z = z_min;
+    for(std::size_t i = 0; i < N; ++i)
+    {
+        const real_type pot1 = hg.potential(0, z + h);
+        const real_type pot2 = hg.potential(0, z - h);
+        const real_type dpot = (pot1 - pot2) / (2 * h);
+        const real_type deri = hg.derivative(0, z);
+
+        if(std::abs(deri) > tol)
+        {
+            BOOST_TEST(dpot == deri, boost::test_tools::tolerance(tol));
+        }
+        else
+        {
+            BOOST_TEST(deri == 0.0, boost::test_tools::tolerance(tol));
+        }
+        z += dz;
+    }
+}
